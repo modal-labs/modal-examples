3c939777ac4bd70204d2641f798487d28a509c8d
Elias Freider <elias@modal.com>
2022-08-25 11:28:13 +0200

    Updated examples from private repo

--- 06_gpu/stable_diffusion_slackbot.py
+++ 06_gpu/stable_diffusion_slackbot.py
@@ -1,33 +1,68 @@
 # ---
 # integration-test: false
+# output-directory: "/tmp/render"
 # ---
+# # Stable diffusion slackbot
+#
+# This tutorial shows you how to build a Slackbot that uses [stable diffusion](https://stability.ai/blog/stable-diffusion-public-release) to produce
+# realistic images from text prompts on command.
+# ![stable diffusion slackbot](./stable_diff_screenshot.png)
+
+# ## Basic setup
 
 import io
 import os
 from typing import Optional
 
-from fastapi import Request
-
 import modal
 
-stub = modal.Stub(
-    "stable-diff-bot", image=modal.DebianSlim().pip_install(["diffusers", "transformers", "scipy", "ftfy"])
-)
+# All Modal applications need a [`Stub`](/docs/reference/modal.Stub)—an object that acts as a recipe for
+# a Modal application. Let's give it a friendly name.
+
+stub = modal.Stub("stable-diff-bot")
+
+# ## Inference Function
+#
+# ### HuggingFace token
+# We're going to make use of the pre-trained [stable diffusion model](https://github.com/CompVis/stable-diffusion), made available
+# through HuggingFace's `diffusers` library. To gain access, you first need to log in to your HuggingFace account ([sign up here](https://huggingface.co/join))
+# and request access on the [model card page](https://huggingface.co/CompVis/stable-diffusion-v1-4).
+#
+# Next, [create a HuggingFace access token](https://huggingface.co/settings/tokens). To make it available to a Modal function, we
+# can create a Modal Secret from the [secrets page](https://modal.com/secrets). Let's use the key name `HUGGINGFACE_TOKEN`. This means
+# that functions that use this secret will automatically have `HUGGINGFACE_TOKEN` set as an environment variable.
+# ![create a huggingface token](./huggingface_token.png)
+# ### Model cache
+#
+# The `diffusers` library downloads the weights for a pre-trained model to a local directory, if those weights don't already exist.
+# To decrease start-up time, we want this download to happen just once, even across separate function invocations.
+# To accomplish this, we use a [`SharedVolume`](/docs/guide/shared-volumes), a writable volume that can be attached to Modal functions
+# and persisted across function runs.
 
 volume = modal.SharedVolume().persist("stable-diff-model-vol")
+
+#
+# ### The actual function
+#
+# Now that we have our token and `SharedVolume` set up, we can put everything together.
+#
+# Let's define a function that takes a text prompt and an optional channel name (so we can post results to Slack if the value is set)
+# and runs stable diffusion.
+# The `@stub.function` decorator declares all the resources this function will use: we configure it to use a GPU, run on an image that has
+# all the packages we need to run the model, mount the `SharedVolume` to a path of our choice, and also provide it the secret that
+# contains the token we created above.
+#
+# By setting the `cache_dir` argument for the model to the mount path of our `SharedVolume`, we ensure that the model weights are downloaded only the first time the function is run.
 
 CACHE_PATH = "/root/model_cache"
 
 
-@stub.function(image=modal.DebianSlim().pip_install(["slack-sdk"]), secret=modal.ref("stable-diff-secret"))
-def post_to_slack(prompt: str, channel_name: str, image_bytes: bytes):
-    import slack_sdk
-
-    client = slack_sdk.WebClient(token=os.environ["SLACK_BOT_TOKEN"])
-    client.files_upload(channels=channel_name, title=prompt, content=image_bytes)
-
-
-@stub.function(gpu=True, shared_volumes={CACHE_PATH: volume}, secret=modal.ref("stable-diff-secret"))
+@stub.function(
+    gpu=True,
+    image=modal.DebianSlim().pip_install(["diffusers", "transformers", "scipy", "ftfy"]),
+    shared_volumes={CACHE_PATH: volume},
+    secret=modal.ref("huggingface-secret"),
+)
 async def run_stable_diffusion(prompt: str, channel_name: Optional[str] = None):
     from diffusers import StableDiffusionPipeline
     from torch import autocast
@@ -41,25 +76,81 @@ async def run_stable_diffusion(prompt: str, channel_name: Optional[str] = None):
     with autocast("cuda"):
         image = pipe(prompt, num_inference_steps=100)["sample"][0]
 
+    # Convert PIL Image to PNG byte array.
     buf = io.BytesIO()
     image.save(buf, format="PNG")
     img_bytes = buf.getvalue()
 
     if channel_name:
-        post_to_slack(prompt, channel_name, img_bytes)
+        # `post_to_slack` is implemented further below.
+        post_image_to_slack(prompt, channel_name, img_bytes)
 
     return img_bytes
 
 
-# python-multipart is needed for fastapi form parsing.
-@stub.webhook(method="POST", image=modal.DebianSlim().pip_install(["python-multipart"]))
+# ## Slack webhook
+#
+# Now that we have our function, we'd like to trigger it interactively from a Slack channel. Slack lets you do this using
+# [slash commands](https://api.slack.com/interactivity/slash-commands)—a feature that lets you register prefixes (such as "/run-my-bot") to
+# trigger webhooks of your choice.
+#
+# To create a Modal function that's served as a webhook, we just use the [`@stub.webhook`](/docs/guide/webhooks#webhook) decorator
+# instead of `@stub.function`. Modal webhooks use [FastAPI](https://fastapi.tiangolo.com/) under the hood; here we accept FastAPI's generic
+# `Request` type and retrieve the form `body` from it. Consult [Slack's documentation](https://api.slack.com/interactivity/slash-commands#app_command_handling) for the form schema.
+#
+# Instead of blocking on the result of the stable diffusion model (which could take some time), we want to notify the user immediately that their request is being processed.
+# Modal Functions let you [`submit`](/docs/reference/modal.Function#submit) an input without waiting for the results, which we use here to kick off model inference as a background task.
+
+from fastapi import Request
+
+
+@stub.webhook(method="POST")
 async def entrypoint(request: Request):
     body = await request.form()
     prompt = body["text"]
-    # Start the function call but return immediately.
     run_stable_diffusion.submit(prompt, body["channel_name"])
     return f"Running stable diffusion for {prompt}."
 
+
+# ## Post to Slack
+#
+# Finally, let's define a function to post images to a Slack channel.
+#
+# First, we need to create a Slack App
+# and store the token for our app as a Modal secret. To do so, visit the the Modal [Secrets](/secrets) page and click on
+# "create a Slack secret". Then, under "Where to find the credentials?" you will find end-to-end instructions
+# for how to create a Slack App, give it OAuth permissions and get a token. Note that you need to add the
+# `file:write` OAuth scope to the created app.
+# ![create a slack secret](./slack_secret.png)
+#
+# Now we define a function that uses this secret and runs on an image that has `slack-sdk` installed.
+
+
+@stub.function(image=modal.DebianSlim().pip_install(["slack-sdk"]), secret=modal.ref("stable-diff-slackbot-secret"))
+def post_image_to_slack(title: str, channel_name: str, image_bytes: bytes):
+    import slack_sdk
+
+    client = slack_sdk.WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+    client.files_upload(channels=channel_name, title=title, content=image_bytes)
+
+
+# ## Deploy the Slackbot
+#
+# That's all the code we need! To deploy your application, run
+# ```shell
+# modal app deploy stable_diffusion_slackbot.py
+# ```
+# If successful, this will print a URL for your new webhook. To point your Slack app at it:
+# - Go back to the [Slack apps page](https://api.slack.com/apps/).
+# - Find your app and navigate to "Slash Commands" under "Features" in the left sidebar.
+# - Click on "Create New Command" and paste the webhook URL from Modal into the "Request URL" field.
+# - Name the command whatever you like, and hit "Save".
+# - Reinstall the app to your workspace.
+#
+# We're done! 🎉 Install the app to any channel you're in, and you can trigger it with the command you chose above.
+#
+# ## Run Manually
+# We can also trigger `run_stable_diffusion` manually for easier debugging.
 
 OUTPUT_DIR = "/tmp/render"
 
@@ -74,6 +165,13 @@ if __name__ == "__main__":
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     with stub.run():
-        for idx, img_bytes in enumerate(run_stable_diffusion.map([prompt] * 3)):
-            with open(os.path.join(OUTPUT_DIR, "output_%d.png" % idx), "wb") as f:
-                f.write(img_bytes)
+        img_bytes = run_stable_diffusion(prompt)
+        with open(os.path.join(OUTPUT_DIR, "output.png"), "wb") as f:
+            f.write(img_bytes)
+
+# The above
+# code lets us call our script as follows:
+# ```shell
+# python stable_diffusion_slackbot.py "a photo of an astronaut riding a horse on mars"
+# ```
+# The resulting image can be found in `/tmp/render/output.png`.
