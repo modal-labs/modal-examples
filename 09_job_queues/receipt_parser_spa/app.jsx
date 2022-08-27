9f2948d0ef9f9651bad1870f1f71a9b71bbb4d33
Akshat Bubna <akshatb42@gmail.com>
2022-08-27 02:17:15 +0000

    use jsx

---
+++ 09_job_queues/receipt_parser_spa/app.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>¡Gracias por tu like!</h1>
+    </div>
+  );
+}
+
+
+const container = document.getElementById('react');
+ReactDOM.createRoot(container).render(<App />);
