4e654ffb14e83d756e365a3edd808372d8919f7f
Akshat Bubna <akshatb42@gmail.com>
2022-09-01 20:10:55 -0400

    Job queue and React SPA tutorials (#7)

---
+++ 09_job_queues/doc_ocr_jobs.py
@@ -0,0 +1,101 @@
+# ---
+# integration-test: false
+# ---
+# # Document OCR job queue
+#
+# This tutorial shows you how to use Modal as an infinitely scalable job queue
+# that can service async tasks from a web app. For the purpose of this tutorial,
+# we've also built a [React + FastAPI web app on Modal](/docs/guide/ex/doc_ocr_webapp)
+# that works together with it, but note that you don't need a web app running on Modal
+# to use this pattern. You can submit async tasks to Modal from any Python 
+# application (for example, a regular Django app running on Kubernetes).
+# 
+# Our job queue will handle a single task: running OCR transcription for images.
+# We'll make use of a pre-trained Document Understanding model using the 
+# [donut](https://github.com/clovaai/donut) package to accomplish this.
+
+# ## Define a Stub
+#
+# Let's first import `modal` and define a [`Stub`](/docs/reference/modal.Stub). Later, we'll use the name provided
+# for our `Stub` to find it from our web app, and submit tasks to it.
+
+import modal
+
+stub = modal.Stub("doc_ocr_jobs")
+
+# ## Model cache
+#
+# `donut` downloads the weights for pre-trained models to a local directory, if those weights don't already exist.
+# To decrease start-up time, we want this download to happen just once, even across separate function invocations.
+# To accomplish this, we use a [`SharedVolume`](/docs/guide/shared-volumes), a writable volume that can be attached
+# to Modal functions and persisted across function runs.
+
+volume = modal.SharedVolume().persist("doc_ocr_model_vol")
+CACHE_PATH = "/root/model_cache"
+
+# ## Handler function
+#
+# Now let's define our handler function. Using the [@stub.function](https://modal.com/docs/reference/modal.Stub#function)
+# decorator, we set up a Modal [Function](/docs/reference/modal.Function) that uses GPUs, 
+# has a [`SharedVolume`](/docs/guide/shared-volumes) mount, runs on a [custom container image](/docs/guide/custom-container),
+# and automatically [retries](/docs/guide/retries#function-retries) failures up to 3 times.
+
+@stub.function(
+    gpu=True,
+    image=modal.DebianSlim().pip_install(["donut-python"]),
+    shared_volumes={CACHE_PATH: volume},
+    retries=3,
+)
+def parse_receipt(image: bytes):
+    from PIL import Image
+    from donut import DonutModel
+    import torch
+    import io
+
+    # Use donut fine-tuned on an OCR dataset.
+    task_prompt = f"<s_cord-v2>"
+    pretrained_model = DonutModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2", cache_dir=CACHE_PATH)
+
+    # Initialize model.
+    pretrained_model.half()
+    device = torch.device("cuda")
+    pretrained_model.to(device)
+
+    # Run inference.
+    input_img = Image.open(io.BytesIO(image))
+    output = pretrained_model.inference(image=input_img, prompt=task_prompt)["predictions"][0]
+    print("Result: ", output)
+
+    return output
+
+# ## Deploy
+#
+# Now that we have a function, we can publish it by deploying the app:
+#
+# ```shell
+# modal app deploy doc_ocr_jobs.py
+# ```
+#
+# Once it's published, we can [look up](/docs/guide/sharing-functions) this function from another 
+# Python process and submit tasks to it:
+#
+# ```python
+# fn = modal.lookup("doc_ocr_jobs", "parse_receipt")
+# fn.submit(my_image)
+# ```
+#
+# Modal will auto-scale to handle all the tasks queued, and
+# then scale back down to 0 when there's no work left. To see how you could use this from a Python web 
+# app, take a look at the [receipt parser frontend](/docs/guide/ex/doc_ocr_webapp)
+# tutorial.
+
+# ## Run manually
+# 
+# We can also trigger `parse_receipt` manually for easier debugging. To try it out, you can find some 
+# example receipts [here](https://drive.google.com/drive/folders/1S2D1gXd4YIft4a5wDtW99jfl38e85ouW).
+
+if __name__ == "__main__":
+    with stub.run():
+        with open("./receipt.png", "rb") as f:
+            image = f.read()
+            print(parse_receipt(image))
---
+++ 09_job_queues/doc_ocr_webapp.py
@@ -0,0 +1,101 @@
+# ---
+# integration-test: false
+# ---
+# # Document OCR web app
+#
+# This tutorial shows you how to use Modal to deploy a fully serverless
+# [React](https://reactjs.org/) + [FastAPI](https://fastapi.tiangolo.com/) application.
+# We're going to build a simple "Receipt Parser" web app that submits OCR transcription
+# tasks to a separate Modal app defined in the [Job Queue 
+# tutorial](/docs/guide/ex/doc_ocr_jobs), polls until the task is completed, and displays
+# the results.
+
+# ## Basic setup
+#
+# Let's get the imports out of the way and define a [`Stub`](/docs/reference/modal.Stub).
+
+import fastapi
+import fastapi.staticfiles
+
+import modal
+from pathlib import Path
+
+stub = modal.Stub()
+
+# Modal works with any [ASGI](/docs/guide/webhooks#serving-asgi-and-wsgi-apps) or 
+# [WSGI](/docs/guide/webhooks#wsgi) web framework. Here, we choose to use [FastAPI](https://fastapi.tiangolo.com/).
+
+web_app = fastapi.FastAPI()
+
+# We're going to submit tasks to the function defined in the [Job Queue 
+# tutorial](/docs/guide/ex/doc_ocr_jobs), so we import it first using 
+# [`modal.lookup`](/docs/guide/sharing-functions#calling-code-from-outside-modal).
+
+parse_receipt = modal.lookup("doc_ocr_jobs", "parse_receipt")
+
+# We need two endpoints: one to accept an image and submit it to the Modal job queue,
+# and another to poll for the results of the job. 
+#
+# `/parse` calls [`.submit()`](/docs/reference/modal.Function#submit) on the function handle
+# we imported above, to kick off our function without blocking on the results. `submit` returns
+# a unique ID for the function call, that we can use later to poll for its result.
+
+@web_app.post("/parse")
+async def parse(request: fastapi.Request):
+    form = await request.form()
+    receipt = await form['receipt'].read()
+    call = parse_receipt.submit(receipt)
+    return { "call_id": call.object_id }
+
+# `/result` uses the provided `call_id` to instantiate a `modal.FunctionCall` object, and attempt
+# to get its result. If the call hasn't finished yet, we return a `202` status code, which indicates
+# that the server is still working on the job.
+
+@web_app.get("/result/{call_id}")
+async def poll_results(call_id: str):
+    from modal.functions import FunctionCall
+
+    function_call = FunctionCall.from_id(call_id)
+    try:
+        result = function_call.get(timeout=0)
+    except TimeoutError:
+        return fastapi.responses.JSONResponse(status_code=202)
+
+    return result
+
+# Finally, we mount the static files for our front-end. We've made [a simple React 
+# app](https://github.com/modal-labs/modal-examples/tree/main/09_job_queues/doc_ocr_frontend)
+# that hits the two endpoints defined above. To package these files with our app, first
+# we get the local assets path, and then create a modal [`Mount`](/docs/guide/local-data#mounting-directories)
+# that mounts this directory at `/assets` inside our container. Then, we instruct FastAPI to [serve
+# this static file directory](https://fastapi.tiangolo.com/tutorial/static-files/) a† our rooth path.
+
+assets_path = Path(__file__).parent / "doc_ocr_frontend"
+
+@stub.asgi(mounts=[modal.Mount("/assets", local_dir=assets_path)])
+def wrapper():
+    web_app.mount("/", fastapi.staticfiles.StaticFiles(directory="/assets", html=True))
+
+    return web_app
+
+# ## Deploy
+#
+# That's all! To deploy your application, run
+#
+# ```shell
+# modal app deploy doc_ocr_webapp.py
+# ```
+#
+# If successful, this will print a URL for your app, that you can navigate to from
+# your browser 🎉 .
+#
+# ![receipt parser frontend](./receipt_parser_frontend.png)
+# 
+# ## Developing
+#
+# If desired, instead of deploying, we can [serve](/docs/guide/webhooks#developing-with-stubserve)
+# our app ephemerally. In this case, Modal watches all the mounted files, and updates
+# the app if anything changes.
+
+if __name__ == "__main__":
+    stub.serve()
