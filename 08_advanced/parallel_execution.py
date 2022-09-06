26b529c9c01ff85dfeee44572fbb295f448ad567
Eric Zhang <ekzhang1@gmail.com>
2022-09-06 17:55:08 +0000

    Fix the parallel execution example

--- 08_advanced/parallel_execution.py
+++ 08_advanced/parallel_execution.py
@@ -4,38 +4,38 @@ import modal
 stub = modal.Stub()
 
 
-
 @stub.function
 def step1(word):
-    time.sleep(7)
+    time.sleep(2)
     print("step1 done")
     return word
 
+
 @stub.function
 def step2(number):
-    time.sleep(3)
-    if number == 0:
-        raise Exception("aborting")
-    return number
+    time.sleep(1)
     print("step2 done")
-
+    if number == 0:
+        raise ValueError("custom error")
+    return number
 
 
 if __name__ == "__main__":
     with stub.run() as app:
-        # .submit() starts running a function and returns a FunctionCall handle immediately
+        # Start running a function and return a handle to its result.
         word_call = step1.submit("foo")
         number_call = step2.submit(2)
-        # prints "foofoo" after 7 seconds
+
+        # Print "foofoo" after 2 seconds.
         print(word_call.get() * number_call.get())
 
-        # alternatively, use `modal.gather(...)` as a convenience wrapper
-        # which also makes sure to raise an error as soon as the first
-        # failing function call fails
+        # Alternatively, use `modal.gather(...)` as a convenience wrapper,
+        # which returns an error if either call fails.
+        results = modal.functions.gather(step1.submit("bar"), step2.submit(4))
+        assert results == ["bar", 4]
 
-
-        # raises exception after 3 seconds:
-        word, number = modal.functions.gather(
-            step1.submit("bar"),
-            step2.submit(0)
-        )
+        # Raise exception after 2 seconds.
+        try:
+            modal.functions.gather(step1.submit("bar"), step2.submit(0))
+        except ValueError as exc:
+            assert str(exc) == "custom error"
