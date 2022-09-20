160c02674b883e25df86ae6a52bd30eeb73396ea
Erik Bernhardsson <mail@erikbern.com>
2022-09-20 16:25:42 -0400

    break it up a bit

--- misc/batch_inference_using_huggingface.py
+++ misc/batch_inference_using_huggingface.py
@@ -1,10 +1,15 @@
-import modal
+import io
+import random
 import tarfile
 import urllib.request as urllib2
 
+import modal
+
 HUGGINGFACE_DIR = "/huggingface"
 stub = modal.Stub(
-    image=modal.Image.debian_slim().pip_install(["torch", "transformers"])
+    image=modal.Image.debian_slim().pip_install(
+        ["matplotlib", "sklearn", "torch", "transformers"]
+    )
 )
 stub.sv = modal.SharedVolume()
 
@@ -15,58 +20,76 @@ class SentimentAnalysis:
     def __enter__(self):
         from transformers import pipeline
 
-        print("loading pipeline...")
         self.sentiment_pipeline = pipeline(
             model="distilbert-base-uncased-finetuned-sst-2-english"
         )
-        print("ready!")
 
     @stub.function(
-        shared_volumes={HUGGINGFACE_DIR: stub.sv}, secrets=[env], concurrency_limit=25
+        shared_volumes={HUGGINGFACE_DIR: stub.sv},
+        secrets=[env],
+        cpu=3,  # TODO: bump CPU
     )
     def predict(self, phrase: str):
-        (pred,) = self.sentiment_pipeline(phrase, truncation=True, max_length=512)
-
-        # Return the positive score
-        if pred["label"] == "POSITIVE":
-            return pred["score"]
-        else:
-            return 1 - pred["score"]
+        pred = self.sentiment_pipeline(phrase, truncation=True, max_length=512, top_k=2)
+        # pred will look like: [{'label': 'NEGATIVE', 'score': 0.99}, {'label': 'POSITIVE', 'score': 0.01}]
+        probs = {p["label"]: p["score"] for p in pred}
+        return probs["POSITIVE"]
 
 
 @stub.function
-def run():
-    # First, read reviews dataset
-    print("reading dataset")
+def get_data():
     rt = urllib2.urlopen(
         "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
     )
     tf = tarfile.open(fileobj=rt, mode="r:gz")
-    labels = []
-    reviews = []
+    data = []
     for member in tf:
         if member.name.startswith("aclImdb/test/pos/"):
-            label = 0
-        elif member.name.startswith("aclImdb/test/neg/"):
             label = 1
+        elif member.name.startswith("aclImdb/test/neg/"):
+            label = 0
         else:
             continue
         with tf.extractfile(member) as tfobj:
             review = tfobj.read().decode()
-            labels.append(label)
-            reviews.append(review)
-    print("got", len(reviews), "reviews")
+            data.append((label, review))
 
-    # In order to force the model to be downloaded only once, run a dummy predictor
-    # Otherwise, the model will be downloaded by every worker in the map(...)
-    predictor = SentimentAnalysis()
-    predictor.predict("test")
+    random.shuffle(data)
+    return data
 
-    # Now, let's run batch inference over it
-    for ret in predictor.predict.map(reviews):
-        print(ret)
+
+@stub.function
+def roc_plot(labels, predictions):
+    from matplotlib import pyplot
+    from sklearn.metrics import RocCurveDisplay
+
+    pyplot.style.use("ggplot")
+    RocCurveDisplay.from_predictions(labels, predictions)
+    buf = io.BytesIO()
+    pyplot.savefig(buf, format="png")
+    return buf.getvalue()
 
 
 if __name__ == "__main__":
     with stub.run():
-        run()
+        print("Downloading data...")
+        data = get_data()
+        # data = data[:100]
+        print("Got", len(data), "reviews")
+        reviews = [review for label, review in data]
+        labels = [label for label, review in data]
+
+        # In order to force the model to be downloaded only once, run a dummy predictor
+        # Otherwise, the model will be downloaded by multiple workers starting simultaneously
+        predictor = SentimentAnalysis()
+        predictor.predict("test")
+
+        # Now, let's run batch inference over it
+        predictions = list(predictor.predict.map(reviews))
+
+        # Generate a ROC plot
+        png_data = roc_plot(labels, predictions)
+        fn = "/tmp/roc.png"
+        with open(fn, "wb") as f:
+            f.write(png_data)
+        print(f"Wrote ROC curve to {fn}")
