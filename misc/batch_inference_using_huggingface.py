62693a60633be6fc2eee874c27b618e0bb5366a3
Erik Bernhardsson <mail@erikbern.com>
2022-09-20 21:40:21 -0400

    Add comments, simplify batch inference example (#20)
    * Add comments, simplify batch inference example
    
    * plot
    
    * black
    
    * more docs
    
    * misc fixes

--- misc/batch_inference_using_huggingface.py
+++ misc/batch_inference_using_huggingface.py
@@ -1,23 +1,48 @@
 # ---
 # integration-test: false
 # ---
+# # Batch inference using a model from Huggingface
+#
+# <center>
+#   <img src="./batch_inference_huggingface.png"/>
+# </center>
+#
+# This example shows how to use a sentiment analysis model from Huggingface to classify 25,000 movie ratings.
+#
+# Some Modal features it uses:
+# * Container lifecycle hook: this lets us load the model once in each container
+# * CPU requests: the prediction function is very CPU-hungry, so we reserve 8 cores
+# * Mapping: we map over 25,000 sentences in about a minute or less
+#
+# ## Basic setup
+#
+# Let's get started writing code.
+# We need some global packages first.
+# Then, let's set up the Modal environment.
+# We need a few Python packages, including `transformers`, which is the main
+# Huggingface package.
 
 import io
-import random
-import tarfile
-import urllib.request as urllib2
 
 import modal
 
-HUGGINGFACE_DIR = "/huggingface"
 stub = modal.Stub(
     image=modal.Image.debian_slim().pip_install(
-        ["matplotlib", "sklearn", "torch", "transformers"]
+        ["datasets", "matplotlib", "sklearn", "torch", "transformers"]
     )
 )
-stub.sv = modal.SharedVolume()
 
-env = modal.Secret({"TRANSFORMERS_CACHE": HUGGINGFACE_DIR})
+# ## Defining the prediction function
+#
+# Instead of a global function, we put the method on a class,
+# and define an `__enter__` method on that class.
+# This method will be executed only once for each container.
+# The point of this is to load the model into memory only once, since
+# this is a slow operaton (a few seconds).
+#
+# Since the transformer model is very CPU-hungry, we allocate 8 CPUs
+# to the model.
+# Every container that runs will have 8 CPUs set aside for it.
 
 
 class SentimentAnalysis:
@@ -28,11 +53,7 @@ class SentimentAnalysis:
             model="distilbert-base-uncased-finetuned-sst-2-english"
         )
 
-    @stub.function(
-        shared_volumes={HUGGINGFACE_DIR: stub.sv},
-        secrets=[env],
-        cpu=3,  # TODO: bump CPU
-    )
+    @stub.function(cpu=8)
     def predict(self, phrase: str):
         pred = self.sentiment_pipeline(phrase, truncation=True, max_length=512, top_k=2)
         # pred will look like: [{'label': 'NEGATIVE', 'score': 0.99}, {'label': 'POSITIVE', 'score': 0.01}]
@@ -40,26 +61,28 @@ class SentimentAnalysis:
         return probs["POSITIVE"]
 
 
+# ## Getting data
+#
+# We need some data to run the batch inference on.
+# We use this [dataset of IMDB reviews](https://ai.stanford.edu/~amaas/data/sentiment/) for this purpose.
+# As it turns out, Huggingface also [offers this data](https://huggingface.co/datasets/imdb), which we
+# can download using the `datasets` package:
+
+
 @stub.function
 def get_data():
-    rt = urllib2.urlopen(
-        "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
-    )
-    tf = tarfile.open(fileobj=rt, mode="r:gz")
-    data = []
-    for member in tf:
-        if member.name.startswith("aclImdb/test/pos/"):
-            label = 1
-        elif member.name.startswith("aclImdb/test/neg/"):
-            label = 0
-        else:
-            continue
-        with tf.extractfile(member) as tfobj:
-            review = tfobj.read().decode()
-            data.append((label, review))
+    from datasets import load_dataset
 
-    random.shuffle(data)
+    imdb = load_dataset("imdb")
+    data = [(row["text"], row["label"]) for row in imdb["test"]]
     return data
+
+
+# ## Plotting the ROC curve
+#
+# In order to evaluate the classifier, let's plot an
+# [ROC curve](https://en.wikipedia.org/wiki/Receiver_operating_characteristic).
+# This is a common way to evaluate classifiers on binary data.
 
 
 @stub.function
@@ -74,20 +97,32 @@ def roc_plot(labels, predictions):
     return buf.getvalue()
 
 
+# Spoiling the end, the output of this script will look like this:
+#
+# ![roc](./batch_inference_roc.png)
+
+# ## Putting it together
+#
+# The main flow of the code downloads the data, then runs the batch inference,
+# then plots the results.
+# Each prediction takes roughly 0.1-1s, so if we ran everything sequentially it would take 2,500-25,000 seconds.
+# That's a lot! Luckily because of Modal's `.map` method, we can process everything in a couple of minutes at most.
+
 if __name__ == "__main__":
     with stub.run():
         print("Downloading data...")
         data = get_data()
-        # data = data[:100]
         print("Got", len(data), "reviews")
-        reviews = [review for label, review in data]
-        labels = [label for label, review in data]
+        reviews = [review for review, label in data]
+        labels = [label for review, label in data]
 
-        # In order to force the model to be downloaded only once, run a dummy predictor
-        # Otherwise, the model will be downloaded by multiple workers starting simultaneously
-        print("Downloading model...")
+        # Let's check that the model works by classifying the first 5 entries
         predictor = SentimentAnalysis()
-        predictor.predict("test")
+        for review, label in data[:5]:
+            prediction = predictor.predict(review)
+            print(
+                f"Sample prediction with positivity score {prediction}:\n{review}\n\n"
+            )
 
         # Now, let's run batch inference over it
         print("Running batch prediction...")
@@ -100,3 +135,19 @@ if __name__ == "__main__":
         with open(fn, "wb") as f:
             f.write(png_data)
         print(f"Wrote ROC curve to {fn}")
+
+# ## Running this
+#
+# When you run this, you should see something like this:
+#
+# ![progress](./batch_inference_progress.png)
+#
+# The whole thing should take 1-2 minutes to run!
+#
+# ## Further optimization notes
+#
+# Every container downloads the model when it starts, which is a bit inefficient.
+# In order to improve this, what you could do is to set up a shared volume that gets
+# mounted to each container.
+# You have to use that in conjunction with the `TRANSFORMERS_CACHE` environment variable
+# to tell Huggingface where to store the model.
