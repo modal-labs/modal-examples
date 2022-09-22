f65b87ee63c4a71b2297792d8f02d5472cc1c13a
Elias Freider <elias@modal.com>
2022-09-22 10:49:29 +0200

    Refactor

--- kedrorun/kedro_modal/kedro_modal/cli.py
+++ kedrorun/kedro_modal/kedro_modal/cli.py
@@ -1,13 +1,9 @@
-from contextlib import contextmanager
-import os
-import shutil
-import subprocess
-import sys
-from typing import Iterator
-import warnings
-import click
 from pathlib import Path
-from modal import App, Image, Mount, SharedVolume, Stub, lookup
+
+import click
+from modal import lookup
+
+from .modal_functions import main_stub, sync_stub
 
 
 @click.group(name="Kedro-Modal")
@@ -22,85 +18,29 @@ def commands():
 def modal_group():
     """Interact with Kedro pipelines run on Modal"""
 
-def run_kedro(project_path: Path, data_path: Path):
-    shutil.rmtree(project_path / "data")  # replace mount
-    (project_path / "data").symlink_to(data_path)
-    os.chdir(project_path)
-    subprocess.call(["kedro", "run"])
 
-
-def sync_data(source: Path, destination: Path, reset: bool=False):
-    """Sync a local data directory *to* a shared volume"""
-
-    # TODO: only sync raw data - no intermediates etc?
-
-    if not destination.exists() or reset:
-        if reset:
-            shutil.rmtree(destination)
-        shutil.copytree(source, destination)
-
-
-
-def _modal_stub(metadata) -> Stub:
-    source_path = metadata.source_dir
-    requirements_txt = (source_path / "requirements.txt")
-
-    image = Image.debian_slim()
-    if requirements_txt.exists():
-        image = image.pip_install_from_requirements(requirements_txt)
-    else:
-        warnings.warn("No requirements.txt in kedro src dir - attaching no dependencies")
-        image = image.pip_install("kedro")
-
-    stub = Stub(image=image)
-    volume_name = f"kedro.{metadata.project_name}.storage"
-    data_volume = SharedVolume().persist(volume_name)
-
-    remote_project_mount_point = Path(f"/kedro-project/{metadata.package_name}")
-    kedro_proj_mount = Mount(remote_dir=remote_project_mount_point, local_dir=metadata.project_path)
-    stub.function(mounts=[kedro_proj_mount], shared_volumes={"/kedro-storage": data_volume})(run_kedro)
-    stub.function(
-        mounts=[kedro_proj_mount],
-        shared_volumes={"/kedro-storage": data_volume}
-    )(sync_data)
-    remote_data_path = Path("/kedro-storage/data")
-    return stub, remote_project_mount_point, remote_data_path
-
-
-@modal_group.command
+@modal_group.command(help="Run kedro project on Modal")
 @click.pass_obj
 def run(metadata):
-    stub, remote_project_mount_point, remote_data_path = _modal_stub(metadata)
+    stub, remote_project_mount_path, remote_data_path = main_stub(metadata.project_path, metadata.project_name, metadata.package_name)
     with stub.run() as app:
-        app.sync_data(remote_project_mount_point / "data", remote_data_path, reset=False)
-        app.run_kedro(remote_project_mount_point, remote_data_path)
+        app.sync_data(remote_project_mount_path / "data", remote_data_path, reset=False)
+        app.run_kedro(remote_project_mount_path, remote_data_path)
 
 
-@modal_group.command
+@modal_group.command(help="Deploy kedro project to Modal, scheduling it to run daily")
 @click.pass_obj
 def deploy(metadata):
-    stub, remote_project_mount_point, remote_data_path = _modal_stub(metadata)
+    stub, remote_project_mount_point, remote_data_path = main_stub(metadata.project_path, metadata.project_name, metadata.package_name)
     name=f"kedro.{metadata.project_name}"
     stub.deploy(name)
     sync_data = lookup(name, "sync_data")  # use the deployed function
     sync_data(remote_project_mount_point / "data", remote_data_path)
 
 
-@modal_group.command
+@modal_group.command(short_help="Sync the local data directory to Modal", help="Sync the local data directory to Modal, overwriting any existing data there")
 @click.pass_obj
 def reset(metadata):
-    stub = Stub(name=f"kedro.{metadata.project_name}")
-    volume_name = f"kedro.{metadata.project_name}.storage"
-    data_volume = SharedVolume().persist(volume_name)
-
-    remote_kedro_data_mount_path = Path(f"/kedro-project/{metadata.package_name}/data")
-    kedro_data_mount = Mount(remote_dir=remote_kedro_data_mount_path, local_dir=metadata.project_path / "data")
-    modal_sync_data = stub.function(
-        mounts=[kedro_data_mount],
-        shared_volumes={"/kedro-storage": data_volume}
-    )(sync_data)
-
-    remote_data_path = Path("/kedro-storage/data")
-
-    with stub.run():
-        modal_sync_data(remote_kedro_data_mount_path, remote_data_path, reset=True)
+    stub, source_path, destination_path = sync_stub(metadata.project_path, metadata.project_name)
+    with stub.run() as app:
+        app.sync_data(source_path, destination_path, reset=True)
