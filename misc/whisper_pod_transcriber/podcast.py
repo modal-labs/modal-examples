6e1e2a15d1387a2fe5a3febdb4e97f991a861da3
Jonathon Belotti <jonathon@modal.com>
2022-10-19 01:16:04 +0000

    Eliminate 1-2 word transcript segments from UI

--- misc/whisper_pod_transcriber/podcast.py
+++ misc/whisper_pod_transcriber/podcast.py
@@ -2,7 +2,9 @@ import dataclasses
 import os
 import pathlib
 import urllib.request
-from typing import NamedTuple, Optional, Union
+from typing import NamedTuple, Optional, TypedDict, Union
+
+Segment = TypedDict('Segment', {'text': str, 'start': float, "end": float})
 
 
 @dataclasses.dataclass
@@ -253,3 +255,32 @@ def store_original_audio(url: str, destination: pathlib.Path, overwrite: bool = 
     with open(destination, "wb") as f:
         f.write(podcast_download_result.data)
     print(f"Stored audio episode at {destination}.")
+
+
+def coalesce_short_transcript_segments(segments: list[Segment]) -> list[Segment]:
+    """
+    Some extracted transcript segments from openai/whisper are really short, like even just one word.
+    This function accepts a minimum segment length and combines short segments until the minimum is reached. 
+    """
+    minimum_transcript_len = 200  # About 2 sentences.
+    previous = None
+    long_enough_segments = []
+    for current in segments:
+        if previous is None:
+            previous = current
+        elif len(previous["text"]) < minimum_transcript_len:
+            previous = _merge_segments(left=previous, right=current)
+        else:
+            long_enough_segments.append(previous)
+            previous = current
+    if previous:
+        long_enough_segments.append(previous)
+    return long_enough_segments
+
+
+def _merge_segments(left: Segment, right: Segment) -> Segment:
+    return {
+        "text": left["text"] + " " + right["text"],
+        "start": left["start"],
+        "end": right["end"],
+    }
