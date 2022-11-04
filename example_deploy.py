c591dc06e54275e8a52294d7c5170c1036fcc8c7
Jonathon Belotti <jonathon@modal.com>
2022-11-04 03:51:28 +0000

    Look at metadata to determine whether example is deployable

--- example_deploy.py
+++ example_deploy.py
@@ -15,8 +15,14 @@ class DeployError(NamedTuple):
     code: int
 
 
-def deploy(module_with_stub: Path, dry_run: bool, filter_pttrn: Optional[str]) -> Optional[DeployError]:
+def deploy(
+    deployable: bool, module_with_stub: Path, dry_run: bool, filter_pttrn: Optional[str]
+) -> Optional[DeployError]:
     if filter_pttrn and not re.match(filter_pttrn, module_with_stub.name):
+        return None
+
+    if not deployable:
+        print(f"⏩ skipping: app '{module_with_stub.name}' is not marked for deploy")
         return None
 
     deploy_command = f"modal app deploy {module_with_stub.name}"
@@ -51,7 +57,15 @@ def main(argv: Optional[list[str]] = None) -> int:
 
     example_modules = (ex for ex in get_examples() if ex.type == ExampleType.MODULE)
     filter_pttrn = (r".*" + arguments.filter + r".*") if arguments.filter else None
-    results = [deploy(module_with_stub=Path(ex_mod.filename), dry_run=arguments.dry_run, filter_pttrn=filter_pttrn) for ex_mod in example_modules]
+    results = [
+        deploy(
+            deployable=("deploy" in ex_mod.metadata),
+            module_with_stub=Path(ex_mod.filename),
+            dry_run=arguments.dry_run,
+            filter_pttrn=filter_pttrn,
+        )
+        for ex_mod in example_modules
+    ]
 
     failures = [r for r in results if r]
     if any(failures):
