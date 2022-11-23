2e920f027fb4be6863f56d4bbed99358812068c4
Jonathon Belotti <jonathon@modal.com>
2022-11-23 20:37:07 +0000

    fix: expire in-progress transcription jobs

--- ml/whisper_pod_transcriber/api.py
+++ ml/whisper_pod_transcriber/api.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
-from typing import List
+import time
+from typing import List, NamedTuple
 
 from fastapi import FastAPI, Request
 
@@ -16,6 +17,14 @@ from .podcast import coalesce_short_transcript_segments
 
 logger = config.get_logger(__name__)
 web_app = FastAPI()
+
+# A transcription taking > 10 minutes should be exceedingly rare.
+MAX_JOB_AGE_SECS = 10 * 60
+
+
+class InProgressJob(NamedTuple):
+    call_id: str
+    start_time: int
 
 
 @web_app.get("/api/episode/{podcast_id}/{episode_guid_hash}")
@@ -85,15 +94,19 @@ async def podcasts_endpoint(request: Request):
 async def transcribe_job(podcast_id: str, episode_id: str):
     from modal import container_app
 
+    now = int(time.time())
     try:
-        existing_call_id = container_app.in_progress[episode_id]
-        logger.info(f"Found existing call ID {existing_call_id} for episode {episode_id}")
-        return {"call_id": existing_call_id}
+        inprogress_job = container_app.in_progress[episode_id]
+        # NB: runtime type check is to handle present of old `str` values that didn't expire.
+        if isinstance(inprogress_job, tuple) and (now - inprogress_job.start_time) < MAX_JOB_AGE_SECS:
+            existing_call_id = inprogress_job.call_id
+            logger.info(f"Found existing, unexpired call ID {existing_call_id} for episode {episode_id}")
+            return {"call_id": existing_call_id}
     except KeyError:
         pass
 
     call = process_episode.submit(podcast_id, episode_id)
-    container_app.in_progress[episode_id] = call.object_id
+    container_app.in_progress[episode_id] = InProgressJob(call_id=call.object_id, start_time=now)
 
     return {"call_id": call.object_id}
 
