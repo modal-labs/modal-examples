d4f16463cfe11720c32b7b434c8be8463e526329
Jonathon Belotti <jonathon@modal.com>
2022-11-24 03:33:12 +0000

    start with a huggingface ex: https://huggingface.co/docs/transformers/training

--- ml/spam_detection/main.py
+++ ml/spam_detection/main.py
@@ -1,9 +1,16 @@
 import sys
+from datetime import timedelta
+
 import modal
 
 image = modal.Image.debian_slim().pip_install(
     [
+        "datasets~=2.7.1",
+        "evaluate~=0.3.0",
         "loguru~=0.6.0",
+        "scikit-learn~=1.1.3",  # Required by evaluate pkg.
+        "torch~=1.13.0",
+        "transformers~=4.24.0",
     ]
 )
 stub = modal.Stub(name="example-spam-detect-llm", image=image)
@@ -17,12 +24,58 @@ def _get_logger():
     return logger
 
 
-@stub.function
+@stub.function(timeout=int(timedelta(minutes=30).total_seconds()))
 def train():
+    import numpy as np
+    import evaluate
+    from datasets import load_dataset
+    from transformers import AutoModelForSequenceClassification
+    from transformers import AutoTokenizer
+    from transformers import TrainingArguments, Trainer
+
     logger = _get_logger()
 
-    print("\033[{}m> DocSearch: \033[0m{}\033[93m {} records\033[0m".format("96", "https://foo.com", 100))
-    logger.opt(colors=True).info("Ready to detect <fg #9dc100><b>SPAM</b></fg #9dc100> from <fg #ffb6c1><b>HAM</b></fg #ffb6c1>?")
+    logger.opt(colors=True).info(
+        "Ready to detect <fg #9dc100><b>SPAM</b></fg #9dc100> from <fg #ffb6c1><b>HAM</b></fg #ffb6c1>?"
+    )
+
+    dataset = load_dataset("yelp_review_full")
+    dataset["train"][100]
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+    model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=5)
+
+    training_args = TrainingArguments(output_dir="test_trainer")
+
+    metric = evaluate.load("accuracy")
+
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
+
+    small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+    small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=small_train_dataset,
+        eval_dataset=small_eval_dataset,
+        compute_metrics=compute_metrics,
+    )
+
+    logger.opt(colors=True).info("<light-yellow>training</light-yellow> 🏋️")
+
+    trainer.train()
 
 
 if __name__ == "__main__":
