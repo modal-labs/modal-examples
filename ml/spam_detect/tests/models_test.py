b37aa487d623d9ea1dbbf4ab72e8263ebc5756a4
Jonathon Belotti <jonathon.i.belotti@gmail.com>
2022-11-25 15:36:53 -0500

    Add basic test of Naive Bayes model

---
+++ ml/spam_detect/tests/models_test.py
@@ -0,0 +1,37 @@
+import math
+from typing import NamedTuple
+
+from spam_detect import models
+
+
+class Example(NamedTuple):
+    email: str
+    spam: bool
+
+
+def test_prob_calculation():
+    dataset = [
+        Example(email="spam rules", spam=True),
+        Example(email="ham rules", spam=False),
+        Example(email="hello ham", spam=False),
+    ]
+
+    classify_func = models.NaiveBayes().train(dataset)
+    email = "hello spam"
+    probs_if_ham = [
+        (1 + 0.5) / (2 + 2 * 0.5),  # "hello" (present)
+        (0 + 0.5) / (2 + 2 * 0.5),  # "spam" (present)
+        1 - (2 + 0.5) / (2 + 2 * 0.5),  # "ham" (not present)
+        1 - (1 + 0.5) / (2 + 2 * 0.5),  # "rules" (not present)
+    ]
+    probs_if_spam = [
+        (0 + 0.5) / (1 + 2 * 0.5),  # "hello" (present)
+        (1 + 0.5) / (1 + 2 * 0.5),  # "spam" (present)
+        1 - (0 + 0.5) / (1 + 2 * 0.5),  # "ham" (not present)
+        1 - (1 + 0.5) / (1 + 2 * 0.5),  # "rules" (not present)
+    ]
+    p_if_spam = math.exp(sum(math.log(p) for p in probs_if_spam))
+    p_if_ham = math.exp(sum(math.log(p) for p in probs_if_ham))
+
+    # Should be about 0.83
+    assert classify_func(email) == p_if_spam / (p_if_spam + p_if_ham)
