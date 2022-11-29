09e7917fd993dd41f0fb1911fffa538cb03b9356
Erik Bernhardsson <mail@erikbern.com>
2022-11-29 16:38:38 -0500

    Use container lifecycle methods

--- 06_gpu/stable_diffusion_cli.py
+++ 06_gpu/stable_diffusion_cli.py
@@ -93,38 +93,33 @@ stub.image = image
 # 1.6s per generation on average. On a T4, it takes 13s to load and 3.7s per
 # generation. Other optimizations are also available [here](https://huggingface.co/docs/diffusers/optimization/fp16#memory-and-speed).
 
-try:
-    is_inside = stub.is_inside()
-except KeyError:
-    # TODO: Fix this bug
-    is_inside = False
-
-if is_inside:
-    import torch
-    import diffusers
-
-    torch.backends.cudnn.benchmark = True
-    torch.backends.cuda.matmul.allow_tf32 = True
-
-    euler = diffusers.EulerAncestralDiscreteScheduler.from_pretrained(
-        cache_path, subfolder="scheduler", cache_dir=cache_path
-    )
-    PIPE = diffusers.StableDiffusionPipeline.from_pretrained(
-        cache_path, torch_dtype=torch.float16, scheduler=euler, cache_dir=cache_path
-    ).to("cuda")
-    PIPE.enable_xformers_memory_efficient_attention()
-
-
 # This is our Modal function. The function runs through the `StableDiffusionPipeline` pipeline.
 # It sends the PIL image back to our CLI where we save the resulting image in a local file.
 
 
-@stub.function(gpu=modal.gpu.A100())
-def _run_inference(prompt: str, steps: int = 20) -> str:
-    with torch.inference_mode():
-        image = PIPE(prompt, num_inference_steps=steps, guidance_scale=7.0).images[0]
+class StableDiffusion:
+    def __enter__(self):
+        import torch
+        import diffusers
 
-    return image
+        torch.backends.cudnn.benchmark = True
+        torch.backends.cuda.matmul.allow_tf32 = True
+
+        euler = diffusers.EulerAncestralDiscreteScheduler.from_pretrained(
+            cache_path, subfolder="scheduler", cache_dir=cache_path
+        )
+        self.pipe = diffusers.StableDiffusionPipeline.from_pretrained(
+            cache_path, torch_dtype=torch.float16, scheduler=euler, cache_dir=cache_path
+        ).to("cuda")
+        self.pipe.enable_xformers_memory_efficient_attention()
+
+    @stub.function(gpu=modal.gpu.A100())
+    def run_inference(self, prompt: str, steps: int = 20) -> str:
+        import torch
+        with torch.inference_mode():
+            image = self.pipe(prompt, num_inference_steps=steps, guidance_scale=7.0).images[0]
+
+        return image
 
 
 # This is the command we'll use to generate images. It takes a `prompt`,
@@ -141,9 +136,10 @@ def entrypoint(prompt: str, samples: int = 10, steps: int = 20):
         dir.mkdir(exist_ok=True, parents=True)
 
     with stub.run():
+        sd = StableDiffusion()
         for i in range(samples):
             t0 = time.time()
-            image = _run_inference(prompt, steps)
+            image = sd.run_inference(prompt, steps)
             output_path = dir / f"output_{i}.png"
             print(f"Sample {i} took {time.time()-t0:.3f}s. Saving it to {output_path}")
             image.save(output_path)
