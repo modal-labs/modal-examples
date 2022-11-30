a7edb578263c40281286566de2f248847823c3f7
Elias Freider <elias@modal.com>
2022-11-30 13:51:30 +0100

    lint

--- data/meltano/meltano_modal.py
+++ data/meltano/meltano_modal.py
@@ -11,17 +11,20 @@ def meltano_install():
     os.environ["MELTANO_PROJECT_ROOT"] = "/meltano_project"
     subprocess.call(["meltano", "install"])
 
+
 # TODO: easier way to add build context from local directory, which also invalidates on file changes
 meltano_source_mount = modal.Mount(
     local_dir=Path(__file__).parent / "meltano_project",
     remote_dir="/meltano_source",
-    condition=lambda path: not any(p.startswith(".") for p in Path(path).parts)
+    condition=lambda path: not any(p.startswith(".") for p in Path(path).parts),
 )
 
-meltano_img = modal.Image.debian_slim() \
-    .apt_install(["git"]) \
-    .pip_install(["meltano"]) \
+meltano_img = (
+    modal.Image.debian_slim()
+    .apt_install(["git"])
+    .pip_install(["meltano"])
     .run_function(meltano_install, mounts=[meltano_source_mount])
+)
 
 
 stub = modal.Stub(image=meltano_img)
@@ -30,7 +33,12 @@ stub = modal.Stub(image=meltano_img)
 db_volume = modal.SharedVolume().persist("meltano_db")
 db_path = Path("/meltano_db_volume/meltano.db")
 
-@stub.wsgi(image=meltano_img, shared_volumes={"/meltano_db_volume": db_volume}, secrets=[modal.Secret.from_name("meltano-secrets")])
+
+@stub.wsgi(
+    image=meltano_img,
+    shared_volumes={"/meltano_db_volume": db_volume},
+    secrets=[modal.Secret.from_name("meltano-secrets")],
+)
 def meltano_ui():
     # init database if it doesn't exist
     if not db_path.exists():
@@ -48,10 +56,16 @@ def meltano_ui():
     os.environ["MELTANO_PROJECT_READONLY"] = "true"
     os.environ["MELTANO_DATABASE_URI"] = f"sqlite:///{db_path}"
     import meltano.api.app
+
     return meltano.api.app.create_app()
 
 
-@stub.function(image=meltano_img, schedule=modal.Period(days=1), shared_volumes={"/meltano_db_volume": db_volume}, secrets=[modal.Secret.from_name("meltano-secrets")])
+@stub.function(
+    image=meltano_img,
+    schedule=modal.Period(days=1),
+    shared_volumes={"/meltano_db_volume": db_volume},
+    secrets=[modal.Secret.from_name("meltano-secrets")],
+)
 def daily_ingest():
     os.environ["MELTANO_PROJECT_ROOT"] = "/meltano_project"
     os.environ["MELTANO_PROJECT_READONLY"] = "true"
