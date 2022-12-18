b20317fd0fd61f17040066de193afcbb6e66c316
Akshat Bubna <akshatb42@gmail.com>
2022-12-18 22:29:49 +0000

    webapp build

--- 06_gpu_and_ml/riffusion_server.py
+++ 06_gpu_and_ml/riffusion_server.py
@@ -1,29 +1,63 @@
 import modal
 
-RIFFUSION_PATH = "/root/riffusion"
+RIFFUSION_PKG_PATH = "/root/riffusion"
 
-image = (
+inference_image = (
     modal.Image.debian_slim()
     .apt_install(["git"])
     .run_commands(
         [
-            f"git clone https://github.com/hmartiro/riffusion-inference {RIFFUSION_PATH}",
-            f"pip install -r {RIFFUSION_PATH}/requirements.txt",
+            f"git clone https://github.com/hmartiro/riffusion-inference {RIFFUSION_PKG_PATH}",
+            f"pip install -r {RIFFUSION_PKG_PATH}/requirements.txt",
         ]
     )
 )
 
-stub = modal.Stub("example-riffusion", image=image)
+stub = modal.Stub("example-riffusion")
 
 
-@stub.wsgi()
-def server():
-    import sys
+class Riffusion:
+    def __enter__(self):
+        print("loading")
+        import sys
 
-    sys.path.insert(0, RIFFUSION_PATH)
-    from riffusion.server import app
+        # HACK since riffusion is not a package, but we want to use it like one.
+        sys.path.insert(0, RIFFUSION_PKG_PATH)
+        from riffusion.server import load_model
 
-    return app
+        load_model()
+        print("loaded")
+
+    @stub.wsgi(image=inference_image)
+    def server(self):
+        from riffusion.server import app
+
+        return app
+
+
+# web_image = (
+#     modal.Image.debian_slim()
+#     .apt_install(["git", "curl"])
+#     # Install npm
+#     .run_commands(
+#         [
+#             "curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash",
+#             ". $HOME/.nvm/nvm.sh && nvm install 16.14.2",
+#         ]
+#     )
+#     .run_commands(
+#         [
+#             f"git clone https://github.com/hmartiro/riffusion-app.git ~/riffusion-app",
+#             ". $HOME/.nvm/nvm.sh && cd ~/riffusion-app && npm install && next build && next export",
+#         ]
+#     )
+# )
+
+# @stub.wsgi(image=web_image)
+# def web_server():
+#     from riffusion.server import app
+
+#     return app
 
 
 if __name__ == "__main__":
