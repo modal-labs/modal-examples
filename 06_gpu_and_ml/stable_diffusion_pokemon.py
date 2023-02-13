e0b4c4547a925bec9cb831480841bc6b9e891eff
Akshat Bubna <akshatb42@gmail.com>
2023-02-13 21:17:00 +0000

    use apt_install; don't copy cloned repo; use 4 A10Gs

--- 06_gpu_and_ml/stable_diffusion_pokemon.py
+++ 06_gpu_and_ml/stable_diffusion_pokemon.py
@@ -1,6 +1,5 @@
 # Follows https://lambdalabs.com/blog/how-to-fine-tune-stable-diffusion-how-we-made-the-text-to-pokemon-model-at-lambda to fine-tune stable diffusion for Pokemon
 
-from pathlib import Path
 
 from fastapi import FastAPI
 
@@ -9,13 +8,16 @@ import modal
 web_app = FastAPI()
 stub = modal.Stub(name="stable-diffusion-fine-tune-pokemon")
 
+REPO_PATH = "/stable-diffusion"
+MODEL_DIR = "/model"
+
 image = (
     modal.Image.debian_slim()
+    .apt_install("git", "wget", "libgl1", "libglib2.0-0")
     .run_commands(
         [
-            "apt-get install -y git wget libgl1 libglib2.0-0",
-            "git clone https://github.com/justinpinkney/stable-diffusion.git repo",
-            "cd repo && pip install -r requirements.txt",
+            f"git clone https://github.com/justinpinkney/stable-diffusion.git {REPO_PATH}",
+            f"cd {REPO_PATH} && pip install -r requirements.txt",
         ]
     )
     .pip_install("gradio~=3.10")
@@ -24,23 +26,18 @@ image = (
 # A persistent shared volume will store model artefacts across Modal app runs.
 # This is crucial as finetuning runs are separate from the Gradio app we run as a webhook.
 
-volume = modal.SharedVolume().persist("stable-diffusion-pokemon")
-MODEL_DIR = Path("/stable-diffusion")
+volume = modal.SharedVolume().persist("sd-pokemon")
 
 
 @stub.function(
     image=image,
-    gpu="A100",  # finetuning is VRAM hungry, so this should be an A100
-    shared_volumes={
-        str(MODEL_DIR): volume,  # fine-tuned model will be stored at `MODEL_DIR`
-    },
+    gpu=modal.gpu.A10G(count=4),
+    # fine-tuned model will be stored at `MODEL_DIR`
+    shared_volumes={MODEL_DIR: volume},
     timeout=5 * 60 * 60,  # 5 hours
 )
 def train():
     import subprocess
-
-    print("Copying training repo")
-    subprocess.check_call(["cp", "-R", "repo", "stable-diffusion"], cwd=Path("/"))
 
     print("Downloading model")
     subprocess.check_call(
@@ -61,7 +58,7 @@ def train():
             "--base",
             "configs/stable-diffusion/pokemon.yaml",
             "--gpus",
-            ",0",
+            "0,1,2,3",
             "--scale_lr",
             "False",
             "--num_nodes",
@@ -87,20 +84,21 @@ def train():
 
 @stub.asgi(
     image=image,
-    gpu="A100",
+    gpu="A10G",
     shared_volumes={str(MODEL_DIR): volume},
 )
 def fastapi_app():
+    import os
+
     import gradio as gr
     from gradio.routes import mount_gradio_app
-
-    import os
 
     # take the latest one in logs
     logs_dir = max(os.listdir(MODEL_DIR / "logs"))
     checkpoint = "last.ckpt"
     go = prepare_model(
-        MODEL_DIR / "configs/stable-diffusion/pokemon.yaml", MODEL_DIR / f"logs/{logs_dir}/checkpoints/{checkpoint}"
+        MODEL_DIR / "configs/stable-diffusion/pokemon.yaml",
+        MODEL_DIR / f"logs/{logs_dir}/checkpoints/{checkpoint}",
     )
 
     # add a gradio UI around inference
@@ -145,13 +143,13 @@ def load_model_from_config(config, ckpt, verbose=False):
 
 # from https://github.com/justinpinkney/stable-diffusion/blob/main/scripts/txt2img.py
 def prepare_model(config_file, ckpt_file):
-    from pytorch_lightning import seed_everything
-    from omegaconf import OmegaConf
-    import torch
-    from ldm.models.diffusion.ddim import DDIMSampler
-    from einops import rearrange
-    from PIL import Image
     import numpy as np
+    import torch
+    from einops import rearrange
+    from ldm.models.diffusion.ddim import DDIMSampler
+    from omegaconf import OmegaConf
+    from PIL import Image
+    from pytorch_lightning import seed_everything
 
     C, H, W, f = 4, 512, 512, 8
 
@@ -178,9 +176,13 @@ def prepare_model(config_file, ckpt_file):
                     x_T=start_code,
                 )
                 x_samples_ddim = model.decode_first_stage(samples_ddim)
-                x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                x_samples_ddim = torch.clamp(
+                    (x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0
+                )
                 x_sample = x_samples_ddim[0]
-                x_sample = 255.0 * rearrange(x_sample.cpu().numpy(), "c h w -> h w c")
+                x_sample = 255.0 * rearrange(
+                    x_sample.cpu().numpy(), "c h w -> h w c"
+                )
                 return Image.fromarray(x_sample.astype(np.uint8))
 
     return go
