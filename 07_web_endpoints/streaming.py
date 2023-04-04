c06180ec58896fdc1a89958ca43e3b17c66ae3b1
Jonathon Belotti <jonathon@modal.com>
2023-04-04 14:05:37 -0400

    Extend basic streaming example mod (#267)

--- 07_web_endpoints/streaming.py
+++ 07_web_endpoints/streaming.py
@@ -3,6 +3,7 @@
 # deploy: true
 # ---
 import asyncio
+import time
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 
@@ -12,7 +13,7 @@ stub = modal.Stub("example-fastapi-streaming")
 
 web_app = FastAPI()
 
-# This is 'faker' asynchronous generator function simulates
+# This asynchronous generator function simulates
 # progressively returning data to the client. The `asyncio.sleep`
 # is not necessary, but makes it easier to see the iterative behavior
 # of the response.
@@ -37,11 +38,31 @@ def fastapi_app():
     return web_app
 
 
+@stub.function()
+def sync_fake_video_streamer():
+    for i in range(10):
+        yield f"frame {i}: some data\n".encode()
+        time.sleep(1)
+
+
+@stub.function()
+@stub.web_endpoint()
+def hook():
+    # `iter()` is used because `.call` returns an iterable object but not in Iterator,
+    # so the `StreamingResponse` can't call `next()` on it.
+    # `iter()` produces an Iterator from an iterable.
+    #
+    # See: https://docs.python.org/3/library/stdtypes.html#typeiter
+    return StreamingResponse(
+        iter(sync_fake_video_streamer.call()), media_type="text/event-stream"
+    )
+
+
 # This is a very basic 'hello world' example of a webhook streaming response.
 #
 #
 # ```
-# modal server streaming.py
+# modal serve streaming.py
 # ```
 #
 # To try out the webhook, ensure that your client is not buffering the server response
@@ -50,4 +71,5 @@ def fastapi_app():
 #
 # ```shell
 # curl --no-buffer https://modal-labs--example-fastapi-streaming-fastapi-app.modal.run
+# curl --no-buffer https://modal-labs--example-fastapi-streaming-hook.modal.run
 # ````
