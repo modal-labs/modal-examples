4a3cc421c80005660770f35f3c52d7cd992b4b95
Erik Bernhardsson <mail@erikbern.com>
2023-04-04 21:02:30 -0400

    Remove deprecated syntax

--- 09_job_queues/doc_ocr_jobs.py
+++ 09_job_queues/doc_ocr_jobs.py
@@ -95,7 +95,7 @@ def parse_receipt(image: bytes):
 # Python process and submit tasks to it:
 #
 # ```python
-# fn = modal.lookup("example-doc-ocr-jobs", "parse_receipt")
+# fn = modal.Function.lookup("example-doc-ocr-jobs", "parse_receipt")
 # fn.spawn(my_image)
 # ```
 #
