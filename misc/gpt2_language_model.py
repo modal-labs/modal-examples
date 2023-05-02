18258de76a610e7b81a1081fe1a6203fecd76f1f
Jonathon Belotti <jonathon@modal.com>
2023-05-01 21:13:01 -0400

    Update GPT-2 example code (#300)

--- misc/gpt2_language_model.py
+++ misc/gpt2_language_model.py
@@ -5,20 +5,34 @@ import modal
 
 stub = modal.Stub("example-gpt2")
 
-volume = modal.SharedVolume().persist("gpt2")
-
+# volume = modal.SharedVolume().persist("gpt2")
 CACHE_PATH = "/root/model_cache"
 
 
+def download_model():
+    from transformers import pipeline
+
+    generator = pipeline("text-generation", model="gpt2")
+    generator.save_pretrained(CACHE_PATH)
+
+
 @stub.function(
-    image=modal.Image.debian_slim().pip_install("torch", "transformers"),
-    shared_volumes={CACHE_PATH: volume},
-    secret=modal.Secret({"TRANSFORMERS_CACHE": CACHE_PATH}),
+    image=modal.Image.debian_slim()
+    .pip_install("torch", "transformers")
+    .run_function(download_model),
 )
 def generate_text(prompt: str):
     from transformers import pipeline
 
-    generator = pipeline("text-generation", model="gpt2")
+    # NOTE: This model load runs on every function invocation. It's more efficient
+    # to use lifecycle methods: modal.com/docs/guide/lifecycle-functions#container-lifecycle-beta.
+    generator = pipeline("text-generation", model=CACHE_PATH)
     return generator(prompt, do_sample=True, min_length=50, max_length=250)[0][
         "generated_text"
     ]
+
+
+@stub.local_entrypoint()
+def main(prompt: str = ""):
+    generation = generate_text.call(prompt=prompt or "Show me the meaning of")
+    print(generation)
