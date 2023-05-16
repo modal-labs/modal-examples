098c234b1b1cd6303eb0136d44caaf96f9f416f5
Jonathon Belotti <jonathon@modal.com>
2023-05-16 03:05:50 +0000

    stop writing inline DBT profile

--- 10_integrations/dbt/dbt_duckdb.py
+++ 10_integrations/dbt/dbt_duckdb.py
@@ -5,40 +5,6 @@ import modal
 
 # this must match the bucket source in `sample_proj_duckdb_s3/sources.yml`
 BUCKET_NAME = "example-dbt-duckdb-s3"
-
-
-def create_inline_dbt_profile_mount(remote_path: Path) -> modal.Mount:
-    """
-    Writes an inline DBT profile to a temporary file that's
-    return as a modal.Mount.
-    """
-    import tempfile
-
-    dbt_profile = {
-        "sample_proj": {
-            "target": "modal",
-            "outputs": {
-                "modal": {
-                    "type": "duckdb",
-                    "path": "/tmp/dbt.duckdb",
-                    "extensions": ["httpfs", "parquet"],
-                    "settings": {
-                        "s3_region": "us-east-1",
-                        "s3_access_key_id": "{{ env_var('AWS_ACCESS_KEY_ID') }}",
-                        "s3_secret_access_key": "{{ env_var('AWS_SECRET_ACCESS_KEY') }}",
-                    },
-                }
-            },
-        }
-    }
-    temp_profile_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_profile_file.write(json.dumps(dbt_profile).encode())
-    return modal.Mount.from_local_file(
-        local_path=temp_profile_file.name,
-        remote_path=Path(remote_path, "profiles.yml"),
-    )
-
-
 LOCAL_DBT_PROJECT = Path(__file__).parent / "sample_proj_duckdb_s3"
 PROJ_PATH = "/root/dbt"
 PROFILES_PATH = "/root/dbt_profile"
@@ -63,7 +29,17 @@ stub = modal.Stub(name="example-dbt-duckdb-s3", image=dbt_image)
 dbt_project = modal.Mount.from_local_dir(
     LOCAL_DBT_PROJECT, remote_path=PROJ_PATH
 )
+dbt_profiles = modal.Mount.from_local_file(
+    local_path=LOCAL_DBT_PROJECT / "profiles.yml",
+    remote_path=Path(PROFILES_PATH, "profiles.yml"),
+)
 dbt_target = modal.SharedVolume().persist("dbt-target")
+# Create this secret using the "AWS" template at https://modal.com/secrets/create.
+# Be sure that the AWS user you provide credentials for has permission to
+# create S3 buckets and read/write data from them.
+#
+# Below we will use a Modal function to create an S3 bucket and populate it with
+# .parquet data.
 s3_secret = modal.Secret.from_name("personal-aws-user")
 
 
@@ -93,10 +69,7 @@ def seed():
 @stub.function(
     schedule=modal.Period(days=1),
     secrets=[s3_secret],
-    mounts=[
-        dbt_project,
-        create_inline_dbt_profile_mount(remote_path=PROFILES_PATH),
-    ],
+    mounts=[dbt_project, dbt_profiles],
     shared_volumes={TARGET_PATH: dbt_target},
 )
 def daily_build() -> None:
@@ -105,10 +78,7 @@ def daily_build() -> None:
 
 @stub.function(
     secrets=[s3_secret],
-    mounts=[
-        dbt_project,
-        create_inline_dbt_profile_mount(remote_path=PROFILES_PATH),
-    ],
+    mounts=[dbt_project, dbt_profiles],
     shared_volumes={TARGET_PATH: dbt_target},
 )
 def run(command: str) -> None:
