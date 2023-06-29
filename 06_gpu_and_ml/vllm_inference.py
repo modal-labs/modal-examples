cb6e2b0014c17044d99a56f639dd37390cd96926
gongy <richard@modal.com>
2023-06-29 18:35:04 +0000

    lint

--- 06_gpu_and_ml/vllm_inference.py
+++ 06_gpu_and_ml/vllm_inference.py
@@ -17,8 +17,9 @@
 
 from modal import Stub, Image, method
 
+
 # We now want to create a Modal image which has the model weights pre-saved. The benefit of this
-# is that the container no longer has to re-download the model from Huggingface - instead, it will take 
+# is that the container no longer has to re-download the model from Huggingface - instead, it will take
 # advantage of Modal's internal filesystem for faster cold starts.
 #
 # Since `vLLM` uses the default Huggingface cache location, so we can use library functions to pre-download the model into our image.
@@ -28,15 +29,20 @@ def download_model():
     snapshot_download("lmsys/vicuna-13b-v1.3")
     snapshot_download("hf-internal-testing/llama-tokenizer")
 
+
 image = (
-    Image
-    .from_dockerhub("nvcr.io/nvidia/pytorch:22.12-py3")
-    .pip_install("torch==2.0.1", index_url="https://download.pytorch.org/whl/cu118")
-    .pip_install("vllm @ git+https://github.com/vllm-project/vllm.git@2b7d3aca2e1dd25fe26424f57c051af3b823cd71")
+    Image.from_dockerhub("nvcr.io/nvidia/pytorch:22.12-py3")
+    .pip_install(
+        "torch==2.0.1", index_url="https://download.pytorch.org/whl/cu118"
+    )
+    .pip_install(
+        "vllm @ git+https://github.com/vllm-project/vllm.git@2b7d3aca2e1dd25fe26424f57c051af3b823cd71"
+    )
     .run_function(download_model)
 )
 
 stub = Stub(image=image)
+
 
 # ## The model class
 #
@@ -49,18 +55,25 @@ class Model:
     def __enter__(self):
         from vllm import LLM
 
-        self.llm = LLM(model="lmsys/vicuna-13b-v1.3") # Load the model
-        self.template = "You are a helpful assistant.\n\n### USER:\n{}\n### ASSISTANT:\n"
-    
+        self.llm = LLM(model="lmsys/vicuna-13b-v1.3")  # Load the model
+        self.template = (
+            "You are a helpful assistant.\n\n### USER:\n{}\n### ASSISTANT:\n"
+        )
+
     @method()
     def generate(self, user_questions):
         from vllm import SamplingParams
 
         prompts = [self.template.format(q) for q in user_questions]
-        sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=512)
-        result = self.llm.generate(prompts, sampling_params) # Trigger inference
+        sampling_params = SamplingParams(
+            temperature=0.8, top_p=0.95, max_tokens=512
+        )
+        result = self.llm.generate(
+            prompts, sampling_params
+        )  # Trigger inference
         for output in result:
             print(output.prompt, output.outputs[0].text, "\n\n", sep="")
+
 
 # ## Run the model
 # We define a [`local_entrypoint`](/docs/guide/apps#entrypoints-for-ephemeral-apps) to call our remote function
@@ -68,8 +81,10 @@ class Model:
 @stub.local_entrypoint()
 def main():
     model = Model()
-    model.generate.call([
-        "Implement a Python function to compute the Fibonacci numbers.",
-        "Write a Rust function that performs fast exponentiation.",
-        "What is the fable involving a fox and grapes?"
-    ])
+    model.generate.call(
+        [
+            "Implement a Python function to compute the Fibonacci numbers.",
+            "Write a Rust function that performs fast exponentiation.",
+            "What is the fable involving a fox and grapes?",
+        ]
+    )
