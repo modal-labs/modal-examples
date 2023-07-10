3ad1a36fb15cd8e4faf25e7b48a2e2238a460a46
Jonathon Belotti <jonathon@modal.com>
2023-07-10 01:48:45 +0000

    support inference against arbitrary model at runtime

--- 06_gpu_and_ml/spam-detect/spam_detect/serving.py
+++ 06_gpu_and_ml/spam-detect/spam_detect/serving.py
@@ -13,11 +13,6 @@ from .app import stub, volume
 
 web_app = FastAPI()
 
-if stub.is_inside():
-    classifier, metadata = models.load_model(model_id=config.SERVING_MODEL_ID)
-else:
-    classifier, metadata = None, None
-
 
 class ModelInput(BaseModel):
     text: str
@@ -34,18 +29,41 @@ class ModelOutput(BaseModel):
     metadata: ModelMetdata
 
 
+# TODO(Jonathon): This will acquire a GPU even when `model_id` doesn't
+# require it, which is inefficient. Find an elegant way to make the GPU optional.
+@stub.cls(gpu="A10G", volumes={config.VOLUME_DIR: volume})
+class Model:
+    def __init__(self, model_id: str) -> None:
+        self.model_id = model_id
+        classifier, metadata = models.load_model(model_id=self.model_id)
+        self.classifier = classifier
+        self.metadata = metadata
+
+    @modal.method()
+    def generate(self, text: str) -> ModelOutput:
+        prediction = self.classifier(text)
+        return ModelOutput(
+            spam=prediction.spam,
+            score=prediction.score,
+            metadata=ModelMetdata(
+                model_name=self.metadata.impl_name,
+                model_id=self.model_id,
+            ),
+        )
+
+
 @web_app.get("/api/v1/models")
-async def handle_list_models(user_agent: Optional[str] = Header(None)):
+async def handle_list_models():
     """
     Show details of actively serving models.
     """
+    _, metadata = models.load_model(config.SERVING_MODEL_ID)
     return {config.SERVING_MODEL_ID: metadata.serialize()}
 
 
-# TODO(Jonathon): Adopt lifeycle method structure when it's supported for modal.asgi_app.
 @web_app.post("/api/v1/classify")
 async def handle_classification(
-    input_: ModelInput, user_agent: Optional[str] = Header(None)
+    input_: ModelInput, model_id: Optional[str] = Header(None)
 ):
     """
     Classify a body of text as spam or ham.
@@ -55,21 +73,17 @@ async def handle_classification(
     ```bash
     curl -X POST https://modal-labs--example-spam-detect-llm-web.modal.run/api/v1/classify \ 
     -H 'Content-Type: application/json' \
+    -H 'Model-Id: sha256.12E5065BE4C3F7D2F79B7A0FD203380869F6E308DCBB4B8C9579FFAE6F32B837' \
     -d '{"text": "hello world"}'
     ```
     """
-    prediction = classifier(input_.text)
-    return ModelOutput(
-        spam=prediction.spam,
-        score=prediction.score,
-        metadata=ModelMetdata(
-            model_name=metadata.impl_name,
-            model_id=config.SERVING_MODEL_ID,
-        ),
-    )
+    model_id = model_id or config.SERVING_MODEL_ID
+    print(model_id)
+    model = Model.remote(model_id)
+    return model.generate(input_.text)
 
 
-@stub.function(volumes={config.VOLUME_DIR: volume})
+@stub.function()
 @modal.asgi_app()
 def web():
     return web_app
