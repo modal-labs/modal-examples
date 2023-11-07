50d2061dcdbe3f5a43b5c29ad1413f95a955104c
Jonathon Belotti <jonathon@modal.com>
2023-11-07 21:54:01 +0000

    Deployable, shareable Streamlit app with session timeout info

--- 10_integrations/streamlit/app.py
+++ 10_integrations/streamlit/app.py
@@ -1,36 +1,57 @@
-import streamlit as st
-import pandas as pd
+import argparse
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
 import numpy as np
+import pandas as pd
+import streamlit as st
 
-st.title('Uber pickups in NYC!')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--timeout", type=int, default=300)
+    args = parser.parse_args()
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    st.title('Uber pickups in NYC!')
+    now = datetime.now(ZoneInfo('UTC'))
+    dt_format = "%B %d, %Y, %I:%M %p (%Z)"
+    end_of_session = now + timedelta(seconds=args.timeout)
+    st.markdown(
+        f"⌛️ Streamlit session will time out at **:orange[{end_of_session.strftime(dt_format)}]**. "
+        f"Session start time was {now.strftime(dt_format)}."
+    )
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+    DATE_COLUMN = 'date/time'
+    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+    @st.cache_data
+    def load_data(nrows):
+        data = pd.read_csv(DATA_URL, nrows=nrows)
+        def lowercase(x):
+            return str(x).lower()
+        data.rename(lowercase, axis='columns', inplace=True)
+        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+    data_load_state = st.text('Loading data...')
+    data = load_data(10000)
+    data_load_state.text("Done! (using st.cache_data)")
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
+
+    # Some number in the range 0-23
+    hour_to_filter = st.slider('hour', 0, 23, 17)
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+    st.map(filtered_data)
+
+if __name__ == "__main__":
+    main()
--- 10_integrations/streamlit/serve_app.py
+++ 10_integrations/streamlit/serve_app.py
@@ -1,38 +1,38 @@
-import asyncio
-import os
 import pathlib
 
 import modal
 
 image = modal.Image.debian_slim().pip_install("streamlit", "numpy", "pandas")
 stub = modal.Stub(name="example-modal-streamlit", image=image)
+stub.q = modal.Queue.new()
+
+session_timeout = 15 * 60
 
 @stub.function(
     mounts=[
         modal.Mount.from_local_file(pathlib.Path(__file__).parent / "app.py", remote_path="/root/app.py")
     ],
 )
-def f():
-    from streamlit.web.bootstrap import _on_server_start, _set_up_signal_handler, load_config_options
-    from streamlit.web.server import Server
-
-    # Create the server. It won't start running yet.
-    server = Server("/root/app.py", None)
-
-    async def run_server() -> None:
-        # Start the server
-        await server.start()
-        _on_server_start(server)
-
-        # Install a signal handler that will shut down the server
-        # and close all our threads
-        _set_up_signal_handler(server)
-
-        # Wait until `Server.stop` is called, either by our signal handler, or
-        # by a debug websocket session.
-        await server.stopped
-
+def run_streamlit(publish_url: bool = False):
+    from streamlit.web.bootstrap import load_config_options, run
     # Run the server. This function will not return until the server is shut down.
     with modal.forward(8501) as tunnel:
+        # Reload Streamlit config with information about Modal tunnel address.
+        if publish_url:
+            stub.q.put(tunnel.url)
         load_config_options({"browser.serverAddress": tunnel.host, "browser.serverPort": 443})
-        asyncio.run(run_server())
+        run(
+            main_script_path="/root/app.py",
+            command_line=None,
+            args=["--timeout", str(session_timeout)],
+            flag_options={},
+        )
+
+
+@stub.function()
+@modal.web_endpoint(method="GET")
+def spawn():
+    from fastapi.responses import RedirectResponse
+    run_streamlit.spawn(publish_url=True)
+    url = stub.q.get()
+    return RedirectResponse(url, status_code=303)
