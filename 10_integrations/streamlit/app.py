f10b4fe49ad3b090f908c565ed019c009e5158a2
Akshat Bubna <akshatb42@gmail.com>
2023-12-14 02:42:24 +0000

    fix

--- 10_integrations/streamlit/app.py
+++ 10_integrations/streamlit/app.py
@@ -1,13 +1,9 @@
 # ---
 # lambda-test: false
 # ---
-from datetime import datetime
-from zoneinfo import ZoneInfo
-
 # ## Demo Streamlit application.
 #
-# This application is the example from https://docs.streamlit.io/library/get-started/create-an-app
-# with a minor modification to show the session timeout time to users.
+# This application is the example from https://docs.streamlit.io/library/get-started/create-an-app.
 #
 # Streamlit is designed to run its apps as Python scripts, not functions, so we separate the Streamlit
 # code into this module, away from the Modal application code.
@@ -18,17 +14,7 @@ def main():
     import pandas as pd
     import streamlit as st
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--timeout", type=int, default=300)
-    # args = parser.parse_args()
-
     st.title("Uber pickups in NYC!")
-    datetime.now(ZoneInfo("UTC"))
-    # end_of_session = now + timedelta(seconds=args.timeout)
-    # st.markdown(
-    #     f"⌛️ Streamlit session will time out at **:orange[{end_of_session.strftime(dt_format)}]**. "
-    #     f"Session start time was {now.strftime(dt_format)}."
-    # )
 
     DATE_COLUMN = "date/time"
     DATA_URL = (
@@ -67,8 +53,6 @@ def main():
 
     st.subheader("Map of all pickups at %s:00" % hour_to_filter)
     st.map(filtered_data)
-
-    return st
 
 
 if __name__ == "__main__":
