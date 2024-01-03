030327fb025121ca8291a205d69bc5f56f0d52e1
Eric Zhang <ekzhang1@gmail.com>
2024-01-03 18:47:09 +0000

    Fix dreambooth_app

--- 06_gpu_and_ml/dreambooth/dreambooth_app.py
+++ 06_gpu_and_ml/dreambooth/dreambooth_app.py
@@ -77,7 +77,7 @@ image = (
 # This is crucial as finetuning runs are separate from the Gradio app we run as a webhook.
 
 volume = Volume.persisted("dreambooth-finetuning-volume")
-MODEL_DIR = Path("/model")
+MODEL_DIR = "/model"
 
 # ## Config
 #
@@ -138,21 +138,21 @@ class AppConfig(SharedConfig):
 # So we can fetch just a few images, stored on consumer platforms like Imgur or Google Drive
 # -- no need for expensive data collection or data engineering.
 
-IMG_PATH = Path("/img")
 
-
-def load_images(image_urls):
+def load_images(image_urls: list[str]) -> Path:
     import PIL.Image
     from smart_open import open
 
-    os.makedirs(IMG_PATH, exist_ok=True)
+    img_path = Path("/img")
+
+    img_path.mkdir(parents=True, exist_ok=True)
     for ii, url in enumerate(image_urls):
         with open(url, "rb") as f:
             image = PIL.Image.open(f)
-            image.save(IMG_PATH / f"{ii}.png")
+            image.save(img_path / f"{ii}.png")
     print("Images loaded.")
 
-    return IMG_PATH
+    return img_path
 
 
 # ## Finetuning a text-to-image model
@@ -183,24 +183,21 @@ def load_images(image_urls):
 # `modal run dreambooth_app.py::stub.train`.
 # It should take about ten minutes.
 #
-# Tip: if the results you're seeing don't match the prompt too well, and instead produce an image of your subject again, the model has likely overfit. In this case, repeat training with a lower # of max_train_steps. On the other hand, if the results don't look like your subject, you might need to increase # of max_train_steps.
+# Tip: if the results you're seeing don't match the prompt too well, and instead produce an image
+# of your subject again, the model has likely overfit. In this case, repeat training with a lower
+# value of `max_train_steps`. On the other hand, if the results don't look like your subject, you
+# might need to increase `max_train_steps`.
 
 
 @stub.function(
     image=image,
-    gpu="A100",  # finetuning is VRAM hungry, so this should be an A100
-    volumes={
-        str(
-            MODEL_DIR
-        ): volume,  # fine-tuned model will be stored at `MODEL_DIR`
-    },
+    gpu="A100",  # fine-tuning is VRAM-heavy and requires an A100 GPU
+    volumes={MODEL_DIR: volume},  # stores fine-tuned model
     timeout=1800,  # 30 minutes
-    secrets=[Secret.from_name("huggingface")],
 )
 def train(instance_example_urls):
     import subprocess
 
-    import huggingface_hub
     from accelerate.utils import write_basic_config
     from transformers import CLIPTokenizer
 
@@ -213,10 +210,6 @@ def train(instance_example_urls):
 
     # set up hugging face accelerate library for fast training
     write_basic_config(mixed_precision="fp16")
-
-    # authenticate to hugging face so we can download the model weights
-    hf_key = os.environ["HUGGINGFACE_TOKEN"]
-    huggingface_hub.login(hf_key)
 
     # check whether we can access to model repo
     try:
@@ -283,7 +276,7 @@ def train(instance_example_urls):
 @stub.cls(
     image=image,
     gpu="A100",
-    volumes={str(MODEL_DIR): volume},
+    volumes={MODEL_DIR: volume},
 )
 class Model:
     def __enter__(self):
