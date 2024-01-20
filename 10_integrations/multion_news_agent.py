cae4140271328ad07e2634e4a5d1901c216b083e
Divyansh Garg <Div99@users.noreply.github.com>
2024-01-20 21:51:02 +0000

    fixes to docs

--- 10_integrations/multion_news_agent.py
+++ 10_integrations/multion_news_agent.py
@@ -31,7 +31,7 @@ multion_image = modal.Image.debian_slim().pip_install("multion")
 # [MultiOn](https://multion.ai/) is a next-gen Web Action Agent that can take actions on behalf of the user. You can watch it in action here: [Youtube demo](https://www.youtube.com/watch?v=Rm67ry6bogw).
 #
 # The MultiOn API enables building the next level of web automation & custom AI agents capable of performing complex actions on the internet with just a few lines of code.
-
+#
 # To get started, first create an account with [MultiOn](https://app.multion.ai/), install the [MultiOn chrome extension](https://chrome.google.com/webstore/detail/ddmjhdbknfidiopmbaceghhhbgbpenmm) and login to your Twitter account in your browser.
 # To use the API create a [MultiOn API Key](https://app.multion.ai/api-keys) and store it as a modal secret on [the dashboard](https://modal.com/div99/secrets)
 
