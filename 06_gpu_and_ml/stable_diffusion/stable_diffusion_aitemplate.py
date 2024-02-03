eaaf0d984a50b774b1d7078f0e0d435f4b0abf38
Michael Waskom <michael@modal.com>
2024-02-02 19:07:37 -0500

    Run black

--- 06_gpu_and_ml/stable_diffusion/stable_diffusion_aitemplate.py
+++ 06_gpu_and_ml/stable_diffusion/stable_diffusion_aitemplate.py
@@ -224,9 +224,7 @@ stub = modal.Stub("example-stable-diffusion-aitemplate")
 
 
 class InferenceRequest(BaseModel):
-    prompt: str = (
-        "photo of a wolf in the snow, blue eyes, highly detailed, 8k, 200mm canon lens, shallow depth of field"
-    )
+    prompt: str = "photo of a wolf in the snow, blue eyes, highly detailed, 8k, 200mm canon lens, shallow depth of field"
     num_inference_steps: int = 10
     guidance_scale: float = 7.5
     negative_prompt: str = "deformed, extra legs, no tail"
