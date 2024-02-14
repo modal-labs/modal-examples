0bffafd3f33af292341c00ba16ec223a491fa1d4
Charles Frye <charles@modal.com>
2024-02-14 17:41:29 +0000

    more cleanup on hello_world

--- 01_getting_started/hello_world.py
+++ 01_getting_started/hello_world.py
@@ -1,10 +1,10 @@
 # # Hello, world!
 #
-# This tutorial demonstrates some core features of Modal `Function`s:
+# This tutorial demonstrates some core features of Modal:
 #
-# * You can run a `Function` locally or remotely in the cloud.
-# * You can immediately see a `Function`'s logs, even if it's remote.
-# * You can map a `Function` over many inputs to run it in parallel.
+# * You can run functions just as easily locally on your infrastructure as you can remotely on Modal's cloud infrastructure.
+# * Logs to `stdout` and `stderr` show up immediately, even for remote functions.
+# * Running functions in parallel on Modal is simple and fast.
 #
 # ## Importing Modal and defining the app
 #
@@ -23,7 +23,7 @@ stub = modal.Stub("example-hello-world")
 #
 # So first we've got to write some code.
 #
-# Let's do a simple, silly function:
+# Let's do something simple and silly:
 # logging `"hello"` to standard out if the input is even
 # or `"world"` to standard error if it's not,
 # then returning the input times itself.
@@ -45,7 +45,7 @@ def f(i):
 #
 # 1. As a regular `local` call on your computer, with `f.local`
 #
-# 2. As a `remote` call that runs on the cloud, with `f.remote`
+# 2. As a `remote` call that runs in the cloud, with `f.remote`
 #
 # 3. By `map`ping many copies of `f` in the cloud over many inputs, with `f.map`
 #
@@ -68,38 +68,41 @@ def main():
     print(total)
 
 # Enter `modal run hello_world.py` in a shell and you'll see
-# a Modal app start up, and then you'll see the `print`ed logs of
+# a Modal app start up. Modal runs functions in _containers_,
+# so you'll see .
+# Then you'll see the `print`ed logs of
 # the `main` function and, mixed in with them, all the logs of `f` as it is run
 # locally, then remotely, and then remotely and in parallel.
 #
-# That's all triggered by the `@stub.local_entrypoint()` decorator on `main`,
+# That's all triggered by adding the `@stub.local_entrypoint()` decorator on `main`,
 # which defines it as the function we start from locally when we invoke `modal run`.
 #
-# ## Why?
+# ## But why?
 #
 # Try doing one of these things next to start seeing the power of Modal!
 #
-# ### Change the code and run again
+# ### Change the code and run it again
 #
 # For instance, change the `print` statement in the function `f`
-# and run the app again.
-# You will see that that your new code is run with no extra work from you.
+# to print `"spam"` and `"eggs"` instead and run the app again.
+# You'll see that that your new code is run with no extra work from you --
+# and it even runs faster!
 #
 # Modal's goal is to make running code in the cloud feel like you're
-# running code locally. That means no running rebuild commands,
-# no fiddling with container pushes, and no context-switching to a web UI to inspect logs.
+# running code locally. That means no waiting for long image builds when you've just moved a comma,
+# no fiddling with container image pushes, and no context-switching to a web UI to inspect logs.
 #
 # ### Map over more data
 #
 # Change the `map` range from `20` to some large number, like `1170`. You'll see
-# Modal create and run even more containers in parallel.
+# Modal create and run even more containers in parallel this time.
 #
 # The function `f` is obviously silly and doesn't do much, but in its place
-# imagine something more significant, like:
+# imagine something that matters to you, like:
 #
 # * Running [language model inference](/docs/examples/vllm_mixtral) or [fine-tuning](/docs/examples/slack-finetune)
 # * Manipulating [audio](/docs/examples/discord-musicgen) or [images](stable_diffusion_xl_turbo)
 # * [Collecting financial data](/docs/examples/fetch_stock_prices) to backtest a trading algorithm.
 #
-# Modal lets you parallelize that operation trivially by running hundreds or
+# Modal lets you parallelize that operation effortlessly by running hundreds or
 # thousands of containers in the cloud.
