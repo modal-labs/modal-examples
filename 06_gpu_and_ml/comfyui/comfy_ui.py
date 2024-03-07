b219b3d0b571f11f427c9e1e66830757786ab12e
Kenny Ning <kennyning@gmail.com>
2024-03-07 13:20:44 -0500

    allow multiple checkpoints, comfy manager

--- 06_gpu_and_ml/comfyui/comfy_ui.py
+++ 06_gpu_and_ml/comfyui/comfy_ui.py
@@ -23,37 +23,38 @@ import modal
 # [notebooks/comfyui_colab.ipynb](https://github.com/comfyanonymous/ComfyUI/blob/master/notebooks/comfyui_colab.ipynb).
 #
 # This download function is run as the final image building step, and takes around 10 seconds to download
-# the ~2.0 GiB model checkpoint.
+# the ~2.0 GiB model checkpoint. To download other checkpoints, add the download url to checkpoints.txt.
 
 
 def download_checkpoint():
     import httpx
     from tqdm import tqdm
 
-    url = "https://huggingface.co/dreamlike-art/dreamlike-photoreal-2.0/resolve/main/dreamlike-photoreal-2.0.safetensors"
-    checkpoints_directory = "/root/models/checkpoints"
-    local_filename = url.split("/")[-1]
-    local_filepath = pathlib.Path(checkpoints_directory, local_filename)
-    local_filepath.parent.mkdir(parents=True, exist_ok=True)
+    with open("/checkpoints.txt") as f:
+        checkpoints = [line.strip() for line in f.readlines()]
 
-    print(f"downloading {url} ...")
-    with httpx.stream("GET", url, follow_redirects=True) as stream:
-        total = int(stream.headers["Content-Length"])
-        with open(local_filepath, "wb") as f, tqdm(
-            total=total, unit_scale=True, unit_divisor=1024, unit="B"
-        ) as progress:
-            num_bytes_downloaded = stream.num_bytes_downloaded
-            for data in stream.iter_bytes():
-                f.write(data)
-                progress.update(
-                    stream.num_bytes_downloaded - num_bytes_downloaded
-                )
+    for url in checkpoints:
+        checkpoints_directory = "/root/models/checkpoints"
+        local_filename = url.split("/")[-1]
+        local_filepath = pathlib.Path(checkpoints_directory, local_filename)
+        local_filepath.parent.mkdir(parents=True, exist_ok=True)
+
+        print(f"downloading {url} ...")
+        with httpx.stream("GET", url, follow_redirects=True) as stream:
+            total = int(stream.headers["Content-Length"])
+            with open(local_filepath, "wb") as f, tqdm(
+                total=total, unit_scale=True, unit_divisor=1024, unit="B"
+            ) as progress:
                 num_bytes_downloaded = stream.num_bytes_downloaded
+                for data in stream.iter_bytes():
+                    f.write(data)
+                    progress.update(stream.num_bytes_downloaded - num_bytes_downloaded)
+                    num_bytes_downloaded = stream.num_bytes_downloaded
 
 
 # Pin to a specific commit from https://github.com/comfyanonymous/ComfyUI/commits/master/
 # for stability. To update to a later ComfyUI version, change this commit identifier.
-comfyui_commit_sha = "b3b5ddb07a23b3d070df292c7a7fd6f83dc8fd50"
+comfyui_commit_sha = "a38b9b3ac152fb5679dad03813a93c09e0a4d15e"
 
 image = (
     modal.Image.debian_slim()
@@ -67,13 +68,13 @@ image = (
         "cd /root && git remote add --fetch origin https://github.com/comfyanonymous/ComfyUI",
         f"cd /root && git checkout {comfyui_commit_sha}",
         "cd /root && pip install xformers!=0.0.18 -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu121",
+        # Use Comfy Manager to install plugins (remember to restart your Comfy instance afterwards)
+        "cd /root/custom_nodes && git clone https://github.com/ltdrdata/ComfyUI-Manager.git",
+        "cd /root/custom_nodes/ComfyUI-Manager && pip install -r requirements.txt",
     )
-    # Use fork of https://github.com/valohai/asgiproxy with bugfixes.
-    .pip_install(
-        "git+https://github.com/modal-labs/asgiproxy.git@ef25fe52cf226f9a635e87616e7c049e451e2bd8",
-        "httpx",
-        "tqdm",
-    )
+    # Use fork until https://github.com/valohai/asgiproxy/pull/11 is merged.
+    .pip_install("git+https://github.com/modal-labs/asgiproxy.git", "httpx", "tqdm")
+    .copy_local_file("checkpoints.txt")
     .run_function(download_checkpoint)
 )
 stub = modal.Stub(name="example-comfy-ui", image=image)
@@ -135,6 +136,8 @@ def spawn_comfyui_in_background():
     # to be on a single container.
     concurrency_limit=1,
     timeout=10 * 60,
+    # Consider uncommenting the line below so that the server stays up for a longer session (default is 60s)
+    container_idle_timeout=300,
 )
 @modal.asgi_app()
 def web():
