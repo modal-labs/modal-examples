e8a585bd4e63d8e5d74a8bd5e018928c2ca385e1
Charles Frye <charles@modal.com>
2024-03-07 00:15:27 +0000

    switches matplotlib plotting to remote, minor text fixes

--- 10_integrations/s3_bucket_mount.py
+++ 10_integrations/s3_bucket_mount.py
@@ -1,31 +1,33 @@
 # # Mount S3 buckets
 #
-# This example shows how to mount an S3 bucket in a Modal app with [`CloudBucketMount`](/docs/reference/modal.CloudBucketMount).
-# We will both write and read from that bucket in parallel using [`map`](/docs/reference/modal.Function#map) and [`starmap`](/docs/reference/modal.Function#starmap).
+# This example shows how to mount an S3 bucket in a Modal app with [`CloudBucketMount`](https://modal.com/docs/reference/modal.CloudBucketMount).
+# We will write to and then read from that bucket, in each case Modal's [parallel execution features](https://modal.com/docs/guide/scale)
+# to handle many files at once.
 #
 # ## Basic setup
 #
-# You will need to have a S3 bucket and AWS credentials to run this example. Refer to the documentation
-# for detailed [IAM permissions](/docs/guide/cloud-bucket-mounts#iam-permissions) your credentials will need.
+# You will need to have an S3 bucket and AWS credentials to run this example. Refer to the documentation
+# for the exact [IAM permissions](https://modal.com/docs/guide/cloud-bucket-mounts#iam-permissions) your credentials will need.
 #
 # After you are done creating a bucket and configuring IAM settings,
-# you now need to create a [Modal Secret](/docs/guide/secrets). Navigate to the "Secrets" tab and
-# click on the AWS card, then fill in the fields with the AWS key and secret created
-# previously. Name the Secret `s3-bucket-secret`.
+# you now need to create a [`Secret`](https://modal.com/docs/guide/secrets) to share
+# the relevant AWS credentials with your Modal apps. Navigate to the "Secrets" tab and
+# click on the AWS card, then fill in the fields with your credentials.
+# Name the secret `s3-bucket-secret`.
 
 from datetime import datetime
 from pathlib import Path
 
 from modal import CloudBucketMount, Image, Secret, Stub
 
-image = Image.debian_slim().pip_install("requests==2.31.0", "duckdb==0.10.0")
+image = Image.debian_slim().pip_install("requests==2.31.0", "duckdb==0.10.0", "matplotlib==3.8.3")
 stub = Stub(image=image)
 
 MOUNT_PATH: Path = Path("/bucket")
 YELLOW_TAXI_DATA_PATH: Path = MOUNT_PATH / "yellow_taxi"
 
 
-# Dependencies are not available locally. The following block instructs Modal
+# Dependencies installed above are not available locally. The following block instructs Modal
 # to only make imports inside the container.
 with image.imports():
     import duckdb
@@ -41,8 +43,8 @@ with image.imports():
 # mounting a `modal.CloudBucketMount` with the S3 bucket name and its respective credentials.
 # The bucket will be mounted in `MOUNT_PATH`.
 #
-# This function takes two parameters: year and month. This allows us to use Modal's [`starmap`](/docs/reference/modal.Function#starmap)
-# to paralellize this function, downloading files in parallel into our S3 bucket.
+# This function takes two parameters: year and month, so we need to use Modal's [`starmap`](https://modal.com/docs/reference/modal.Function#starmap)
+# to parallelize this function. Running it in parallel across files massively speeds up the download.
 @stub.function(
     volumes={
         MOUNT_PATH: CloudBucketMount(
@@ -74,7 +76,8 @@ def download_data(year: int, month: int) -> str:
 # [DuckDB](https://duckdb.org/) is an analytical database with rich support for Parquet files.
 # It is also very fast. We will write a Modal Function that aggregates the number of yellow taxi trips
 # per Parquet file in our S3 bucket (files are organized by year and month combination).
-# This will allow for parallelism using Modal's [`map`](/docs/reference/modal.Function#map).
+# Since there's only a single argument, the path to the Parquet file,
+# we will parallelize it using [`map`](https://modal.com/docs/reference/modal.Function#map).
 @stub.function(
     volumes={
         MOUNT_PATH: CloudBucketMount(
@@ -109,10 +112,13 @@ def aggregate_data(path: str) -> tuple[datetime, int]:
 
 # ## Plot daily taxi rides
 #
-# Here we write a function to create a plot withour results.
+# Here we write a function to create a plot with our results.
 # The plot shows the number of yellow taxi rides per day in NYC.
-# This function will run locally and store the resulting plot image in `./nyc_yellow_taxi_trips.png`.
-def plot(dataset) -> None:
+# This function will run remotely, so we don't need to install plotting libraries locally.
+@stub.function()
+def plot(dataset) -> bytes:
+    import io
+
     import matplotlib.pyplot as plt
 
     # Sorting data by date
@@ -128,21 +134,29 @@ def plot(dataset) -> None:
     plt.ylabel("Number of daily trips")
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig("./nyc_yellow_taxi_trips_s3_mount.png")
+
+    # Saving plot as raw bytes to send back
+    buf = io.BytesIO()
+
+    plt.savefig(buf, format='png')
+
+    buf.seek(0)
+
+    return buf.getvalue()
 
 
 # ## Run everything
 #
 # Create a `@stub.local_entrypoint()` for your Modal program. This allows you to
-# run your program with the CLI by calling `modal run s3_bucket_mount.py`. We call both our Modal Functions
-# in parallel. We first call `download_data()` with `starmap`. `starmap` will map
-# a tuple of inputs (year, month) to the function's parameters. This will download
-# all yellow taxi data files into our locally mounted S3 bucket. Then, we call
-# `aggregate_data()` with `map` on a list of Parquet file paths. These files are
-# also read from our S3 bucket. One function writes files to S3 and the other
-# reads files from S3 in; both run in parallel.
+# run your program with the CLI by calling `modal run s3_bucket_mount.py`.
+# We first call `download_data()` and `starmap` (named because it's kind of like `map(*args)`)
+# on tuples of inputs `(year, month)`. This will download
+# all yellow taxi data files into our locally mounted S3 bucket and return a list of
+# Parquet file paths. Then, we call `aggregate_data()` with `map` on that list. These files are
+# also read from our S3 bucket. So one function writes files to S3 and the other
+# reads files from S3 in; both run across many files in parallel.
 #
-# Finally, we call `plot()` locally generating the following image:
+# Finally, we call `plot` to generate the following figure:
 #
 # ![Number of NYC yellow taxi trips by weekday, 2018-2023](./10_integrations/nyc_yellow_taxi_trips_s3_mount.png)
 #
@@ -165,4 +179,6 @@ def main():
     for r in aggregate_data.map(parquet_files):
         dataset += r
 
-    plot(dataset)
+    figure = plot.remote(dataset)
+    with open(Path(__file__).parent / "nyc_yellow_taxi_trips_s3_mount.png", "wb") as file:
+        file.write(figure)
