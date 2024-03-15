456b7dc9658f5acf041b6f4ad2364ead7c420195
Eric Zhang <ekzhang1@gmail.com>
2024-03-15 18:13:55 -0400

    Update examples to use @web_server (#648)
    * Update comfy_ui and serve_streamlit to use web_server
    
    * Remove `port=`
    
    * Fix typo
    
    * Edit comfy_ui
    
    * Increase comfyui request timeout to 30 minutes

--- 10_integrations/streamlit/serve_streamlit.py
+++ 10_integrations/streamlit/serve_streamlit.py
@@ -14,22 +14,18 @@
 # 2. `app.py`, which is any Streamlit script to be mounted into the Modal
 # function ([download script](https://github.com/modal-labs/modal-examples/blob/main/10_integrations/streamlit/app.py)).
 
-import pathlib
+import shlex
+import subprocess
+from pathlib import Path
 
 import modal
 
 # ## Define container dependencies
 #
 # The `app.py` script imports three third-party packages, so we include these in the example's
-# image definition. We also install `asgiproxy` to proxy the Streamlit server.
+# image definition.
 
-image = (
-    modal.Image.debian_slim()
-    .apt_install("git")
-    .pip_install("streamlit", "numpy", "pandas")
-    # Use fork until https://github.com/valohai/asgiproxy/pull/11 is merged.
-    .pip_install("git+https://github.com/modal-labs/asgiproxy.git")
-)
+image = modal.Image.debian_slim().pip_install("streamlit", "numpy", "pandas")
 
 stub = modal.Stub(name="example-modal-streamlit", image=image)
 
@@ -38,8 +34,8 @@ stub = modal.Stub(name="example-modal-streamlit", image=image)
 # We can just mount the `app.py` script inside the container at a pre-defined path using a Modal
 # [`Mount`](https://modal.com/docs/guide/local-data#mounting-directories).
 
-streamlit_script_local_path = pathlib.Path(__file__).parent / "app.py"
-streamlit_script_remote_path = pathlib.Path("/root/app.py")
+streamlit_script_local_path = Path(__file__).parent / "app.py"
+streamlit_script_remote_path = Path("/root/app.py")
 
 if not streamlit_script_local_path.exists():
     raise RuntimeError(
@@ -54,81 +50,18 @@ streamlit_script_mount = modal.Mount.from_local_file(
 # ## Spawning the Streamlit server
 #
 # Inside the container, we will run the Streamlit server in a background subprocess using
-# `subprocess.Popen`. Here we define `spawn_server()` to do this and then poll until the server
-# is ready to accept connections.
-
-HOST = "127.0.0.1"
-PORT = "8000"
-
-
-def spawn_server():
-    import socket
-    import subprocess
-
-    process = subprocess.Popen(
-        [
-            "streamlit",
-            "run",
-            str(streamlit_script_remote_path),
-            "--browser.serverAddress",
-            HOST,
-            "--server.port",
-            PORT,
-            "--browser.serverPort",
-            PORT,
-            "--server.enableCORS",
-            "false",
-        ]
-    )
-
-    # Poll until webserver accepts connections before running inputs.
-    while True:
-        try:
-            socket.create_connection((HOST, int(PORT)), timeout=1).close()
-            print("Webserver ready!")
-            return process
-        except (socket.timeout, ConnectionRefusedError):
-            # Check if launcher webserving process has exited.
-            # If so, a connection can never be made.
-            retcode = process.poll()
-            if retcode is not None:
-                raise RuntimeError(
-                    f"launcher exited unexpectedly with code {retcode}"
-                )
-
-
-# ## Wrap it in an ASGI app
-#
-# Finally, Modal can only serve apps that speak the [ASGI](https://modal.com/docs/guide/webhooks#asgi) or
-# [WSGI](https://modal.com/docs/guide/webhooks#wsgi) protocols. Since the Streamlit server is neither,
-# we run a separate ASGI app that proxies requests to the Streamlit server using the `asgiproxy` package.
-# Note that at this point `asgiproxy` has a bug with websocket handling, so we are using a
-# [fork](https://github.com/modal-labs/asgiproxy) with the fix for this.
+# `subprocess.Popen`. We also expose port 8000 using the `@web_server` decorator.
 
 
 @stub.function(
-    # Allows 100 concurrent requests per container.
     allow_concurrent_inputs=100,
     mounts=[streamlit_script_mount],
 )
-@modal.asgi_app()
+@modal.web_server(8000)
 def run():
-    from asgiproxy.config import BaseURLProxyConfigMixin, ProxyConfig
-    from asgiproxy.context import ProxyContext
-    from asgiproxy.simple_proxy import make_simple_proxy_app
-
-    spawn_server()
-
-    config = type(
-        "Config",
-        (BaseURLProxyConfigMixin, ProxyConfig),
-        {
-            "upstream_base_url": f"http://{HOST}:{PORT}",
-            "rewrite_host_header": f"{HOST}:{PORT}",
-        },
-    )()
-    proxy_context = ProxyContext(config)
-    return make_simple_proxy_app(proxy_context)
+    target = shlex.quote(str(streamlit_script_remote_path))
+    cmd = f"streamlit run {target} --server.port 8000 --server.enableCORS=false --server.enableXsrfProtection=false"
+    subprocess.Popen(cmd, shell=True)
 
 
 # ## Iterate and Deploy
