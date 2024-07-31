aa3c95c7012bb781d6facf004ed3f4017f6e74cb
Charles Frye <charles@modal.com>
2024-07-31 03:31:00 +0000

    rework vllm_inference example to showcase openai-compatible mode

---
+++ 06_gpu_and_ml/llm-serving/openai_compatible/locustfile.py
@@ -0,0 +1,37 @@
+import logging
+import random
+
+import locust
+
+messages = [
+    {
+        "role": "system",
+        "content": "You are a salesman for Modal, the cloud-native serverless Python computing platform.",
+    },
+    {
+        "role": "user",
+        "content": "Give me two fun date ideas.",
+    },
+]
+
+
+class WebsiteUser(locust.HttpUser):
+    wait_time = locust.between(1, 5)
+    headers = {
+        "Authorization": "Bearer super-secret-token",
+        "Accept": "application/json",
+    }
+
+    @locust.task
+    def chat_completion(self):
+        payload = {
+            "model": "meta-llama/Meta-Llama-3.1-8B-Instruct",
+            "messages": messages,
+        }
+
+        response = self.client.request(
+            "POST", "/v1/chat/completions", json=payload, headers=self.headers
+        )
+        response.raise_for_status()
+        if random.random() < 0.01:
+            logging.info(response.json()["choices"][0]["message"]["content"])
