8b8764482f07a9cbc14fab54c6e38302bba8332f
Charles Frye <charles@modal.com>
2024-09-16 23:14:23 +0000

    skip testing of model file

--- 06_gpu_and_ml/hyperparameter-sweep/model.py
+++ 06_gpu_and_ml/hyperparameter-sweep/model.py
@@ -1,5 +1,6 @@
 # ---
 # pytest: false
+# lambda-test: false
 # ---
 # Transformer model based on
 # [Attention Is All You Need](https://proceedings.neurips.cc/paper_files/paper/2017/file/3f5ee243547dee91fbd053c1c4a845aa-Paper.pdf)
