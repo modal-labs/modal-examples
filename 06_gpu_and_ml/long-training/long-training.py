8be7613ecea266a620c7a6940cb7a264938a29af
Yiren Lu <yirenlu13@gmail.com>
2024-09-20 13:13:50 -0400

    Remove train.py

---
+++ 06_gpu_and_ml/long-training/long-training.py
@@ -0,0 +1,192 @@
+# # Running long training jobs on Modal
+
+# While Modal functions typically have a [maximum timeout of 24 hours](/docs/guide/timeouts), you can still run long training jobs on Modal by implementing a checkpointing mechanism in your code.
+# This allows you to save the model's state periodically and resume from the last saved state.
+# In fact, we recommend implementing checkpointing logic regardless of the duration of your training jobs. This prevents loss of progress in case of interruptions or preemptions.
+
+# In this example, we'll walk through how to implement this pattern using PyTorch Lightning.
+
+# ## Pattern
+
+# The core pattern for long-duration training on Modal is as follows:
+
+# 1. Periodically save checkpoints to a Modal [volume](/docs/guide/volumes)
+# 2. Handle interruptions/timeouts and resume from the last checkpoint
+
+
+# ## Setup
+
+# Let's start by importing the Modal client and defining the Modal app and image. Since we are using PyTorch Lightning, we use a Cuda12 docker image as our base image.
+# and then install pytorch and lightning on top of that.
+
+import os
+
+import modal
+
+app = modal.App("interrupt-resume-lightning")
+
+# Set up the environment
+image = modal.Image.from_registry(
+    "nvidia/cuda:12.1.0-base-ubuntu22.04", add_python="3.10"
+).pip_install("lightning", "torchvision")
+
+
+# ## Define Modal Volume
+
+# Next, we set up a Modal [volume](/docs/guide/volumes) for storing both the training data and the checkpoints
+
+volume = modal.Volume.from_name("training-checkpoints", create_if_missing=True)
+
+VOLUME_PATH = "/vol"
+DATA_PATH = f"{VOLUME_PATH}/data"
+CHECKPOINTS_PATH = f"{VOLUME_PATH}/checkpoints"
+
+# ## Model training
+
+# We implement the actual model training class/functions and the checkpointing logic.
+# PyTorch Lightning offers some [built-in checkpointing](https://pytorch-lightning.readthedocs.io/en/1.2.10/common/weights_loading.html#:~:text=Lightning%20automates%20saving%20and%20loading,having%20to%20retrain%20the%20model.) functionality.
+# You can specify the checkpoint file path that you want to resume from using the `ckpt_path` parameter of [`trainer.fit`](https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.trainer.trainer.Trainer.html)
+# Additionally, you can specify the checkpointing interval with the `every_n_epochs` parameter of [`ModelCheckpoint`](https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.callbacks.ModelCheckpoint.html).
+# In the code below, we save checkpoints every 10 epochs, but this number can be adjusted depending on how long the epochs take. The goal is to minimize the disruption from job failures. Something that takes a few days should be checkpointed perhaps every few hours. Depending on what training framework you are using, how exactly this checkpointing gets implemented may vary.
+
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
+from torch import nn, optim, utils
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+
+
+# define the LightningModule
+class LitAutoEncoder(L.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = nn.functional.mse_loss(x_hat, x)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+def get_autoencoder(checkpoint_path=None):
+    # define any number of nn.Modules (or use your current ones)
+    print("Defining encoder and decoder")
+    encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+    decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+
+    return LitAutoEncoder(encoder, decoder)
+
+
+def get_train_loader(data_dir):
+    # setup data
+    print("Setting up data")
+    dataset = MNIST(data_dir, download=True, transform=ToTensor())
+    train_loader = utils.data.DataLoader(dataset)
+    return train_loader
+
+
+def get_checkpoint(checkpoint_dir):
+    return ModelCheckpoint(
+        dirpath=checkpoint_dir,
+        save_last=True,
+        every_n_epochs=10,
+        filename="epoch{epoch:02d}",
+    )
+
+
+def train_model(data_dir, checkpoint_dir, resume_from_checkpoint=None):
+    # train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+    autoencoder = get_autoencoder()
+    train_loader = get_train_loader(data_dir=data_dir)
+    checkpoint_callback = get_checkpoint(checkpoint_dir)
+    trainer = L.Trainer(
+        limit_train_batches=100, max_epochs=100, callbacks=[checkpoint_callback]
+    )
+    if resume_from_checkpoint:
+        print(f"Resuming from checkpoint: {resume_from_checkpoint}")
+        trainer.fit(
+            model=autoencoder,
+            train_dataloaders=train_loader,
+            ckpt_path=resume_from_checkpoint,
+        )
+    else:
+        print("Starting training from scratch")
+        trainer.fit(autoencoder, train_loader)
+    print("Done training")
+    return trainer
+
+
+# ## Training function deployed on Modal
+#
+# Next, we define the training function running on Modal infrastructure. Note that this function has the volume mounted on it.
+# The training function checks in the volume for an existing latest checkpoint file, and resumes training off that checkpoint if it finds it.
+# The `timeout` parameter in the `@app.function` decorator is set to 30 seconds for demonstration purposes. In a real scenario, you'd set this to a larger value (e.g., several hours) based on your needs.
+# If the function times out, or if the job is [preempted](/docs/guide/preemption#preemption), the main loop below will catch the exception and attempt to resume training from the last checkpoint.
+
+
+@app.function(
+    image=image,
+    # mounts=[train_script_mount],
+    volumes={VOLUME_PATH: volume},
+    gpu="any",
+    timeout=30,
+)
+def train():
+    from train import train_model
+
+    last_checkpoint = os.path.join(CHECKPOINTS_PATH, "last.ckpt")
+
+    try:
+        if os.path.exists(last_checkpoint):
+            # Resume from the latest checkpoint
+            print(
+                f"Resuming training from the latest checkpoint: {last_checkpoint}"
+            )
+            trainer = train_model(
+                DATA_PATH,
+                CHECKPOINTS_PATH,
+                resume_from_checkpoint=last_checkpoint,
+            )
+            print("Training resumed successfully")
+        else:
+            print("Starting training from scratch")
+            trainer = train_model(DATA_PATH, CHECKPOINTS_PATH)
+    except Exception as e:
+        print(f"Training interrupted due to: {str(e)}")
+        return None
+
+    return
+
+
+# ## Run the model
+#
+# We define a [`local_entrypoint`](https://modal.com/docs/guide/apps#entrypoints-for-ephemeral-apps)
+# to run the training. The entrypoint keeps attempting to run the training until it is able to complete successfully.
+# In practice, you may want to add additional monitoring or logging here, or a timeout, so that if there are errors in the training code, it doesn't just cycle indefinitely, using up compute.
+
+# You can run this locally with `modal run long-training.py  --detach`
+# This runs the code in detached mode, allowing it to continue running even if you close your terminal or computer. This is important since training jobs can be long.
+
+
+@app.local_entrypoint()
+def main():
+    while True:
+        try:
+            print("Starting new training run")
+            train.remote()
+
+            print("Finished training")
+            break  # Exit the loop if training completes successfully
+        except Exception as e:
+            print(f"An unexpected error occurred: {str(e)}")
+            print("Will attempt to resume in the next iteration.")
+            continue
