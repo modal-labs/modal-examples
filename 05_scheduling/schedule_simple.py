6bda13378ec1397e1587c3a751614d32e87e91f1
Yiren Lu <yirenlu13@gmail.com>
2024-09-23 11:44:53 -0400

    Add headers

--- 05_scheduling/schedule_simple.py
+++ 05_scheduling/schedule_simple.py
@@ -1,6 +1,18 @@
 # ---
 # cmd: ["python", "-m", "05_scheduling.schedule_simple"]
 # ---
+
+# # Scheduling functions
+# This example shows how you can schedule functions to run at specific times.
+# There are two different ways of doing this with Modal.
+# We
+# define two functions that print the current time. The first function runs every
+# 5 seconds, and the second function runs every minute. We use the `schedule`
+# argument to specify the schedule for each function. The `schedule` argument can
+# take a `modal.Period` object to specify a time interval or a `modal.Cron` object
+# to specify a cron expression. We run the app for 60 seconds to see the functions
+# in action.
+
 import time
 from datetime import datetime
 
