e45f6c017b27475091eb76c72e45b2d46cdb7aae
Kenny Ning <kennyning@gmail.com>
2024-09-23 12:00:38 -0400

    fix link

--- 06_gpu_and_ml/stable_diffusion/flux.py
+++ 06_gpu_and_ml/stable_diffusion/flux.py
@@ -3,7 +3,7 @@
 # ---
 # # Flux.1 (Schnell)
 #
-# This example runs the popular [Flux.1-schnell](https://www.google.com/search?client=firefox-b-1-d&q=flux+schnell+) text-to-image model on Modal.
+# This example runs the popular [Flux.1-schnell](https://huggingface.co/black-forest-labs/FLUX.1-schnell) text-to-image model on Modal.
 #
 # Thanks to [@Arro](https://github.com/Arro) for the original contribution.
 from io import BytesIO
