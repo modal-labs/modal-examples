51bca9d94e1a8fd2100897821f8a9666f96b43d1
Yiren Lu <yirenlu13@gmail.com>
2024-10-17 19:04:18 -0400

    Remove output files

--- 06_gpu_and_ml/sam/sam_example.py
+++ 06_gpu_and_ml/sam/sam_example.py
@@ -29,7 +29,7 @@
 
 import modal
 
-from .helper import show_mask, show_points
+# from .helper import show_mask, show_points
 
 MODEL_TYPE = "facebook/sam2-hiera-large"
 
@@ -51,6 +51,47 @@ image = (
     .run_commands("cd sam2 && pip install -e .")
 )
 app = modal.App("sam2-app", image=image)
+
+# Next, we define some helper functions to show the masks and points on the images:
+
+
+def show_mask(mask, ax, obj_id=None, random_color=False):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        cmap = plt.get_cmap("tab10")
+        cmap_idx = 0 if obj_id is None else obj_id
+        color = np.array([*cmap(cmap_idx)[:3], 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(
+        pos_points[:, 0],
+        pos_points[:, 1],
+        color="green",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+    ax.scatter(
+        neg_points[:, 0],
+        neg_points[:, 1],
+        color="red",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+
 
 # Next, we define the `Model` class that will handle SAM2 operations for both image and video.
 
