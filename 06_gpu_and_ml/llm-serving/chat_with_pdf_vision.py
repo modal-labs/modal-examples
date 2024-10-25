e31fd792eb4d4c9b4a6ac4a1e1902e4d9870f643
erik-dunteman <erikdunteman@gmail.com>
2024-10-24 20:59:57 -0700

    clean up comments

--- 06_gpu_and_ml/llm-serving/chat_with_pdf_vision.py
+++ 06_gpu_and_ml/llm-serving/chat_with_pdf_vision.py
@@ -21,16 +21,15 @@
 import os
 
 import modal
-from fastapi import FastAPI
 
 MINUTES = 60  # seconds
 
 # ## Downloading the Model
 
-# VLMs for embedding and generation add another layer of simplification to RAG apps based on vector search:
-# we only need one model. Here, we use the Qwen2-VL-2B-Instruct model from Alibaba.
+# Vision-language models (VLMs) for embedding and generation add another layer of simplification 
+# to RAG apps based on vector search: we only need one model.
+# Here, we use the Qwen2-VL-2B-Instruct model from Alibaba.
 # The function below downloads the model from the Hugging Face Hub.
-
 
 def download_model(model_dir, model_name, model_revision):
     from huggingface_hub import snapshot_download
@@ -44,7 +43,6 @@ def download_model(model_dir, model_name, model_revision):
         ignore_patterns=["*.pt", "*.bin"],  # using safetensors
     )
     move_cache()
-
 
 # ## Building the image
 
@@ -74,8 +72,8 @@ with model_image.imports():
     from qwen_vl_utils import process_vision_info
     from transformers import AutoProcessor, Qwen2VLForConditionalGeneration
 
-# We can also include other files our application needs in the container image.
-# Here, that's the model weights, which we download by executing our `download_model` function.
+# We can also include other files that our application needs in the container image.
+# Here, we we add the model weights to the image by executing our `download_model` function.
 
 model_image = model_image.run_function(
     download_model,
@@ -89,15 +87,13 @@ model_image = model_image.run_function(
 
 ## Defining a Chat with PDF application
 
-# Running an inference service on Modal is as easy as writing inference in Python.
-
-# We just need to wrap that in a Modal App
+# Running an inference service on Modal is as easy as writing an inference function in Python.
+# We just need to wrap that in a Modal App:
 
 app = modal.App("chat-with-pdf")
 
 # To allow concurrent users, each user chat session state is stored in a modal.Dict
 sessions = modal.Dict.from_name("colqwen-chat-sessions", create_if_missing=True)
-
 
 class Session:
     def __init__(self):
@@ -107,13 +103,14 @@ class Session:
 
 
 # Here we define our on-GPU ColQwen2 service, which runs document indexing and inference
+# It uses the [Modal @app.cls](https://modal.com/docs/guide/lifecycle-functions) feature to load the model on container start, and inference on request.
 @app.cls(
     image=model_image,
     gpu=modal.gpu.A100(size="80GB"),
     container_idle_timeout=10 * MINUTES,  # spin down when inactive
 )
 class Model:
-    @modal.build()
+    @modal.build() 
     @modal.enter()
     def load_models(self):
         self.colqwen2_model = ColQwen2.from_pretrained(
@@ -134,18 +131,28 @@ class Model:
 
     @modal.method()
     def index_pdf(self, session_id, images):
-        if session_id not in sessions:
-            sessions[session_id] = Session()
 
-        session = sessions[session_id]
+        # We store concurrent user chat sessions in a modal.Dict
+
+        # For simplicity, we assume that each session only runs one request at a time
+        # This assumption lets us fetch the session, and write to it later, without fear of race conditions
+
+        session = sessions.get(session_id)
+        if session is None:
+            session = Session()
+
         session.images = images
 
+        # Generated embeddings from the image(s)
         batch_images = self.colqwen2_processor.process_images(images).to(
             self.colqwen2_model.device
         )
         pdf_embeddings = self.colqwen2_model(**batch_images)
+
+        # Store the image embeddings in the session, for later RAG use
         session.pdf_embeddings = pdf_embeddings
-        # Update session state
+
+        # Write session state, including embeddings, back to the modal.Dict
         sessions[session_id] = session
 
     @modal.method()
@@ -154,25 +161,29 @@ class Model:
             sessions[session_id] = Session()
         session = sessions[session_id]
 
-        # nothing to chat about without a PDF!
+        # Nothing to chat about without a PDF!
         if session.images is None:
             return "Please upload a PDF first"
         elif session.pdf_embeddings is None:
             return "Indexing PDF..."
 
-        # retrieve the most relevant image from the PDF for the input query
+        # Retrieve the most relevant image from the PDF for the input query
         def get_relevant_image(message):
             batch_queries = self.colqwen2_processor.process_queries(
                 [message]
             ).to(self.colqwen2_model.device)
             query_embeddings = self.colqwen2_model(**batch_queries)
+
+            # This scores our query embedding against the image embeddings from index_pdf
             scores = self.colqwen2_processor.score_multi_vector(
                 query_embeddings, session.pdf_embeddings
             )[0]
+
+            # Return the best matching image
             max_index = max(range(len(scores)), key=lambda index: scores[index])
             return session.images[max_index]
 
-        # helper function to put message in the format chatbot
+        # Helper functions for chatbot formatting
         def get_chatbot_message_with_image(message, image):
             return {
                 "role": "user",
@@ -182,7 +193,6 @@ class Model:
                 ],
             }
 
-        # helper function add messages to the conversation history in chatbot format
         def append_to_messages(message, user_type="user"):
             session.messages.append(
                 {
@@ -191,7 +201,7 @@ class Model:
                 }
             )
 
-        # pass the query and retrieved image along with conversation history into the VLM for a response
+        # Pass the query and retrieved image along with conversation history into the VLM for a response
         def generate_response(message, image):
             chatbot_message = get_chatbot_message_with_image(message, image)
             query = self.qwen2_vl_processor.apply_chat_template(
@@ -222,12 +232,17 @@ class Model:
             )[0]
             return output_text
 
+        # RAG, Retrieval-Augmented Generation, is two steps:
+
+        # Retrieval of the most relevant image to answer the user's query
         relevant_image = get_relevant_image(message)
+
+        # Generation, passing in the query and the retrieved image
         output_text = generate_response(message, relevant_image)
+
+        # Update session state for future chats
         append_to_messages(message, user_type="user")
         append_to_messages(output_text, user_type="assistant")
-
-        # Update session state
         sessions[session_id] = session
 
         return output_text
@@ -238,16 +253,15 @@ class Model:
 # With the Gradio library, we can create a simple web interface around our class in Python,
 # then use Modal to host it for anyone to try out.
 
-# To deploy up your own, run
+# To deploy your own, run
 
 # ```bash
 # modal deploy chat_with_pdf_vision.py
 # ```
 
 # and navigate to the URL that appears in your teriminal.
-# If you’re editing the code, use `modal serve` instead to see changes live.
+# If you’re editing the code, use `modal serve` instead to see changes hot-reload.
 
-web_app = FastAPI()
 
 web_image = (
     modal.Image.debian_slim(python_version="3.12")
@@ -259,8 +273,6 @@ web_image = (
         "pdf2image==1.17.0",
     )
 )
-
-
 @app.function(
     image=web_image,
     keep_warm=1,
@@ -273,24 +285,32 @@ web_image = (
 @modal.asgi_app()
 def ui():
     import uuid
+
     import gradio as gr
+    from fastapi import FastAPI
     from gradio.routes import mount_gradio_app
     from gradio_pdf import PDF
     from pdf2image import convert_from_path
 
+    web_app = FastAPI()
+
+    # Since this Gradio app is running from its own container,
+    # allowing us to run the inference service via .remote() methods.
     model = Model()
 
     def upload_pdf(path, session_id):
-        if session_id == "":
+        if session_id == "" or session_id is None:
             # Generate session id if new client
             session_id = str(uuid.uuid4())
 
         images = convert_from_path(path)
+        # Call to our remote inference service to index the PDF
         model.index_pdf.remote(session_id, images)
 
         return session_id
 
     def respond_to_message(message, _, session_id):
+        # Call to our remote inference service to run RAG
         return model.respond_to_message.remote(session_id, message)
 
     with gr.Blocks(theme="soft") as demo:
