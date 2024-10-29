52c76aa5ce85b13be367a284bfc761bcfbd02eaa
Yiren Lu <yirenlu13@gmail.com>
2024-10-29 19:42:17 -0400

    Update

--- 06_gpu_and_ml/embeddings/text_embeddings_inference.py
+++ 06_gpu_and_ml/embeddings/text_embeddings_inference.py
@@ -1,8 +1,7 @@
 # ---
 # cmd: ["modal", "run", "06_gpu_and_ml/embeddings/text_embeddings_inference.py::embed_dataset"]
-# tags: ["use-case-embedding-model", "featured"]
 # ---
-# # Run an embedding model with TextEmbeddingsInference (TEI) on Modal
+# # Run TextEmbeddingsInference (TEI) on Modal
 #
 # This example runs the [Text Embedding Inference (TEI)](https://github.com/huggingface/text-embeddings-inference) toolkit on the Hacker News BigQuery public dataset.
 
