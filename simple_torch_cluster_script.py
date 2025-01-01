36cb58d540e18be200beac15909719ca15bfbd36
Jonathon Belotti <jonathon@modal.com>
2025-01-01 21:55:31 +0000

    fmt; typo

--- simple_torch_cluster_script.py
+++ simple_torch_cluster_script.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from contextlib import contextmanager
 
 import torch
 import torch.distributed as dist
@@ -14,7 +15,7 @@ MASTER_RANK = 0
 # This `run` function performs a simple distributed data transfer between containers
 # using the specified distributed communication backend.
 #
-# An example topology on the cluster when WORLD_SIZE=4 is shown below:
+# An example topology of the cluster when WORLD_SIZE=4 is shown below:
 #
 #        +---------+
 #        | Master  |
@@ -35,10 +36,15 @@ MASTER_RANK = 0
 # The master container (rank 0) sends a tensor to all other containers.
 # Each container then receives that tensor from the master container.
 
+
 def run(backend):
     # Helper function providing a vanity name for each container based on its world (i.e. global) rank.
     def container_name(wrld_rank: int) -> str:
-        return f"container-{wrld_rank} (master)" if wrld_rank == 0 else f"container-{wrld_rank}"
+        return (
+            f"container-{wrld_rank} (master)"
+            if wrld_rank == 0
+            else f"container-{wrld_rank}"
+        )
 
     tensor = torch.zeros(1)
 
@@ -48,13 +54,20 @@ def run(backend):
         tensor = tensor.to(device)
 
     if WORLD_RANK == MASTER_RANK:
-        print(f"{container_name(WORLD_RANK)} sending data to all other containers...\n")
+        print(
+            f"{container_name(WORLD_RANK)} sending data to all other containers...\n"
+        )
         for rank_recv in range(1, WORLD_SIZE):
             dist.send(tensor=tensor, dst=rank_recv)
-            print(f"{container_name(WORLD_RANK)} sent data to {container_name(rank_recv)}\n")
+            print(
+                f"{container_name(WORLD_RANK)} sent data to {container_name(rank_recv)}\n"
+            )
     else:
         dist.recv(tensor=tensor, src=MASTER_RANK)
-        print(f"{container_name(WORLD_RANK)} has received data from {container_name(MASTER_RANK)}\n")
+        print(
+            f"{container_name(WORLD_RANK)} has received data from {container_name(MASTER_RANK)}\n"
+        )
+
 
 # In order for the broadcast operation to happen across the cluster, we need to have the master container (rank 0)
 # learn the network addresses of all other containers.
@@ -62,8 +75,6 @@ def run(backend):
 # This is done by calling `dist.init_process_group` with the specified backend.
 #
 # See https://pytorch.org/docs/stable/distributed.html#torch.distributed.init_process_group for more details.
-from contextlib import contextmanager
-
 
 @contextmanager
 def init_processes(backend):
@@ -73,6 +84,7 @@ def init_processes(backend):
     finally:
         # Remove this if it causes program to hang. ref: https://github.com/pytorch/pytorch/issues/75097.
         dist.destroy_process_group()
+
 
 if __name__ == "__main__":
     # This is a minimal CLI interface adhering to the requirements of torch.distributed.run (torchrun).
