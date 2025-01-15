526bee4e1900b5278ce777a5bc815e6f25d85a39
Jonathon Belotti <jonathon@modal.com>
2025-01-15 15:23:09 -0500

    simple torch cluster example (#1029)

---
+++ 14_clusters/simple_torch_cluster.py
@@ -0,0 +1,134 @@
+# # Simple PyTorch cluster
+
+# This example shows how you can perform distributed computation with PyTorch.
+# It is a kind of 'hello world' example for distributed ML training: setting up a cluster
+# and executing a broadcast operation to share a single tensor.
+
+# ## Basic setup: Imports, dependencies, and a script
+
+# Let's get the imports out of the way first.
+# We need to import `modal.experimental` to use this feature, since it's still under development.
+# Let us know if you run into any issues!
+
+import os
+from pathlib import Path
+
+import modal
+import modal.experimental
+
+# Communicating between nodes in a cluster requires communication libraries.
+# We'll use `torch`, so we add it to our container's [Image](https://modal.com/docs/guide/images) here.
+
+image = modal.Image.debian_slim(python_version="3.12").pip_install(
+    "torch~=2.5.1", "numpy~=2.2.1"
+)
+
+# The approach we're going to take is to use a Modal [Function](https://modal.com/docs/reference/modal.Function)
+# to launch the underlying script we want to distribute over the cluster nodes.
+# The script is located in another file in the same directory
+# of [our examples repo](https://github.com/modal-labs/modal-examples/).
+# In order to use it in our remote Modal Function,
+# we need to duplicate it remotely, which we do with `add_local_file`.
+
+this_directory = Path(__file__).parent
+
+image = image.add_local_file(
+    this_directory / "simple_torch_cluster_script.py",
+    remote_path="/root/script.py",
+)
+
+app = modal.App("example-simple-torch-cluster", image=image)
+
+# ## Configuring a test cluster
+
+# Some basic configuration allows for demoing either a CPU-only cluster or a GPU-enabled cluster
+# with one GPU per container.
+
+# First, we set the size of the cluster in containers/nodes. This can be between 1 and 8.
+# This is part of our Modal configuration, since Modal is responsible for spinning up our cluster.
+
+n_nodes = 4
+
+# Next, we set the number of processes we run per node.
+# The usual practice is to run one process per GPU,
+# so we set those two values to be equal.
+# Note that `N_GPU` is Modal configuration ("how many GPUs should we spin up for you?")
+# while `nproc_per_node` is `torch.distributed` configuration ("how many processes should we spawn for you?").
+
+n_proc_per_node = N_GPU = 1
+
+# Lastly, we need to select our communications library: the software that will handle
+# sending messages between nodes in our cluster.
+# Since we are running on GPUs, we use the
+# [NVIDIA Collective Communications Library](https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/index.html)
+# (`nccl`, pronounced "nickle").
+
+# This is part of `torch.distributed` configuration --
+# Modal handles the networking infrastructure but not the communication protocol.
+
+backend = "nccl"  # or "gloo" on CPU, see https://pytorch.org/docs/stable/distributed.html#which-backend-to-use
+
+# This cluster configurations is nice for testing, but typically
+# you'll want to run a cluster with the maximum number of GPUs per container --
+# 8 if you're running on H100s, the beefiest GPUs we offer on Modal.
+
+# ## Launching the script
+
+# Our Modal Function is merely a 'launcher' that sets up the distributed
+# cluster environment and then calls `torch.distributed.run`,
+# the underlying Python code exposed by the [`torchrun`](https://pytorch.org/docs/stable/elastic/run.html)
+# command line tool.
+
+# So executing this distributed job is easy! Just run
+
+# ```bash
+# modal run simple_torch_cluster.py
+# ```
+
+# in your terminal.
+
+# In addition to the values set in code above, you can pass additional arguments to `torch.distributed.run` with
+# the `additional_args` parameter, like so:
+
+# ```bash
+# modal run simple_torch_cluster.py --additional-args "--max-restarts=1"
+# ```
+
+
+@app.function(gpu=modal.gpu.H100(count=N_GPU))
+@modal.experimental.clustered(size=n_nodes)
+def dist_run_script(additional_args: str = ""):
+    import shlex
+
+    from torch.distributed.run import parse_args, run
+
+    cluster_info = (  # we populate this data for you
+        modal.experimental.get_cluster_info()
+    )
+    # which container am I?
+    container_rank = cluster_info.rank
+    # how many containers are in this cluster?
+    world_size = len(cluster_info.container_ips)
+    # what's the leader/master/main container's address?
+    main_addr = cluster_info.container_ips[0]
+    # what's the identifier of this cluster task in Modal?
+    task_id = os.environ["MODAL_TASK_ID"]
+    print(f"hello from {container_rank=}")
+    if container_rank == 0:
+        print(
+            f"reporting cluster state from rank0/main: {main_addr=}, {world_size=}, {task_id=}"
+        )
+
+    run(
+        parse_args(
+            [
+                f"--nnodes={n_nodes}",
+                f"--node_rank={cluster_info.rank}",
+                f"--master_addr={main_addr}",
+                f"--nproc-per-node={n_proc_per_node}",
+                "--master_port=1234",
+            ]
+            + shlex.split(additional_args)
+            + ["/root/script.py", "--backend", backend]
+        )
+    )
---
+++ 14_clusters/simple_torch_cluster_script.py
@@ -0,0 +1,113 @@
+# ---
+# lambda-test: false
+# pytest: false
+# ---
+import argparse
+import os
+from contextlib import contextmanager
+
+import torch
+import torch.distributed as dist
+
+# Environment variables set by torch.distributed.run.
+LOCAL_RANK = int(os.environ["LOCAL_RANK"])
+WORLD_SIZE = int(os.environ["WORLD_SIZE"])
+WORLD_RANK = int(os.environ["RANK"])
+# The master (or leader) rank is always 0 with torch.distributed.run.
+MASTER_RANK = 0
+
+# This `run` function performs a simple distributed data transfer between containers
+# using the specified distributed communication backend.
+
+# An example topology of the cluster when WORLD_SIZE=4 is shown below:
+#
+#        +---------+
+#        | Master  |
+#        | Rank 0  |
+#        +----+----+
+#             |
+#             |
+#    +--------+--------+
+#    |        |        |
+#    |        |        |
+# +--+--+  +--+--+  +--+--+
+# |Rank 1| |Rank 2| |Rank 3|
+# +-----+  +-----+  +-----+
+
+# A broadcast operation (https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/usage/collectives.html#broadcast)
+# is performed between the master container (rank 0) and all other containers.
+
+# The master container (rank 0) sends a tensor to all other containers.
+# Each container then receives that tensor from the master container.
+
+
+def run(backend):
+    # Helper function providing a vanity name for each container based on its world (i.e. global) rank.
+    def container_name(wrld_rank: int) -> str:
+        return (
+            f"container-{wrld_rank} (main)"
+            if wrld_rank == 0
+            else f"container-{wrld_rank}"
+        )
+
+    tensor = torch.zeros(1)
+
+    # Need to put tensor on a GPU device for NCCL backend.
+    if backend == "nccl":
+        device = torch.device("cuda:{}".format(LOCAL_RANK))
+        tensor = tensor.to(device)
+
+    if WORLD_RANK == MASTER_RANK:
+        print(
+            f"{container_name(WORLD_RANK)} sending data to all other containers...\n"
+        )
+        for rank_recv in range(1, WORLD_SIZE):
+            dist.send(tensor=tensor, dst=rank_recv)
+            print(
+                f"{container_name(WORLD_RANK)} sent data to {container_name(rank_recv)}\n"
+            )
+    else:
+        dist.recv(tensor=tensor, src=MASTER_RANK)
+        print(
+            f"{container_name(WORLD_RANK)} has received data from {container_name(MASTER_RANK)}\n"
+        )
+
+
+# In order for the broadcast operation to happen across the cluster, we need to have the master container (rank 0)
+# learn the network addresses of all other containers.
+
+# This is done by calling `dist.init_process_group` with the specified backend.
+
+# See https://pytorch.org/docs/stable/distributed.html#torch.distributed.init_process_group for more details.
+
+
+@contextmanager
+def init_processes(backend):
+    try:
+        dist.init_process_group(backend, rank=WORLD_RANK, world_size=WORLD_SIZE)
+        yield
+    finally:
+        # Remove this if it causes program to hang. ref: https://github.com/pytorch/pytorch/issues/75097.
+        dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    # This is a minimal CLI interface adhering to the requirements of torch.distributed.run (torchrun).
+    #
+    # Our Modal Function will use torch.distributed.run to launch this script.
+    #
+    # See https://pytorch.org/docs/stable/elastic/run.html for more details on the CLI interface.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--local-rank",
+        "--local_rank",
+        type=int,
+        help="Local rank. Necessary for using the torch.distributed.launch utility.",
+    )
+    parser.add_argument(
+        "--backend", type=str, default="gloo", choices=["nccl", "gloo"]
+    )
+    args = parser.parse_args()
+
+    with init_processes(backend=args.backend):
+        run(backend=args.backend)
