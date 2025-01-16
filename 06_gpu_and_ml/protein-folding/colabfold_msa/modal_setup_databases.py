fdbff9dc6292405ed7e0586a087cb3c3f031014a
Charles Frye <charles@modal.com>
2025-01-16 22:55:48 +0000

    minor fixes

--- 06_gpu_and_ml/protein-folding/colabfold_msa/modal_setup_databases.py
+++ 06_gpu_and_ml/protein-folding/colabfold_msa/modal_setup_databases.py
@@ -22,8 +22,7 @@ import modal
 
 L.basicConfig(
     level=L.INFO,
-    format="\033[0;32m%(asctime)s %(levelname)s
-[%(filename)s.%(funcName)-22s:%(lineno)-3d] %(message)s\033[0m",
+    format="\033[0;32m%(asctime)s %(levelname)s[%(filename)s.%(funcName)-22s:%(lineno)-3d] %(message)s\033[0m",
     datefmt="%b %d %H:%M:%S",
 )
 
@@ -32,7 +31,7 @@ here = Path(__file__).parent  # the directory of this file
 MINUTES = 60  # seconds
 HOURS = 60 * MINUTES  # seconds
 
-GiB = 1024 # mebibytes
+GiB = 1024  # mebibytes
 
 ARIA_NUM_CONNECTIONS = 8
 
@@ -56,10 +55,9 @@ colabfold_image = (
         "git clone https://github.com/soedinglab/MMseqs2",
         f"cd MMseqs2 && git checkout {mmseqs_commit_id}",
         "cd MMseqs2 && mkdir build",
-        "cd MMseqs2/build && cmake -DCMAKE_BUILD_TYPE=RELEASE -DHAVE_ZLIB=1
--DCMAKE_INSTALL_PREFIX=. ..",
+        "cd MMseqs2/build && cmake -DCMAKE_BUILD_TYPE=RELEASE -DHAVE_ZLIB=1 -DCMAKE_INSTALL_PREFIX=. ..",
         "cd MMseqs2/build && make -j4",
-        "cd MMseqs2/build && make install ", # TODO GPU installn
+        "cd MMseqs2/build && make install ",  # TODO GPU installn
         "ln -s /MMseqs2/build/bin/mmseqs /usr/local/bin/mmseqs",
     )
     .run_commands(
@@ -82,24 +80,26 @@ mmcif_image = (
 with mmcif_image.imports():
     import shutil
 
+
 def download_file(url, filename):
     import subprocess
+
     command = [
-       "aria2c",
+        "aria2c",
         "--log-level=warn",
-        "-x", str(ARIA_NUM_CONNECTIONS),
-        "-o", filename,
+        "-x",
+        str(ARIA_NUM_CONNECTIONS),
+        "-o",
+        filename,
         "-c",
-        "-d", data_path,
-        url
+        "-d",
+        data_path,
+        url,
     ]
     subprocess.run(command, check=True)
 
-def extract_with_progress(
-    filepath,
-    with_pattern="",
-    chunk_size=1024 * 1024
-):
+
+def extract_with_progress(filepath, with_pattern="", chunk_size=1024 * 1024):
     import tarfile
 
     from tqdm import tqdm
@@ -115,7 +115,10 @@ def extract_with_progress(
                 continue
             member_path = data_path / member.name
 
-            if member_path.exists() and member_path.stat().st_size == member.size:
+            if (
+                member_path.exists()
+                and member_path.stat().st_size == member.size
+            ):
                 L.info(f"already extracted {member.name}, skipping")
                 continue
 
@@ -123,9 +126,9 @@ def extract_with_progress(
             L.info(f"member size: {format_human_readable_bytes(member.size)}")
 
             file_progress = tqdm(
-                total=member.size, unit='B', desc=member.name, unit_scale=True
+                total=member.size, unit="B", desc=member.name, unit_scale=True
             )
-            with open(member_path, 'wb') as f:
+            with open(member_path, "wb") as f:
                 while True:
                     chunk = extract_file.read(chunk_size)
                     if not chunk:
@@ -142,14 +145,17 @@ def extract_with_progress(
     memory=2 * GiB,
     timeout=4 * HOURS,
 )
-def setup_colabfold_db(url: str, mmseqs_no_index: bool, mmseqs_force_merge: bool):
+def setup_colabfold_db(
+    url: str, mmseqs_no_index: bool, mmseqs_force_merge: bool
+):
     import subprocess
+
     assert mmseqs_no_index, "mmseqs_no_index=false is not supported yet (wip)"
 
     filename = urlparse(url).path.split("/")[-1]
     dest_filepath = data_path / filename
-    extraction_complete_filepath = (
-        dest_filepath.with_suffix("").with_suffix(".complete")
+    extraction_complete_filepath = dest_filepath.with_suffix("").with_suffix(
+        ".complete"
     )
 
     L.info(f"downloading from {url} to {dest_filepath}")
@@ -163,7 +169,7 @@ def setup_colabfold_db(url: str, mmseqs_no_index: bool, mmseqs_force_merge: bool
         L.info("extraction already complete, skipping")
     extracted_folder = data_path / Path(Path(filename).stem).stem
 
-    db_foldername = extracted_folder.with_stem(extracted_folder.stem  + "_db")
+    db_foldername = extracted_folder.with_stem(extracted_folder.stem + "_db")
 
     L.info(f"converting TSV to MMseqs2 DB: {db_foldername}")
     setup_env = os.environ.copy()
@@ -176,6 +182,7 @@ def setup_colabfold_db(url: str, mmseqs_no_index: bool, mmseqs_force_merge: bool
     ]
     subprocess.run(command, check=True, env=setup_env)
     volume.commit()
+
 
 @app.function(
     image=colabfold_image,
@@ -194,14 +201,18 @@ def setup_hhsuite_data(url: str):
     extract_with_progress(data_path / filename, with_pattern="a3m")
     volume.commit()
 
+
 @app.function(
     image=colabfold_image,
     volumes={volume_path: volume},
     memory=2 * GiB,
     timeout=1 * HOURS,
 )
-def setup_colabfold_fasta_db(url: str, mmseqs_no_index: bool, mmseqs_force_merge: bool):
+def setup_colabfold_fasta_db(
+    url: str, mmseqs_no_index: bool, mmseqs_force_merge: bool
+):
     import subprocess
+
     assert mmseqs_no_index, "mmseqs_no_index=false is not supported yet (wip)"
     try:
         subprocess.run(["nvidia-smi"], check=True)
@@ -222,7 +233,7 @@ def setup_colabfold_fasta_db(url: str, mmseqs_no_index: bool, mmseqs_force_merge
         "mmseqs",
         "createdb",
         dest_filepath,
-        dest_filepath.with_suffix("").with_suffix("")
+        dest_filepath.with_suffix("").with_suffix(""),
     ]
     subprocess.run(command, check=True, env=setup_env)
     volume.commit()
@@ -230,13 +241,15 @@ def setup_colabfold_fasta_db(url: str, mmseqs_no_index: bool, mmseqs_force_merge
 
 def move_to_modal(mmcif_relative_path, s3_dir_path, modal_dir_path):
     def exists_and_same_size(a, b):
-        return a.exists() and b.exists() and a.stat().st_size == b.stat().st_size
+        return (
+            a.exists() and b.exists() and a.stat().st_size == b.stat().st_size
+        )
 
     retries = 3
     while retries > 0:
         try:
             source_filepath = s3_dir_path / mmcif_relative_path
-            dest_filepath   = modal_dir_path / mmcif_relative_path
+            dest_filepath = modal_dir_path / mmcif_relative_path
 
             if exists_and_same_size(source_filepath, dest_filepath):
                 print(f"exists: {mmcif_relative_path.stem} ", end="")
@@ -253,16 +266,17 @@ def move_to_modal(mmcif_relative_path, s3_dir_path, modal_dir_path):
             retries -= 1
     return False
 
+
 def scan_directory(subdir_path):
     return list([f for f in subdir_path.rglob("*") if f.is_file()])
+
 
 @app.function(
     image=mmcif_image,
     volumes={
         volume_path: volume,
         s3_bucket_path: modal.CloudBucketMount(
-            bucket_name="pdbsnapshots",
-            read_only=True
+            bucket_name="pdbsnapshots", read_only=True
         ),
     },
     timeout=6 * HOURS,
@@ -284,8 +298,14 @@ def setup_mmcif_database(
     max_workers = num_workers_per_cpu * os.cpu_count()
 
     s3_dir_path = (
-        s3_bucket_path / snapshot_id / "pub" / "pdb"
-        / "data" / "structures" / pdb_type / "mmCIF"
+        s3_bucket_path
+        / snapshot_id
+        / "pub"
+        / "pdb"
+        / "data"
+        / "structures"
+        / pdb_type
+        / "mmCIF"
     )
     modal_dir_path = data_path / "pdb" / pdb_type
 
@@ -293,10 +313,10 @@ def setup_mmcif_database(
     mmcif_paths = [f for f in s3_dir_path.iterdir() if f.is_file()]
     s3_subdir_paths = [d for d in s3_dir_path.iterdir() if d.is_dir()]
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
-         futures = [executor.submit(scan_directory, d) for d in s3_subdir_paths]
+        futures = [executor.submit(scan_directory, d) for d in s3_subdir_paths]
 
-         for future in tqdm(futures, desc="scanning s3 directories"):
-             mmcif_paths.extend(future.result())
+        for future in tqdm(futures, desc="scanning s3 directories"):
+            mmcif_paths.extend(future.result())
 
     mmcif_relative_paths = [f.relative_to(s3_dir_path) for f in mmcif_paths]
 
@@ -304,8 +324,9 @@ def setup_mmcif_database(
         (f, s3_dir_path, modal_dir_path) for f in mmcif_relative_paths
     ]
 
-    L.info(f"d/l all PDBs with process pool, max_workers={max_workers},"
-          f" {len(tasks_args)} d/l tasks will be started."
+    L.info(
+        f"d/l all PDBs with process pool, max_workers={max_workers},"
+        f" {len(tasks_args)} d/l tasks will be started."
     )
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
         futures = [executor.submit(move_to_modal, *args) for args in tasks_args]
@@ -318,23 +339,24 @@ def setup_mmcif_database(
     L.info(f"finalizying downloads with: {pdb_server}")
     command = [
         "rsync",
-        "-rlpt", "-z",
+        "-rlpt",
+        "-z",
         "--delete",
         f"--port={pdb_port}",
         f"{pdb_server}/data/structures/{pdb_type}/mmCIF",
-        f"{modal_dir_path}"
+        f"{modal_dir_path}",
     ]
     subprocess.run(command, check=True)
     volume.commit()
 
+
 @app.local_entrypoint()
 def main(
-    mmseqs_no_index: bool = True, 
-    mmseqs_force_merge: bool = False, 
+    mmseqs_no_index: bool = True,
+    mmseqs_force_merge: bool = False,
     pdb_aws_snapshot: str = "20240101",
 ):
-
-    colabfold_url = "https://wwwuser.gwdg.de/~compbiol/colabfold/" # sic
+    colabfold_url = "https://wwwuser.gwdg.de/~compbiol/colabfold/"  # sic
     hhsuite_url = (
         "https://wwwuser.gwdg.de/~compbiol/data/hhsuite/databases/hhsuite_dbs/"
     )
@@ -352,14 +374,14 @@ def main(
             mmseqs_force_merge,
         ),
         setup_colabfold_fasta_db.spawn(
-            urljoin(colabfold_url,  "pdb100_230517.fasta.gz"),
+            urljoin(colabfold_url, "pdb100_230517.fasta.gz"),
             mmseqs_no_index,
             mmseqs_force_merge,
         ),
-
         # Data from HHSuite
-        setup_hhsuite_data.spawn(urljoin(hhsuite_url, "pdb100_foldseek_230517.tar.gz")),
-
+        setup_hhsuite_data.spawn(
+            urljoin(hhsuite_url, "pdb100_foldseek_230517.tar.gz")
+        ),
         # MMCIF data
         setup_mmcif_database.spawn(pdb_aws_snapshot, "divided"),
         setup_mmcif_database.spawn(pdb_aws_snapshot, "obsolete"),
@@ -373,8 +395,9 @@ def main(
 
 # ### Helper Functions
 
+
 def format_human_readable_bytes(size):
-    for unit in ['B', 'KB', 'MB', 'GB']:
+    for unit in ["B", "KB", "MB", "GB"]:
         if size < 1024:
             return f"{size:.2f} {unit}"
         size /= 1024
