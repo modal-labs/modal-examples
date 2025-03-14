c5148e99df685f4e8249ff4d5e3c3090eb3b18ee
Charles Frye <charles@modal.com>
2025-03-13 17:44:02 -0700

    adds a minimal in-memory qdrant example (#1097)

---
+++ 06_gpu_and_ml/embeddings/qdrant.py
@@ -0,0 +1,43 @@
+import modal
+
+app = modal.App("example-qdrant-in-memory")
+
+image = modal.Image.debian_slim(python_version="3.11").pip_install(
+    "qdrant-client[fastembed-gpu]==1.13.3"
+)
+
+
+@app.function(image=image, gpu="any")
+def query(inpt):
+    from qdrant_client import QdrantClient
+
+    client = QdrantClient(":memory:")
+
+    docs = [
+        "Qdrant has Langchain integrations",
+        "Qdrant also has Llama Index integrations",
+    ]
+
+    print("querying documents:", *docs, sep="\n\t")
+
+    client.add(collection_name="demo_collection", documents=docs)
+
+    print("query:", inpt, sep="\n\t")
+
+    search_results = client.query(
+        collection_name="demo_collection",
+        query_text=inpt,
+        limit=1,
+    )
+
+    print("result:", search_results[0], sep="\n\t")
+
+    return search_results[0].document
+
+
+@app.local_entrypoint()
+def main(inpt: str = None):
+    if not inpt:
+        inpt = "alpaca"
+
+    print(query.remote(inpt))
