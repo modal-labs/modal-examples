9e51dfe71b50995e38266b5419ded50b2fdee496
Shariq Mobin <shariq@modal.com>
2025-04-03 15:02:38 -0700

    add chat iface for trtllm latency example
    ---------

--- 06_gpu_and_ml/llm-serving/trtllm_latency.py
+++ 06_gpu_and_ml/llm-serving/trtllm_latency.py
@@ -5,16 +5,16 @@
 
 # Many popular language model applications, like chatbots and code editing,
 # put humans and models in direct interaction. According to
-# [classic results in human-computer-interaction](https://lawsofux.com/doherty-threshold/),
+# [classic results in human-computer interaction](https://lawsofux.com/doherty-threshold/),
 # computer systems need to keep their response times under 400ms
 # in order to keep up with their human users.
 
 # To hit this target, we use the [TensorRT-LLM](https://github.com/NVIDIA/TensorRT-LLM)
 # inference framework from NVIDIA. TensorRT-LLM is the Lamborghini of inference engines:
 # it achieves seriously impressive latency, but only if you tune it carefully.
-# With the out-of-the-box defaults we observe an unacceptable median latency of 1.1s,
-# but with careful configuration, we'll bring that down to just 250ms
-# -- over a 4x speed up!
+# With the out-of-the-box defaults we observe an unacceptable median time
+# to last token of over a second, but with careful configuration,
+# we'll bring that down to under 250ms  -- over a 4x speed up!
 
 # These latencies were measured on a single NVIDIA H100 GPU with prompts and generations
 # of a few dozen to a few hundred tokens.
@@ -27,10 +27,10 @@
 # for building and running TensorRT-LLM engines, and
 
 # 2. how to use recommendations from the
-# [performance guide](https://github.com/NVIDIA/TensorRT-LLM/blob/b763051ba429d60263949da95c701efe8acf7b9c/docs/source/performance/performance-tuning-guide/useful-build-time-flags.md)
+# [TensorRT-LLM performance guide](https://github.com/NVIDIA/TensorRT-LLM/blob/b763051ba429d60263949da95c701efe8acf7b9c/docs/source/performance/performance-tuning-guide/useful-build-time-flags.md)
 # to optimize the engine for low latency.
 
-# Be sure to check out TRTLLM's
+# Be sure to check out TensorRT-LLM's
 # [examples](https://nvidia.github.io/TensorRT-LLM/llm-api-examples)
 # for sample code beyond what we cover here, like low-rank adapters (LoRAs).
 
@@ -73,7 +73,7 @@ tensorrt_image = modal.Image.from_registry(
 # On top of that, we add some system dependencies of TensorRT-LLM,
 # including OpenMPI for distributed communication, some core software like `git`,
 # the `tensorrt_llm` package itself, and finally `flashinfer` for optimized
-# [flash attention kernels](https://docs.flashinfer.ai/) that TensorRT-LLM uses.
+# [flash attention kernels](https://docs.flashinfer.ai/).
 
 tensorrt_image = (
     tensorrt_image.apt_install(
@@ -104,10 +104,12 @@ tensorrt_image = (
 
 # Next, we'll set up a few things to download the model to persistent storage and do it quickly --
 # this is a latency-optimized example after all! For persistent, distributed storage, we use
-# [Modal Volumes](https://modal.com/docs/guide/volumes) which can be accessed from any container.
+# [Modal Volumes](https://modal.com/docs/guide/volumes), which can be accessed from any container
+# with read speeds in excess of a gigabyte per second.
 
-# We also set up the `HF_HOME` environment variable to point to the Volume so that the model
-# is cached there. Then we install `hf-transfer` to get max download throughput from Hugging Face.
+# We also set the `HF_HOME` environment variable to point to the Volume so that the model
+# is cached there. And we install `hf-transfer` to get maximum download throughput from
+# the Hugging Face Hub, in the hundreds of megabytes per second.
 
 volume = modal.Volume.from_name(
     "example-trtllm-inference-volume", create_if_missing=True
@@ -139,18 +141,18 @@ with tensorrt_image.imports():
 # ### Quantization
 
 # The amount of [GPU RAM](https://modal.com/gpu-glossary/device-hardware/gpu-ram)
-# on a single card is a tight constraint for most LLMs:
-# RAM is measured in billions of bytes and models have billions of parameters,
+# on a single card is a tight constraint for large models:
+# RAM is measured in billions of bytes and large models have billions of parameters,
 # each of which is two to four bytes.
 # The performance cliff if you need to spill to CPU memory is steep,
 # so all of those parameters must fit in the GPU memory,
 # along with other things like the KV cache built up while processing prompts.
 
 # The simplest way to reduce LLM inference's RAM requirements is to make the model's parameters smaller,
-# to fit their values in a smaller number of bits, like four or eight. This is known as _quantization_.
+# fitting their values in a smaller number of bits, like four or eight. This is known as _quantization_.
 
 # NVIDIA's [Ada Lovelace/Hopper chips](https://modal.com/gpu-glossary/device-hardware/streaming-multiprocessor-architecture),
-# like the 4090, L40S, and H100, are capable of native 8bit floating point calculations
+# like the L40S and H100, are capable of native 8bit floating point calculations
 # in their [Tensor Cores](https://modal.com/gpu-glossary/device-hardware/tensor-core),
 # so we choose that as our quantization format.
 # These GPUs are capable of twice as many floating point operations per second in 8bit as in 16bit --
@@ -158,10 +160,9 @@ with tensorrt_image.imports():
 
 # Quantization buys us two things:
 
-# - faster startup, since less data has to be moved over the netwprk onto CPU and GPU RAM
+# - faster startup, since less data has to be moved over the network onto CPU and GPU RAM
 
-# - faster inference, since we get twice the FLOP/s
-# and less data has to be moved from GPU RAM into
+# - faster inference, since we get twice the FLOP/s and less data has to be moved from GPU RAM into
 # [on-chip memory](https://modal.com/gpu-glossary/device-hardware/l1-data-cache) and
 # [registers](https://modal.com/gpu-glossary/device-hardware/register-file)
 # with each computation
@@ -183,7 +184,7 @@ def get_quant_config():
 # Quantization is a lossy compression technique. The impact on modal quality can be
 # minimized by tuning the quantization parameters on even a small dataset. Typically, we
 # see less than 2% degradation in evaluation metrics when using `fp8`. We'll use the
-# trtllm `CalibrationConfig` class to specify the calibration dataset:
+# `CalibrationConfig` class to specify the calibration dataset.
 
 
 def get_calib_config():
@@ -202,22 +203,25 @@ def get_calib_config():
 # TensorRT-LLM is an LLM inference framework built on top of NVIDIA's TensorRT,
 # which is a generic inference framework for neural networks.
 
-# TensortRT includes a "plugin" extension system that allows you to adjust behavior,
+# TensorRT includes a "plugin" extension system that allows you to adjust behavior,
 # like configuring the [CUDA kernels](https://modal.com/gpu-glossary/device-software/kernel)
 # used by the engine.
 # The [General Matrix Multiply (GEMM)](https://docs.nvidia.com/deeplearning/performance/dl-performance-matrix-multiplication/index.html)
 # plugin, for instance, adds heavily-optimized matrix multiplication kernels
-# from NVIDIA's cuBLASLt library.
+# from NVIDIA's [cuBLAS library of linear algebra routines](https://docs.nvidia.com/cuda/cublas/).
 
 # We'll specify a number of plugins for our engine implementation.
 # The first is
 # [multiple profiles](https://github.com/NVIDIA/TensorRT-LLM/blob/b763051ba429d60263949da95c701efe8acf7b9c/docs/source/performance/performance-tuning-guide/useful-build-time-flags.md#multiple-profiles),
-# which configures TensorRT to prepare multiple kernels, optimized for different input sizes.
+# which configures TensorRT to prepare multiple kernels for each high-level operation,
+# where different kernels are optimized for different input sizes.
 # The second is `paged_kv_cache` which enables a
 # [paged attention algorithm](https://arxiv.org/abs/2309.06180)
 # for the key-value (KV) cache.
 
-# The last two parameters are GEMM plugins optimized specifically for low latency.
+# The last two parameters are GEMM plugins optimized specifically for low latency,
+# the `low_latency` plugins for `gemm` and `gemm_swiglu`.
+
 # The `low_latency_gemm_swiglu_plugin` plugin fuses the two matmul operations
 # and non-linearity of the feedforward component of the Transformer block into a single kernel,
 # reducing round trips between GPU
@@ -225,6 +229,7 @@ def get_calib_config():
 # and RAM. For details on kernel fusion, see
 # [this blog post by Horace He of Thinking Machines](https://horace.io/brrr_intro.html).
 # Note that at the time of writing, this only works for `FP8` on Hopper GPUs.
+
 # The `low_latency_gemm_plugin` is a variant of the GEMM plugin that brings in latency-optimized
 # kernels from NVIDIA's [CUTLASS library](https://github.com/NVIDIA/cutlass).
 
@@ -248,9 +253,12 @@ def get_plugin_config():
 # avoiding the auto-regressive bottleneck in the Transformer architecture.
 # Generating multiple tokens in parallel exposes more parallelism to the GPU.
 # It works best for text that has predicable patterns, like code,
-# but it's worth testing for any workload where latency is crtical.
+# but it's worth testing for any workload where latency is critical.
 
-# We'll use a simple speculative decoding strategy called lookahead decoding here:
+# Speculative decoding can use any technique to guess tokens, including running another,
+# smaller language model. Here, we'll use a simple, but popular and effective
+# speculative decoding strategy called "lookahead decoding",
+# which essentially guesses that token sequences from the past will occur again.
 
 
 def get_speculative_config():
@@ -266,9 +274,9 @@ def get_speculative_config():
 # ### Set the build config
 
 # Finally, we'll specify the overall build configuration for the engine. This includes
-# more typical parameters such as the max input length, the max number of tokens
-# to process at once before queueing occurs, and the max number of prompts
-# to process at once before queueing occurs:
+# more obvious parameters such as the maximum input length, the maximum number of tokens
+# to process at once before queueing occurs, and the maximum number of prompts
+# to process at once before queueing occurs.
 
 ALLOW_CONCURRENT_INPUTS = 1
 
@@ -294,19 +302,20 @@ def get_build_config():
 
 app = modal.App("trtllm-latency")
 
-# Thanks to our custom container runtime system even this large, many gigabyte container boots in seconds.
+# Thanks to our [custom container runtime system](https://modal.com/blog/jono-containers-talk),
+# even this large container boots in seconds.
 
-# On the first container start, we mount the volume, download the model, and build the engine,
-# which takes a few minutes.
-# But subsequent starts will be much faster, as the engine is cached in the volume.
+# On the first container start, we mount the Volume, download the model, and build the engine,
+# which takes a few minutes. Subsequent starts will be much faster,
+# as the engine is cached in the Volume and loaded in seconds.
 
-# Container starts are triggered when Modal scales up your infrastructure,
+# Container starts are triggered when Modal scales up your Function,
 # like the first time you run this code or the first time a request comes in after a period of inactivity.
 # For details on optimizing container start latency, see
 # [this guide](https://modal.com/docs/guide/cold-start).
 
 # Container lifecycles in Modal are managed via our `Cls` interface, so we define one below
-# to separate out the engine startup and engine execution.
+# to separate out the engine startup (`enter`) and engine execution (`generate`).
 # For details, see [this guide](https://modal.com/docs/guide/lifecycle-functions).
 
 MINUTES = 60  # seconds
@@ -373,22 +382,32 @@ class Model:
 
     @modal.method()
     def generate(self, prompt) -> dict:
-        SYSTEM_PROMPT = "You are a helpful, harmless, and honest AI assistant created by Meta."
-
-        messages = [
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": prompt},
-        ]
-
-        text = self.tokenizer.apply_chat_template(
-            messages, tokenize=False, add_generation_prompt=True
-        )
-
         start_time = time.perf_counter()
+        text = self.text_from_prompt(prompt)
         output = self.llm.generate(text, self.sampling_params)
         latency_ms = (time.perf_counter() - start_time) * 1000
 
         return output.outputs[0].text, latency_ms
+
+    @modal.method()
+    async def generate_async(self, prompt):
+        text = self.text_from_prompt(prompt)
+        async for output in self.llm.generate_async(
+            text, self.sampling_params, streaming=True
+        ):
+            yield output.outputs[0].text_diff
+
+    def text_from_prompt(self, prompt):
+        SYSTEM_PROMPT = "You are a helpful, harmless, and honest AI assistant created by Meta."
+
+        if isinstance(prompt, str):
+            prompt = [{"role": "user", "content": prompt}]
+
+        messages = [{"role": "system", "content": SYSTEM_PROMPT}] + prompt
+
+        return self.tokenizer.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
 
     @modal.method()
     def boot(self):
@@ -470,10 +489,60 @@ def main(mode: str = "fast"):
         print(f"Processed prompt in {latency_ms:.2f}ms")
         print(f"Prompt: {prompt}")
         print(f"Generated Text: {generated_text}")
-        print("🏎️" * 160)
+        print("🏎️ " * 40)
 
-    p50 = sorted(latencies_ms)[int(len(latencies_ms) * 0.5)]
-    p90 = sorted(latencies_ms)[int(len(latencies_ms) * 0.9)]
+    p50 = sorted(latencies_ms)[int(len(latencies_ms) * 0.5) - 1]
+    p90 = sorted(latencies_ms)[int(len(latencies_ms) * 0.9) - 1]
     print(
         f"🏎️  mode={mode} inference latency (p50, p90): ({p50:.2f}ms, {p90:.2f}ms)"
     )
+
+
+# Once deployed with `modal deploy`, this `Model.generate` function
+# can be called from other Python code. It can also be converted to an HTTP endpoint
+# for invocation over the Internet by any client.
+# For details, see [this guide](https://modal.com/docs/guide/trigger-deployed-functions).
+
+# As a quick demo, we've included some sample chat client code in the
+# Python main entrypoint below. To use it, first deploy with
+
+# ```bash
+# modal deploy trtllm_latency.py
+# ```
+
+# and then run the client with
+
+# ```python notest
+# python trtllm_latency.py
+# ```
+
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        Model = modal.Cls.from_name("trtllm-latency", "Model")
+        print("🏎️  connecting to model")
+        model = Model(mode="fast")
+        model.boot.remote()
+    except modal.exception.NotFoundError as e:
+        raise SystemError("Deploy this app first with modal deploy") from e
+
+    print("🏎️  starting chat. exit with :q, ctrl+C, or ctrl+D")
+    try:
+        prompt = []
+        while (nxt := input("🏎️  > ")) != ":q":
+            prompt.append({"role": "user", "content": nxt})
+            resp = ""
+            for out in model.generate_async.remote_gen(prompt):
+                print(out, end="", flush=True)
+                resp += out
+            print("\n")
+            prompt.append({"role": "assistant", "content": resp})
+    except KeyboardInterrupt:
+        pass
+    except SystemExit:
+        pass
+    finally:
+        print("\n")
+        sys.exit(0)
