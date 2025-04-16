27cf4456fbd53a3ac35495525a137e07981ffa53
shababo <benshababo@gmail.com>
2025-04-16 06:44:16 -0700

    access webcam clientside for websocket example

--- 07_web_endpoints/websockets_simple/websockets_simple.py
+++ 07_web_endpoints/websockets_simple/websockets_simple.py
@@ -3,40 +3,34 @@ from pathlib import Path
 
 static_path = Path(__file__).parent.resolve()
 
-app = modal.App("websockets-simple-text-echo")
+app = modal.App("websockets-video-echo")
 app.image = modal.Image.debian_slim().run_commands("pip install --upgrade pip").pip_install("fastapi", "websockets", "gradio").add_local_dir(static_path, remote_path="/assets")
 
 @app.cls()
 @modal.concurrent(max_inputs=100)
-class TextEcho:
-
-    
-
+class VideoEcho:
     @modal.enter()
     def init(self):
-        self.user_text = ""
+        pass
 
     @modal.asgi_app()
     def endpoint(self):
-
         from fastapi import FastAPI, WebSocket, Request
         from fastapi.responses import HTMLResponse
 
         web_app = FastAPI()
-        self.user_text = ""
 
         @web_app.websocket("/ws")
         async def websocket_handler(websocket: WebSocket) -> None:
             await websocket.accept()
             while True:
                 data = await websocket.receive_text()
-                self.user_text += data
-                await websocket.send_text(self.user_text)
+                # Simply echo back the image data
+                await websocket.send_text(data)
 
         @web_app.get("/")
         async def get(request: Request):
             html = open("/assets/index.html").read()
-            # Get the current URL and replace the protocol with wss
             base_url = str(request.url).replace("http", "ws")
             html = html.replace("url-placeholder", f"{base_url}ws")
             return HTMLResponse(html)
