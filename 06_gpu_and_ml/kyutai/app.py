c835d67031b9c77b95df1f58fbc74403e0f3b47b
Andrew Hinh <ajhinh@gmail.com>
2025-07-15 15:17:58 -0700

    Add example for Kyutai STT (#1256)
    * add kyutai eg; move parakeet eg
    
    * small fixes
    
    * remove react; add readme
    
    * refactor + add perf metrics
    
    * fix concurrent inp behavior
    
    * add kyutai eg; move parakeet eg
    
    * small fixes
    
    * remove react; add readme
    
    * refactor + add perf metrics
    
    * fix concurrent inp behavior
    
    * remove inp concurrency; fix ttft calc
    
    * revert ollmama file
    
    * move examples out of nested directory
    
    * fix paths
    
    * add local entrypoint stub
    
    * complete local entrypoint
    
    * add vad; fix timing; refactor
    
    * remove timing
    
    ---------
    
    Co-authored-by: Ben Shababo <shababo@users.noreply.github.com>
    Co-authored-by: shababo <ben.shababo@modal.com>

---
+++ 06_gpu_and_ml/kyutai/app.py
@@ -0,0 +1,429 @@
+import asyncio
+import base64
+import time
+from pathlib import Path
+
+import modal
+
+app = modal.App(name="kyutai-streaming-stt")
+
+stt_image = (
+    modal.Image.debian_slim(python_version="3.12")
+    .pip_install("uv")
+    .run_commands(
+        "uv pip install --system --compile-bytecode moshi==0.2.9 fastapi==0.115.14 hf_transfer==0.1.9 julius==0.2.7",
+    )
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+)
+
+MODEL_NAME = "kyutai/stt-1b-en_fr"
+
+hf_cache_vol = modal.Volume.from_name(f"{app.name}-hf-cache", create_if_missing=True)
+hf_cache_vol_path = Path("/root/.cache/huggingface")
+volumes = {hf_cache_vol_path: hf_cache_vol}
+
+MINUTES = 60
+
+
+@app.cls(
+    image=stt_image,
+    gpu="l40s:1",
+    volumes=volumes,
+    timeout=10 * MINUTES,
+)
+class STT:
+    BATCH_SIZE = 1
+
+    @modal.enter()
+    def enter(self):
+        import torch
+        from huggingface_hub import snapshot_download
+        from moshi.models import LMGen, loaders
+
+        start_time = time.monotonic_ns()
+
+        print("Downloading model if necessary...")
+        snapshot_download(MODEL_NAME)
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        checkpoint_info = loaders.CheckpointInfo.from_hf_repo(MODEL_NAME)
+        self.mimi = checkpoint_info.get_mimi(device=self.device)
+        self.frame_size = int(self.mimi.sample_rate / self.mimi.frame_rate)
+
+        self.moshi = checkpoint_info.get_moshi(device=self.device)
+        self.lm_gen = LMGen(
+            self.moshi,
+            # sampling params
+            temp=0,
+            temp_text=0,
+        )
+
+        self.mimi.streaming_forever(self.BATCH_SIZE)
+        self.lm_gen.streaming_forever(self.BATCH_SIZE)
+
+        self.text_tokenizer = checkpoint_info.get_text_tokenizer()
+
+        self.audio_silence_prefix_seconds = checkpoint_info.stt_config.get(
+            "audio_silence_prefix_seconds", 1.0
+        )
+        self.audio_delay_seconds = checkpoint_info.stt_config.get(
+            "audio_delay_seconds", 5.0
+        )
+        self.padding_token_id = checkpoint_info.raw_config.get(
+            "text_padding_token_id", 3
+        )
+
+        # warmup gpus
+        for _ in range(4):
+            codes = self.mimi.encode(
+                torch.zeros(self.BATCH_SIZE, 1, self.frame_size).to(self.device)
+            )
+            for c in range(codes.shape[-1]):
+                tokens = self.lm_gen.step(codes[:, :, c : c + 1])
+                if tokens is None:
+                    continue
+        torch.cuda.synchronize()
+
+        print(f"Model loaded in {round((time.monotonic_ns() - start_time) / 1e9, 2)}s")
+
+    def reset_state(self):
+        # reset llm chat history for this input
+        self.mimi.reset_streaming()
+        self.lm_gen.reset_streaming()
+
+    async def transcribe(self, pcm, all_pcm_data):
+        import numpy as np
+        import torch
+
+        if pcm is None:
+            yield all_pcm_data
+            return
+        if len(pcm) == 0:
+            yield all_pcm_data
+            return
+
+        if pcm.shape[-1] == 0:
+            yield all_pcm_data
+            return
+        if all_pcm_data is None:
+            all_pcm_data = pcm
+        else:
+            all_pcm_data = np.concatenate((all_pcm_data, pcm))
+
+        # infer on each frame
+        while all_pcm_data.shape[-1] >= self.frame_size:
+            chunk = all_pcm_data[: self.frame_size]
+            all_pcm_data = all_pcm_data[self.frame_size :]
+
+            with torch.no_grad():
+                chunk = torch.from_numpy(chunk)
+                chunk = chunk.unsqueeze(0).unsqueeze(0)  # (1, 1, frame_size)
+                chunk = chunk.expand(
+                    self.BATCH_SIZE, -1, -1
+                )  # (batch_size, 1, frame_size)
+                chunk = chunk.to(device=self.device)
+
+                # inference on audio chunk
+                codes = self.mimi.encode(chunk)
+
+                # language model inference against encoded audio
+                for c in range(codes.shape[-1]):
+                    text_tokens, vad_heads = self.lm_gen.step_with_extra_heads(
+                        codes[:, :, c : c + 1]
+                    )
+                    if text_tokens is None:
+                        # model is silent
+                        yield all_pcm_data
+                        return
+                    if vad_heads:
+                        pr_vad = vad_heads[2][0, 0, 0].cpu().item()
+                        if pr_vad > 0.5:
+                            # end of turn detected
+                            yield all_pcm_data
+                            return
+
+                    assert text_tokens.shape[1] == self.lm_gen.lm_model.dep_q + 1
+
+                    text_token = text_tokens[0, 0, 0].item()
+                    if text_token not in (0, 3):
+                        text = self.text_tokenizer.id_to_piece(text_token)
+                        text = text.replace("▁", " ")
+                        yield text
+
+        yield all_pcm_data
+
+    @modal.asgi_app()
+    def web(self):
+        import sphn
+        from fastapi import FastAPI, Response, WebSocket, WebSocketDisconnect
+
+        web_app = FastAPI()
+
+        @web_app.get("/status")
+        async def status():
+            return Response(status_code=200)
+
+        @web_app.websocket("/ws")
+        async def websocket_endpoint(ws: WebSocket):
+            await ws.accept()
+
+            opus_stream_inbound = sphn.OpusStreamReader(self.mimi.sample_rate)
+            transcription_queue = asyncio.Queue()
+
+            print("Session started")
+            tasks = []
+
+            # asyncio to run multiple loops concurrently within single websocket connection
+            async def recv_loop():
+                """
+                Receives Opus stream across websocket, appends into inbound queue.
+                """
+                nonlocal opus_stream_inbound
+                while True:
+                    data = await ws.receive_bytes()
+
+                    if not isinstance(data, bytes):
+                        print("received non-bytes message")
+                        continue
+                    if len(data) == 0:
+                        print("received empty message")
+                        continue
+                    opus_stream_inbound.append_bytes(data)
+
+            async def inference_loop():
+                """
+                Runs streaming inference on inbound data, and if any response audio is created, appends it to the outbound stream.
+                """
+                nonlocal opus_stream_inbound, transcription_queue
+                all_pcm_data = None
+
+                while True:
+                    await asyncio.sleep(0.001)
+
+                    pcm = opus_stream_inbound.read_pcm()
+                    async for msg in self.transcribe(pcm, all_pcm_data):
+                        if isinstance(msg, str):
+                            transcription_queue.put_nowait(msg)
+                        else:
+                            all_pcm_data = msg
+
+            async def send_loop():
+                """
+                Reads outbound data, and sends it across websocket
+                """
+                nonlocal transcription_queue
+                while True:
+                    data = await transcription_queue.get()
+
+                    if data is None:
+                        continue
+
+                    msg = b"\x01" + bytes(
+                        data, encoding="utf8"
+                    )  # prepend "\x01" as a tag to indicate text
+                    await ws.send_bytes(msg)
+
+            # run all loops concurrently
+            try:
+                tasks = [
+                    asyncio.create_task(recv_loop()),
+                    asyncio.create_task(inference_loop()),
+                    asyncio.create_task(send_loop()),
+                ]
+                await asyncio.gather(*tasks)
+
+            except WebSocketDisconnect:
+                print("WebSocket disconnected")
+                await ws.close(code=1000)
+            except Exception as e:
+                print("Exception:", e)
+                await ws.close(code=1011)  # internal error
+                raise e
+            finally:
+                for task in tasks:
+                    task.cancel()
+                await asyncio.gather(*tasks, return_exceptions=True)
+                self.reset_state()
+
+        return web_app
+
+    @modal.method()
+    async def local_transcribe(self, q: modal.Queue):
+        import tempfile
+
+        import sphn
+
+        all_pcm_data = None
+
+        while True:
+            chunk = await q.get.aio(partition="audio")
+            if chunk is None:
+                await q.put.aio(None, partition="transcription")
+                break
+
+            # to avoid having to encode the audio and retrieve with OpusStreamReader:
+            with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as tmp:
+                tmp.write(chunk)
+                tmp.flush()
+                pcm, _ = sphn.read(tmp.name)
+                pcm = pcm.squeeze(0)
+
+            async for msg in self.transcribe(pcm, all_pcm_data):
+                if isinstance(msg, str):
+                    await q.put.aio(msg, partition="transcription")
+                else:
+                    all_pcm_data = msg
+
+
+@app.local_entrypoint()
+async def test(
+    audio_url: str = "https://github.com/kyutai-labs/delayed-streams-modeling/raw/refs/heads/main/audio/bria.mp3",
+    chunk_size: int = 24000,
+    rtf: int = 1000,
+):
+    from urllib.request import urlopen
+
+    print(f"Downloading audio file from {audio_url}")
+    audio_bytes = urlopen(audio_url).read()
+    print(f"Downloaded {len(audio_bytes)} bytes")
+
+    print("Starting transcription")
+    start_time = time.monotonic_ns()
+    with modal.Queue.ephemeral() as q:
+        STT().local_transcribe.spawn(q)
+        send = asyncio.create_task(send_audio(audio_bytes, q, chunk_size, rtf))
+        recv = asyncio.create_task(receive_text(q))
+        await asyncio.gather(send, recv)
+    print(
+        f"\nTranscription complete in {round((time.monotonic_ns() - start_time) / 1e9, 2)}s"
+    )
+
+
+async def chunk_audio(data: bytes, chunk_size: int):
+    for i in range(0, len(data), chunk_size):
+        yield data[i : i + chunk_size]
+
+
+async def send_audio(audio_bytes, q, chunk_size, rtf):
+    async for chunk in chunk_audio(audio_bytes, chunk_size):
+        await q.put.aio(chunk, partition="audio")
+        await asyncio.sleep(chunk_size / chunk_size / rtf)
+    await q.put.aio(None, partition="audio")
+
+
+async def receive_text(q):
+    while True:
+        data = await q.get.aio(partition="transcription")
+        if data is None:
+            break
+        print(data, end="")
+
+
+web_image = (
+    modal.Image.debian_slim(python_version="3.12")
+    .pip_install("uv")
+    .run_commands(
+        "uv pip install --system --compile-bytecode python-fasthtml==0.12.20",
+    )
+    .add_local_dir(Path(__file__).parent / "frontend", "/root/frontend")
+)
+
+
+@app.function(
+    image=web_image,
+    timeout=10 * MINUTES,
+)
+@modal.concurrent(max_inputs=100)
+@modal.asgi_app()
+def web():
+    import fasthtml.common as fh
+
+    modal_logo_svg = open("/root/frontend/modal-logo.svg").read()
+    modal_logo_base64 = base64.b64encode(modal_logo_svg.encode()).decode()
+    app_js = open("/root/frontend/audio.js").read()
+
+    fast_app, rt = fh.fast_app(
+        hdrs=[
+            # Audio recording libraries
+            fh.Script(
+                src="https://cdn.jsdelivr.net/npm/opus-recorder@latest/dist/recorder.min.js"
+            ),
+            fh.Script(
+                src="https://cdn.jsdelivr.net/npm/opus-recorder@latest/dist/encoderWorker.min.js"
+            ),
+            fh.Script(
+                src="https://cdn.jsdelivr.net/npm/ogg-opus-decoder/dist/ogg-opus-decoder.min.js"
+            ),
+            # Styling
+            fh.Link(
+                href="https://fonts.googleapis.com/css?family=Inter:300,400,600",
+                rel="stylesheet",
+            ),
+            fh.Script(src="https://cdn.tailwindcss.com"),
+            fh.Script("""
+                tailwind.config = {
+                    theme: {
+                        extend: {
+                            colors: {
+                                ground: "#0C0F0B",
+                                primary: "#9AEE86",
+                                "accent-pink": "#FC9CC6",
+                                "accent-blue": "#B8E4FF",
+                            },
+                        },
+                    },
+                };
+            """),
+        ],
+    )
+
+    @rt("/")
+    def get():
+        return (
+            fh.Title(
+                "Kyutai Streaming STT",
+            ),
+            fh.Body(
+                fh.Div(
+                    fh.Div(
+                        fh.Div(
+                            id="text-output",
+                            cls="flex flex-col-reverse overflow-y-auto max-h-64 pr-2",
+                        ),
+                        cls="w-full overflow-y-auto max-h-64",
+                    ),
+                    cls="bg-gray-800 rounded-lg shadow-lg w-full max-w-xl p-6",
+                ),
+                fh.Footer(
+                    fh.Span(
+                        "Built with ",
+                        fh.A(
+                            "Kyutai",
+                            href="https://github.com/kyutai-labs/delayed-streams-modeling",
+                            target="_blank",
+                            rel="noopener noreferrer",
+                            cls="underline",
+                        ),
+                        " and",
+                        cls="text-sm font-medium text-gray-300 mr-2",
+                    ),
+                    fh.A(
+                        fh.Img(
+                            src=f"data:image/svg+xml;base64,{modal_logo_base64}",
+                            alt="Modal logo",
+                            cls="w-24",
+                        ),
+                        cls="flex items-center p-2 rounded-lg bg-gray-800 shadow-lg hover:bg-gray-700 transition-colors duration-200",
+                        href="https://modal.com",
+                        target="_blank",
+                        rel="noopener noreferrer",
+                    ),
+                    cls="fixed bottom-4 inline-flex items-center justify-center",
+                ),
+                fh.Script(app_js),
+                cls="relative bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center p-4",
+            ),
+        )
+
+    return fast_app
