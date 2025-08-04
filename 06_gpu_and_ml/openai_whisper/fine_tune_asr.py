ae634260476714c37cfc05dbb6c7b28058658bdc
Will Shainin <will@modal.com>
2025-08-04 12:49:06 -0700

    Importing finetune asr code

---
+++ 06_gpu_and_ml/openai_whisper/fine_tune_asr.py
@@ -0,0 +1,347 @@
+# ---
+# args: ["--test"]
+# ---
+
+# # Fine-tune Whisper to Improve Domain-Specific Transcription
+
+# This example demonstrates how to fine-tune an OpenAI Whisper model to improve the
+# model's abillity to transcribe domain-specific vocabulary.
+
+import os
+import time
+from dataclasses import dataclass
+from typing import Optional
+from pathlib import Path
+from typing import Any, Union
+
+import modal
+
+OUTPUT_DIR = "/outputs"
+cache_volume = modal.Volume.from_name("hf-hub-cache", create_if_missing=True)
+
+CACHE_DIR = "/cache"
+output_volume = modal.Volume.from_name(
+    "fine-tune-asr-example",  # TODO: rename to match examples repo
+    create_if_missing=True,
+)
+
+image = (
+    modal.Image.debian_slim(python_version="3.12")
+    .pip_install(
+        "accelerate",
+        "datasets==3.6.0",
+        "evaluate",
+        "huggingface_hub[hf_transfer]",
+        "jiwer",
+        "librosa",
+        "torch",
+        "torchaudio",
+        "transformers",
+    )
+    .env(
+        {
+            "HF_HUB_ENABLE_HF_TRANSFER": "1",
+            "HF_HUB_CACHE": CACHE_DIR,
+        }
+    )
+    .pip_install("ipdb", "IPython")  # TODO: REMOVE
+    .add_local_python_source("english_spelling_mapping")
+)
+
+with image.imports():
+    import datasets
+    import evaluate
+    import torch
+    import transformers
+    from transformers.models.whisper.english_normalizer import (
+        EnglishTextNormalizer,
+    )
+    from transformers.trainer_utils import get_last_checkpoint, is_main_process
+
+    from english_spelling_mapping import english_spelling_mapping
+
+
+app = modal.App(
+    name="example-whisper-fine-tune",
+)
+
+# TODO: REMOVE
+app.set_description(os.environ.get("APP_NAME", app.name))
+
+
+@dataclass
+class Config:
+    """Configuration for the fine-tuning run."""
+
+    # Model config
+    model_name: str = "openai/whisper-tiny.en"
+
+    # Dataset config
+    dataset_name: str = "speechcolab/gigaspeech"
+    dataset_subset: str = "s"  # "small"
+    dataset_category: int = 15  # "Science and Technology"
+    max_duration_in_seconds: float = 20.0
+    min_duration_in_seconds: float = 0.0
+
+    # Training config
+    num_train_epochs: int = 5
+    warmup_steps: int = 400
+    max_steps: int = -1
+
+
+@app.local_entrypoint()
+def main(test: bool = False):
+    if test:
+        config = Config(
+            dataset_subset="xs",
+            num_train_epochs=1.0,
+            warmup_steps=0,
+            max_steps=1,
+        )
+    else:
+        config = Config()
+
+    start = time.perf_counter()
+    train.remote(config)
+    print(f"Training took {time.perf_counter() - start:.6f} seconds")
+
+
+@app.function(
+    image=image,
+    secrets=[modal.Secret.from_name("huggingface-secret", required_keys=["HF_TOKEN"])],
+    gpu="H100!",
+    volumes={CACHE_DIR: cache_volume, OUTPUT_DIR: output_volume},
+    timeout=24 * 60 * 60,  # TODO: Tighten up
+)
+def train(
+    config: Config,
+):
+    training_args = transformers.Seq2SeqTrainingArguments(
+        length_column_name="input_length",
+        output_dir=Path(OUTPUT_DIR) / app.app_id,
+        num_train_epochs=config.num_train_epochs,
+        per_device_train_batch_size=64,
+        per_device_eval_batch_size=64,
+        gradient_accumulation_steps=1,
+        learning_rate=1e-5,
+        warmup_steps=config.warmup_steps,
+        max_steps=config.max_steps,
+        eval_strategy="steps",
+        save_total_limit=3,
+        fp16=True,
+        group_by_length=True,
+        predict_with_generate=True,
+        generation_max_length=40,
+        generation_num_beams=1,
+    )
+
+    print("Starting training run")
+    print(f"Starting training. Weights will be saved to '{training_args.output_dir}'")
+
+    print("Loading models")
+
+    feature_extractor = transformers.WhisperFeatureExtractor.from_pretrained(
+        pretrained_model_name_or_path=config.model_name,
+        cache_dir=CACHE_DIR,
+    )
+    tokenizer = transformers.WhisperTokenizer.from_pretrained(
+        pretrained_model_name_or_path=config.model_name,
+        cache_dir=CACHE_DIR,
+        # language="en",
+        # task="transcribe",
+    )
+    model = transformers.WhisperForConditionalGeneration.from_pretrained(
+        pretrained_model_name_or_path=config.model_name,
+        cache_dir=CACHE_DIR,
+    )
+    # TODO: API has changed, still bw compatible, but throws warnings
+    # model.generation_config.language = "en"
+    # model.generation_config.task = "transcribe"
+    # model.generation_config.forced_decoder_ids = None
+
+    print("Loading dataset")
+    ds = datasets.load_dataset(
+        config.dataset_name,
+        config.dataset_subset,
+        split="train",  # The test and val splits
+        cache_dir=CACHE_DIR,
+        num_proc=os.cpu_count(),
+        trust_remote_code=True,
+    )
+
+    print("Preparing data")
+    # Remove all rows that aren't in our category
+    ds = ds.select(
+        [i for i, c in enumerate(ds["category"]) if c == config.dataset_category]
+    )
+
+    # Remove all columns except for the audio and text transcription
+    ds = ds.select_columns(["text", "audio"])
+
+    raw_datasets = ds.train_test_split(test_size=0.1, shuffle=True, seed=42)
+
+    # We need to read the audio files as arrays and tokenize the targets.
+    max_input_length = config.max_duration_in_seconds * feature_extractor.sampling_rate
+    min_input_length = config.min_duration_in_seconds * feature_extractor.sampling_rate
+    model_input_name = feature_extractor.model_input_names[0]
+
+    def prepare_dataset(batch):
+        # process audio
+        sample = batch["audio"]
+        inputs = feature_extractor(
+            sample["array"],
+            sampling_rate=sample["sampling_rate"],
+        )
+        batch[model_input_name] = inputs.get(model_input_name)[0]
+        batch["input_length"] = len(sample["array"])
+
+        # Normalize text
+        normalized = (
+            batch["text"]
+            .replace(" <COMMA>", ",")
+            .replace(" <PERIOD>", ".")
+            .replace(" <QUESTIONMARK>", "?")
+            .replace(" <EXCLAMATIONPOINT>", "!")
+            .lower()
+            .strip()
+        )
+
+        batch["labels"] = tokenizer(normalized).input_ids
+
+        return batch
+
+    vectorized_datasets = raw_datasets.map(
+        prepare_dataset,
+        remove_columns=next(iter(raw_datasets.values())).column_names,
+        num_proc=os.cpu_count(),
+        desc="Preprocessing dataset",
+    )
+
+    vectorized_datasets = vectorized_datasets.filter(
+        lambda length: length > min_input_length and length < max_input_length,
+        num_proc=os.cpu_count(),
+        input_columns=["input_length"],
+    )
+
+    normalizer = EnglishTextNormalizer(english_spelling_mapping)
+    metric = evaluate.load("wer")
+
+    def compute_metrics(pred):
+        pred_ids = pred.predictions
+
+        pred.label_ids[pred.label_ids == -100] = tokenizer.pad_token_id
+
+        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+        norm_pred_str = [normalizer(s).strip() for s in pred_str]
+
+        label_str = tokenizer.batch_decode(pred.label_ids, skip_special_tokens=True)
+        norm_label_str = [normalizer(s).strip() for s in label_str]
+
+        wer = metric.compute(predictions=norm_pred_str, references=norm_label_str)
+        return {"wer": wer}
+
+    processor = transformers.WhisperProcessor(
+        feature_extractor=feature_extractor,
+        tokenizer=tokenizer,
+    )
+
+    data_collator = DataCollatorSpeechSeq2SeqWithPadding(
+        processor=processor,
+        decoder_start_token_id=model.config.decoder_start_token_id,
+    )
+
+    trainer = transformers.Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=vectorized_datasets["train"],
+        eval_dataset=vectorized_datasets["test"],
+        processing_class=feature_extractor,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+
+    print("Run evals before training")
+    metrics = trainer.evaluate(
+        metric_key_prefix="test",
+        max_length=training_args.generation_max_length,
+        num_beams=training_args.generation_num_beams,
+    )
+    metrics["eval_samples"] = len(vectorized_datasets["test"])
+
+    trainer.log_metrics("test", metrics)
+    trainer.save_metrics("test", metrics)
+
+    print("Starting training loop")
+    train_result = trainer.train()
+    trainer.save_model()  # Saves the feature extractor too for easy upload
+    print(f"Model saved in '{training_args.output_dir}'")
+
+    metrics = train_result.metrics
+    metrics["train_samples"] = len(vectorized_datasets["train"])
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+    output_volume.commit()
+
+    print("Running final evals")
+    metrics = trainer.evaluate(
+        metric_key_prefix="test",
+        max_length=training_args.generation_max_length,
+        num_beams=training_args.generation_num_beams,
+    )
+    metrics["eval_samples"] = len(vectorized_datasets["test"])
+
+    trainer.log_metrics("test", metrics)
+    trainer.save_metrics("test", metrics)
+
+    print("Training complete!")
+
+
+@dataclass
+class DataCollatorSpeechSeq2SeqWithPadding:
+    """
+    Data collator that will dynamically pad the inputs received.
+    Args:
+        processor ([`WhisperProcessor`])
+            The processor used for processing the data.
+        decoder_start_token_id (`int`)
+            The begin-of-sentence of the decoder.
+    """
+
+    processor: Any
+    decoder_start_token_id: int
+
+    def __call__(
+        self, features: list[dict[str, Union[list[int], torch.Tensor]]]
+    ) -> dict[str, torch.Tensor]:
+        # split inputs and labels since they have to be of different lengths and need
+        # different padding methods
+        model_input_name = self.processor.model_input_names[0]
+        input_features = [
+            {model_input_name: feature[model_input_name]} for feature in features
+        ]
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
+
+        batch = self.processor.feature_extractor.pad(
+            input_features,
+            return_tensors="pt",
+            return_attention_mask=True,
+            padding=True,
+        )
+
+        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
+
+        # replace padding with -100 to ignore loss correctly
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
+
+        # if bos token is appended in previous tokenization step,
+        # cut bos token here as it's appended later anyways
+        # TODO: Is this necessary?
+        if (labels[:, 0] == self.decoder_start_token_id).all().cpu().item():
+            labels = labels[:, 1:]
+
+        batch["labels"] = labels
+
+        return batch
