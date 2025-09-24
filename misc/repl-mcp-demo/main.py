b8c8822425a583ea5e0c966873808f25ba0d3beb
Aadit Juneja <aaditjuneja@Aadits-MacBook-Air.local>
2025-09-24 09:36:43 -0400

    repl mcp demo

---
+++ misc/repl-mcp-demo/main.py
@@ -0,0 +1,80 @@
+import contextlib
+import os
+from typing import List, Optional
+
+import dotenv
+from mcp.server.fastmcp import FastMCP
+from repl import CommandResponse, Repl
+
+dotenv.load_dotenv()
+
+
+"""
+This file specifies the MCP server and its tool with the Claude Desktop app.
+It is automatically ran when the Claude Desktop app is open provided one appropriately
+configures their `claude_desktop_config.json` file as specified in the README.
+"""
+
+sessionRepl: Optional[Repl] = None
+snapshot_id_store_file = os.path.expanduser(os.getenv("SNAPSHOT_ID_FILE_PATH"))
+
+
+mcp = FastMCP("modalrepl")
+
+
+# This tool creates a new repl with the specified timeout and packages.
+@mcp.tool()
+async def create_repl(timeout: int = 600, packages: List[str] = []) -> None:
+    # default timeout is 10 minute
+    try:
+        packages.extend(["fastapi", "uvicorn", "pydantic"])
+        with (
+            contextlib.redirect_stdout(open(os.devnull, "w")),
+            contextlib.redirect_stderr(open(os.devnull, "w")),
+        ):
+            repl = await Repl.create(packages=packages, timeout=timeout)
+        global sessionRepl
+        sessionRepl = repl
+    except Exception as exc:
+        raise RuntimeError(f"Error creating REPL. {exc}")
+
+
+# This tool executes a command in the current repl.
+@mcp.tool()
+async def exec_cmd(command: str) -> CommandResponse:
+    try:
+        if sessionRepl is None:
+            raise RuntimeError("REPL not created")
+        commands = Repl.parse_command(command)
+        res = await sessionRepl.run(commands)
+        return res
+    except Exception as exc:
+        raise RuntimeError(f"Error executing command: {exc}")
+
+
+# This tool restores a repl from a snapshot.
+@mcp.tool()
+async def get_repl_from_snapshot() -> None:
+    try:
+        with open(snapshot_id_store_file, "r") as f:
+            snapshot_id = f.read()
+        repl = await Repl.from_snapshot(snapshot_id)
+        global sessionRepl
+        sessionRepl = repl
+    except Exception as exc:
+        raise RuntimeError(f"Error getting REPL from snapshot: {exc}")
+
+
+@mcp.tool()  # This tool saves the snapshot id to a file
+def end_repl_and_save_snapshot():
+    try:
+        if sessionRepl:
+            snapshot_id = sessionRepl.kill()
+            with open(snapshot_id_store_file, "w") as f:
+                f.write(snapshot_id)
+    except Exception as exc:
+        raise RuntimeError(f"Error shutting down REPL: {exc}")
+
+
+if __name__ == "__main__":
+    mcp.run()
---
+++ misc/repl-mcp-demo/repl_server.py
@@ -0,0 +1,55 @@
+# Copyright Modal Labs 2025
+import io
+from contextlib import redirect_stdout
+from typing import Any, Dict, List, Literal, Tuple
+
+import uvicorn
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+
+"""
+This file specifies the HTTP server that is running inside of the sandbox.
+It is used to execute commands in the repl.
+
+"""
+
+app = FastAPI(title="REPL Server")
+
+
+_exec_context: Dict[str, Any] = {"__builtins__": __builtins__}
+
+
+class ReplCommand(BaseModel):
+    code: List[Tuple[str, Literal["exec", "eval"]]]
+
+
+class ReplCommandResponse(BaseModel):
+    result: str
+    stdout: str
+
+
+@app.post("/", status_code=status.HTTP_200_OK)
+def run_exec(body: ReplCommand) -> ReplCommandResponse:
+    commands = body.code
+    stdout_redir_buffer = io.StringIO()
+    try:
+        for command in commands:
+            if command[1] == "exec":
+                with redirect_stdout(
+                    stdout_redir_buffer
+                ):  # redirects stdout to a string buffer
+                    exec(command[0], _exec_context, _exec_context)
+            else:
+                with redirect_stdout(stdout_redir_buffer):
+                    res = eval(command[0], _exec_context, _exec_context)
+                stdout = stdout_redir_buffer.getvalue()
+                return ReplCommandResponse(result=str(res), stdout=stdout)
+        return ReplCommandResponse(
+            result="", stdout=stdout_redir_buffer.getvalue()
+        )  # just send back blank str if all commands are exec'd
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=repr(exc))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
