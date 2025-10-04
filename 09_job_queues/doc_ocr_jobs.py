35169bb44ad06b96cdc7bd231b3ab2d614916e7f
Charles Frye <charles@modal.com>
2025-10-03 18:06:52 -0700

    docs_ocr - update frontend update backend (#1368)
    * migrate to marker model
    
    * fix e2e
    
    * formatting
    
    * fix example
    
    * fix example
    
    * update
    
    * fix gpu snapshotting
    
    * simpler example
    
    * simpler example
    
    * fix grammar and code
    
    * fix grammar and code
    
    * clean
    
    * fix grammar
    
    * remove warn
    
    * clean
    
    * cleaner response
    
    * update frontend and backend
    
    * revert change to tailwind css
    
    ---------
    
    Co-authored-by: Aniket Maurya <theaniketmaurya@gmail.com>

--- 09_job_queues/doc_ocr_jobs.py
+++ 09_job_queues/doc_ocr_jobs.py
@@ -3,18 +3,19 @@
 # mypy: ignore-errors
 # ---
 
-# # Run a job queue for GOT-OCR
+# # Run a job queue that turns documents into structured data with Datalab Marker
 
 # This tutorial shows you how to use Modal as an infinitely scalable job queue
-# that can service async tasks from a web app. For the purpose of this tutorial,
-# we've also built a [React + FastAPI web app on Modal](https://modal.com/docs/examples/doc_ocr_webapp)
+# that can service async tasks from a web app.
+
+# Our job queue will handle a single task: converting images/PDFs into structured data.
+# We'll use [Marker](https://github.com/datalab-to/marker) from [Datalab](https://www.datalab.to),
+# which can convert images of documents or PDFs to Markdown, JSON, and HTML.
+
+# For the purpose of this tutorial, we've also built a [React + FastAPI web app on Modal](https://modal.com/docs/examples/doc_ocr_webapp)
 # that works together with it, but note that you don't need a web app running on Modal
 # to use this pattern. You can submit async tasks to Modal from any Python
 # application (for example, a regular Django app running on Kubernetes).
-
-# Our job queue will handle a single task: running OCR transcription for images of receipts.
-# We'll make use of a pre-trained model:
-# the [General OCR Theory (GOT) 2.0 model](https://huggingface.co/stepfun-ai/GOT-OCR2_0).
 
 # Try it out for yourself [here](https://modal-labs-examples--example-doc-ocr-webapp-wrapper.modal.run/).
 
@@ -23,87 +24,55 @@
 # ## Define an App
 
 # Let's first import `modal` and define an [`App`](https://modal.com/docs/reference/modal.App).
-# Later, we'll use the name provided for our `App` to find it from our web app and submit tasks to it.
+# Later, we'll use the name provided for our job queue App to find it from our web app and submit tasks to it.
 
 from typing import Optional
 
 import modal
+from typing_extensions import Literal
 
 app = modal.App("example-doc-ocr-jobs")
 
-# We also define the dependencies for our Function by specifying an
+# We also define the dependencies we need by specifying an
 # [Image](https://modal.com/docs/guide/images).
 
-inference_image = modal.Image.debian_slim(python_version="3.12").pip_install(
-    "accelerate==0.28.0",
-    "huggingface_hub[hf_transfer]==0.27.1",
-    "numpy<2",
-    "tiktoken==0.6.0",
-    "torch==2.5.1",
-    "torchvision==0.20.1",
-    "transformers==4.48.0",
-    "verovio==4.3.1",
+inference_image = modal.Image.debian_slim(python_version="3.12").uv_pip_install(
+    "marker-pdf[full]==1.9.3", "torch==2.8.0"
 )
 
 # ## Cache the pre-trained model on a Modal Volume
 
-# We can obtain the pre-trained model we want to run from Hugging Face
-# using its name and a revision identifier.
-
-MODEL_NAME = "ucaslcl/GOT-OCR2_0"
-MODEL_REVISION = "cf6b7386bc89a54f09785612ba74cb12de6fa17c"
-
-# The logic for loading the model based on this information
-# is encapsulated in the `setup` function below.
+# We can obtain the pre-trained model we want to run from Datalab
+# by using the Marker library.
 
 
-def setup():
-    import warnings
+def load_models():
+    import marker.models
 
-    from transformers import AutoModel, AutoTokenizer
+    print("loading models")
 
-    with warnings.catch_warnings():  # filter noisy warnings from GOT modeling code
-        warnings.simplefilter("ignore")
-        tokenizer = AutoTokenizer.from_pretrained(
-            MODEL_NAME, revision=MODEL_REVISION, trust_remote_code=True
-        )
-
-        model = AutoModel.from_pretrained(
-            MODEL_NAME,
-            revision=MODEL_REVISION,
-            trust_remote_code=True,
-            device_map="cuda",
-            use_safetensors=True,
-            pad_token_id=tokenizer.eos_token_id,
-        )
-
-    return tokenizer, model
+    return marker.models.create_model_dict()
 
 
-# The `.from_pretrained` methods from Hugging Face are smart enough
-# to only download models if they haven't been downloaded before.
-# But in Modal's serverless environment, filesystems are ephemeral,
-# and so using this code alone would mean that models need to get downloaded
-# on every request.
+# The `create_model_dict` function downloads model weights from Datalab's
+# cloud storage (S3 bucket) if they aren't already present in the filesystem.
+# However, in Modal's serverless environment, filesystems are ephemeral,
+# so using this code alone would mean that models need to be downloaded
+# many times (every time a new instance of our Function spins up).
 
 # So instead, we create a Modal [Volume](https://modal.com/docs/guide/volumes)
-# to store the model -- a durable filesystem that any Modal Function can access.
+# to store the models. Each Modal Volume is a durable filesystem that any Modal Function can access.
+# You can read more about storing model weights on Modal in [our guide](https://modal.com/docs/guide/model-weights).
 
-model_cache = modal.Volume.from_name("hf-hub-cache", create_if_missing=True)
-
-# We also update the environment variables for our Function
-# to include this new path for the model cache --
-# and to enable fast downloads with the `hf_transfer` library.
-
-MODEL_CACHE_PATH = "/root/models"
-inference_image = inference_image.env(
-    {"HF_HUB_CACHE": MODEL_CACHE_PATH, "HF_HUB_ENABLE_HF_TRANSFER": "1"}
+marker_cache_path = "/root/.cache/datalab/"
+marker_cache_volume = modal.Volume.from_name(
+    "marker-models-modal-demo", create_if_missing=True
 )
+marker_cache = {marker_cache_path: marker_cache_volume}
 
+# ## Run Datalab Marker on Modal
 
-# ## Run OCR inference on Modal by wrapping with `app.function`
-
-# Now let's set up the actual OCR inference.
+# Now let's set up the actual inference.
 
 # Using the [`@app.function`](https://modal.com/docs/reference/modal.App#function)
 # decorator, we set up a Modal [Function](https://modal.com/docs/reference/modal.Function).
@@ -114,30 +83,114 @@ inference_image = inference_image.env(
 # automatically [retry](https://modal.com/docs/guide/retries#function-retries) failures up to 3 times,
 # and have access to our [shared model cache](https://modal.com/docs/guide/volumes).
 
+# Inside the Function, we write out our inference logic,
+# which mostly involves configuring components provided by the `marker` library.
 
-@app.function(
-    gpu="l40s",
-    retries=3,
-    volumes={MODEL_CACHE_PATH: model_cache},
-    image=inference_image,
-)
-def parse_receipt(image: bytes) -> str:
+
+@app.function(gpu="l40s", retries=3, volumes=marker_cache, image=inference_image)
+def parse_document(
+    document: bytes,
+    page_range: str | None = None,
+    force_ocr: bool = False,
+    paginate_output: bool = False,
+    output_format: Literal["markdown", "html", "chunks", "json"] = "markdown",
+    use_llm: bool = False,
+) -> str | dict:
+    """
+    Args:
+        document: Document data (PDF, JPG, PNG) as bytes.
+        page_range: Specify which pages to process. Accepts comma-separated page numbers and ranges.
+        force_ocr: Force OCR processing on the entire document, even for pages that might contain extractable text.
+                    This will also format inline math properly.
+        paginate_output: Paginates the output, using \n\n{PAGE_NUMBER} followed by - * 48, then \n\n
+        output_format: Output format. Can be markdown, JSON, HTML, or chunks.
+        use_llm: use an llm to improve the marker results.
+    """
     from tempfile import NamedTemporaryFile
 
-    tokenizer, model = setup()
+    import marker.config.parser
+    import marker.converters.pdf
+    import marker.output
 
-    with NamedTemporaryFile(delete=False, mode="wb+") as temp_img_file:
-        temp_img_file.write(image)
-        output = model.chat(tokenizer, temp_img_file.name, ocr_type="format")
+    models = load_models()
 
-    print("Result: ", output)
+    # Set up document "converter"
+    config = {
+        "page_range": page_range,
+        "force_ocr": force_ocr,
+        "paginate_output": paginate_output,
+        "output_format": output_format,
+        "use_llm": use_llm,
+    }
 
-    return output
+    config_parser = marker.config.parser.ConfigParser(config)
+    config_dict = config_parser.generate_config_dict()
+    config_dict["pdftext_workers"] = 1
+
+    converter = marker.converters.pdf.PdfConverter(
+        config=config_dict,
+        artifact_dict=models,
+        processor_list=config_parser.get_processors(),
+        renderer=config_parser.get_renderer(),
+        llm_service=config_parser.get_llm_service() if use_llm else None,
+    )
+
+    # Run the converter on our document
+    with NamedTemporaryFile(delete=False, mode="wb+") as temp_path:
+        temp_path.write(document)
+        rendered_output = converter(temp_path.name)
+
+    # Format the output and return it
+    if output_format == "json":
+        result = rendered_output.model_dump_json()
+    else:
+        text, _, images = marker.output.text_from_rendered(rendered_output)
+
+        result = text
+
+    return result
 
 
-# ## Deploy
+# ## Testing and debugging remote code
 
-# Now that we have a function, we can publish it by deploying the app:
+# To make sure this code works, we want a way to kick the tires and debug it.
+
+# We can run it on Modal, with no need to set up separate local testing,
+# by adding a [`local_entrypoint`](https://modal.com/docs/reference/modal.App#local_entrypoint)
+# that invokes the Function `.remote`ly.
+
+
+@app.local_entrypoint()
+def main(document_filename: Optional[str] = None):
+    import urllib.request
+    from pathlib import Path
+
+    if document_filename is None:
+        document_filename = Path(__file__).parent / "receipt.png"
+    else:
+        document_filename = Path(document_filename)
+
+    if document_filename.exists():
+        image = document_filename.read_bytes()
+        print(f"running OCR on {document_filename}")
+    else:
+        document_url = "https://modal-cdn.com/cdnbot/Brandys-walmart-receipt-8g68_a_hk_f9c25fce.webp"
+        print(f"running OCR on sample from URL {document_url}")
+        request = urllib.request.Request(document_url)
+        with urllib.request.urlopen(request) as response:
+            image = response.read()
+    print(parse_document.remote(image, output_format="html"))
+
+
+# You can then run this from the command line with:
+
+# ```shell
+# modal run doc_ocr_jobs.py
+# ```
+
+# ## Deploying the document conversion service
+
+# Now that we have a Function, we can publish it by deploying the App:
 
 # ```shell
 # modal deploy doc_ocr_jobs.py
@@ -147,44 +200,11 @@ def parse_receipt(image: bytes) -> str:
 # from another Python process and submit tasks to it:
 
 # ```python
-# fn = modal.Function.from_name("example-doc-ocr-jobs", "parse_receipt")
-# fn.spawn(my_image)
+# fn = modal.Function.from_name("example-doc-ocr-jobs", "parse_document")
+# fn.spawn(my_document)
 # ```
 
 # Modal will auto-scale to handle all the tasks queued, and
 # then scale back down to 0 when there's no work left. To see how you could use this from a Python web
 # app, take a look at the [receipt parser frontend](https://modal.com/docs/examples/doc_ocr_webapp)
 # tutorial.
-
-# ## Run manually
-
-# We can also trigger `parse_receipt` manually for easier debugging:
-
-# ```shell
-# modal run doc_ocr_jobs
-# ```
-
-# To try it out, you can find some
-# example receipts [here](https://drive.google.com/drive/folders/1S2D1gXd4YIft4a5wDtW99jfl38e85ouW).
-
-
-@app.local_entrypoint()
-def main(receipt_filename: Optional[str] = None):
-    import urllib.request
-    from pathlib import Path
-
-    if receipt_filename is None:
-        receipt_filename = Path(__file__).parent / "receipt.png"
-    else:
-        receipt_filename = Path(receipt_filename)
-
-    if receipt_filename.exists():
-        image = receipt_filename.read_bytes()
-        print(f"running OCR on {receipt_filename}")
-    else:
-        receipt_url = "https://modal-cdn.com/cdnbot/Brandys-walmart-receipt-8g68_a_hk_f9c25fce.webp"
-        request = urllib.request.Request(receipt_url)
-        with urllib.request.urlopen(request) as response:
-            image = response.read()
-        print(f"running OCR on sample from URL {receipt_url}")
-    print(parse_receipt.remote(image))
