4e2c8cead91852c07ad5ea5af9831d3fdb25a25e
Aadit Juneja <aadit@modal.com>
2025-10-30 19:29:13 +0000

    ruff fixes

--- misc/serverless-ssh-proxy/deploy_script.py
+++ misc/serverless-ssh-proxy/deploy_script.py
@@ -1,8 +1,6 @@
-import modal
 import os
-from pathlib import Path
 
-
+import modal
 
 app = modal.App("ssh-proxy-app")
 
@@ -19,23 +17,6 @@ ssh_proxy_image = (
 
 
 
-# modal_toml_path = Path.home() / ".modal.toml"
-
-
-# def get_active_profile_credentials(toml_path):
-#     data = toml.load(toml_path)
-#     for profile_name, profile in data.items():
-#         if isinstance(profile, dict) and profile.get('active') is True:
-#             token_id = profile.get('token_id')
-#             token_secret = profile.get('token_secret')
-#             if token_id and token_secret:
-#                 return token_id, token_secret
-#     raise RuntimeError("No active profile with token_id and token_secret found in {}".format(toml_path))
-
-# try:
-#     modal_token_id, modal_token_secret = get_active_profile_credentials(modal_toml_path)
-# except Exception as e:
-#     raise RuntimeError(f"Failed to load Modal credentials: {e}")
 
 
 modal_secrets = {
@@ -55,12 +36,11 @@ modal_secrets = {
 )
 def run_ssh_proxy():
     import subprocess
-    import os
     subprocess.run(["ls", "/ssh-proxy"], check=True)
     with modal.forward(22, unencrypted=True) as tunnel:
         (hostname, port) = tunnel.tcp_socket
 
-        print(f"""Tunnel created\n. Use the following SSH command to connect: 
+        print(f"""Tunnel created\n. Use the following SSH command to connect:
         ssh -p {port} -i [private_key_path] user@{hostname}
         Note that in this implementation user can be any string.""")
         result = subprocess.run(["/root/.cargo/bin/cargo", "run", "--release", "--bin", "ssh_proxy"], check=True, cwd="/ssh-proxy")
