4738c5e19ba53d7c3c719ec32a0a6a4f2a2dd21d
molocule <34072934+molocule@users.noreply.github.com>
2025-12-17 11:51:28 -0500

    fix

--- 06_gpu_and_ml/llm-serving/sglang_low_latency.py
+++ 06_gpu_and_ml/llm-serving/sglang_low_latency.py
@@ -18,9 +18,12 @@
 # We'll use the [SGLang inference server](https://github.com/sgl-project/sglang).
 # Note that we need to build the SGLang image from source since the official image does not support the `--enable-cpu-backup` flag.
 
+import json
 import subprocess
 import time
+from typing import Any
 
+import aiohttp
 import modal
 import modal.experimental
 
@@ -32,6 +35,7 @@ MODEL_REVISION = (
 
 PORT = 8000
 MIN_CONTAINERS = 1
+MINUTE = 60
 
 HF_CACHE_VOL: modal.Volume = modal.Volume.from_name(
     "huggingface-cache", create_if_missing=True
@@ -71,7 +75,7 @@ app = modal.App(name=APP_NAME)
     experimental_options={"enable_gpu_snapshot": True},
     region="us-east",
     min_containers=MIN_CONTAINERS,
-    timeout=6 * 60,
+    timeout=4 * MINUTE,
 )
 @modal.experimental.http_server(
     port=PORT, proxy_regions=["us-east"], exit_grace_period=5
@@ -125,7 +129,7 @@ class SGLang:
             ).raise_for_status()
 
     @staticmethod
-    def _wait_ready(process: subprocess.Popen, timeout: int = 5 * 60):
+    def _wait_ready(process: subprocess.Popen, timeout: int = 3 * MINUTE):
         def check_process_is_running() -> Exception | None:
             if process is not None and process.poll() is not None:
                 return Exception(
@@ -163,166 +167,132 @@ class SGLang:
             json={},
         ).raise_for_status()
 
-# # ## Deploy the server
+## Deploy the server
 
-# # To deploy the API on Modal, just run
-# # ```bash
-# # modal deploy ministral3_inference.py
-# # ```
+# To deploy the API on Modal, just run
+# ```bash
+# modal deploy sglang_low_latency.py
+# ```
 
-# # This will create a new app on Modal, build the container image for it if it hasn't been built yet,
-# # and deploy the app.
+# This will create a new app on Modal, build the container image for it if it hasn't been built yet,
+# and deploy the app.
 
-# # ## Interact with the server
+## Interact with the server
 
-# # Once it is deployed, you'll see a URL appear in the command line,
-# # something like `https://your-workspace-name--example-ministral3-inference-serve.modal.run`.
+# Once it is deployed, you'll see a URL appear in the command line,
+# something like `https://your-workspace-name--example-sglang-low-latency-serve.us-east.modal.direct`.
 
-# # You can find [interactive Swagger UI docs](https://swagger.io/tools/swagger-ui/)
-# # at the `/docs` route of that URL, i.e. `https://your-workspace-name--example-ministral-inference-serve.modal.run/docs`.
-# # These docs describe each route and indicate the expected input and output
-# # and translate requests into `curl` commands.
+# You can find [interactive Swagger UI docs](https://swagger.io/tools/swagger-ui/)
+# at the `/docs` route of that URL, i.e. `https://your-workspace-name--example-sglang-low-latency-serve.us-east.modal.direct/docs`.
+# These docs describe each route and indicate the expected input and output
+# and translate requests into `curl` commands.
 
-# # For simple routes like `/health`, which checks whether the server is responding,
-# # you can even send a request directly from the docs.
+# For simple routes like `/health`, which checks whether the server is responding,
+# you can even send a request directly from the docs.
 
-# # To interact with the API programmatically in Python, we recommend the `openai` library.
+## Test the server
 
-# # ## Test the server
+# To make it easier to test the server setup, we also include a `local_entrypoint`
+# that does a healthcheck and then hits the server.
 
-# # To make it easier to test the server setup, we also include a `local_entrypoint`
-# # that does a healthcheck and then hits the server.
+# If you execute the command
 
-# # If you execute the command
+# ```bash
+# modal run sglang_low_latency.py
+# ```
 
-# # ```bash
-# # modal run ministral3_inference.py
-# # ```
+# a fresh replica of the server will be spun up on Modal while
+# the code below executes on your local machine.
 
-# # a fresh replica of the server will be spun up on Modal while
-# # the code below executes on your local machine.
-
-# # Think of this like writing simple tests inside of the `if __name__ == "__main__"`
-# # block of a Python script, but for cloud deployments!
+# Think of this like writing simple tests inside of the `if __name__ == "__main__"`
+# block of a Python script, but for cloud deployments!
 
 
-# @app.local_entrypoint()
-# async def test(test_timeout=10 * MINUTES, content=None, twice=True):
-#     url = VllmServer().serve.get_web_url()
+@app.local_entrypoint()
+async def test(test_timeout=10 * MINUTE, content=None, twice=True):
+    url = SGLang()._experimental_get_flash_urls()[0]
 
-#     system_prompt = {
-#         "role": "system",
-#         "content": "You are a pirate who can't help but drop sly reminders that he went to Harvard.",
-#     }
-#     if content is None:
-#         image_url = "https://static.wikia.nocookie.net/essentialsdocs/images/7/70/Battle.png/revision/latest?cb=20220523172438"
+    system_prompt = {
+        "role": "system",
+        "content": "You are a pirate who can't help but drop sly reminders that he went to Harvard.",
+    }
+    if content is None:
+        image_url = "https://static.wikia.nocookie.net/essentialsdocs/images/7/70/Battle.png/revision/latest?cb=20220523172438"
 
-#         content = [
-#             {
-#                 "type": "text",
-#                 "text": "What action do you think I should take in this situation?"
-#                 " List all the possible actions and explain why you think they are good or bad.",
-#             },
-#             {"type": "image_url", "image_url": {"url": image_url}},
-#         ]
+        content = [
+            {
+                "type": "text",
+                "text": "What action do you think I should take in this situation?"
+                " List all the possible actions and explain why you think they are good or bad.",
+            },
+            {"type": "image_url", "image_url": {"url": image_url}},
+        ]
 
-#     messages = [  # OpenAI chat format
-#         system_prompt,
-#         {"role": "user", "content": content},
-#     ]
+    messages = [  # OpenAI chat format
+        system_prompt,
+        {"role": "user", "content": content},
+    ]
 
-#     async with aiohttp.ClientSession(base_url=url) as session:
-#         print(f"Running health check for server at {url}")
-#         async with session.get("/health", timeout=test_timeout - 1 * MINUTES) as resp:
-#             up = resp.status == 200
-#         assert up, f"Failed health check for server at {url}"
-#         print(f"Successful health check for server at {url}")
+    start_time = time.time()
+    async with aiohttp.ClientSession(base_url=url) as session:
+        while time.time() - start_time < test_timeout:
+            print(f"Running health check for server at {url}")
+            async with session.get("/health", timeout=test_timeout - 1 * MINUTE) as resp:
+                if resp.status == 200:
+                    print(f"Successful health check for server at {url}")
+                    break
+                time.sleep(10)
+        print(f"Sending messages to {url}:", *messages, sep="\n\t")
+        await _send_request(session, "llm", messages, timeout=1 * MINUTE)
 
-#         print(f"Sending messages to {url}:", *messages, sep="\n\t")
-#         await _send_request(session, "llm", messages, timeout=1 * MINUTES)
-#         if twice:
-#             messages[0]["content"] = """Yousa culled Jar Jar Binks.
-#             Always be talkin' in da Gungan style, like thisa, okeyday?
-#             Helpin' da user with big big enthusiasm, makin' tings bombad clear!"""
-#             print(f"Sending messages to {url}:", *messages, sep="\n\t")
-#             await _send_request(session, "llm", messages, timeout=1 * MINUTES)
+async def _send_request(
+    session: aiohttp.ClientSession, model: str, messages: list, timeout: int
+) -> None:
+    # `stream=True` tells an OpenAI-compatible backend to stream chunks
+    payload: dict[str, Any] = {
+        "messages": messages,
+        "model": model,
+        "stream": True,
+        "temperature": 0.15,
+    }
 
+    headers = {"Content-Type": "application/json", "Accept": "text/event-stream"}
 
-# async def _send_request(
-#     session: aiohttp.ClientSession, model: str, messages: list, timeout: int
-# ) -> None:
-#     # `stream=True` tells an OpenAI-compatible backend to stream chunks
-#     payload: dict[str, Any] = {
-#         "messages": messages,
-#         "model": model,
-#         "stream": True,
-#         "temperature": 0.15,
-#     }
+    async with session.post(
+        "/v1/chat/completions", json=payload, headers=headers, timeout=timeout
+    ) as resp:
+        async for raw in resp.content:
+            resp.raise_for_status()
+            # extract new content and stream it
+            line = raw.decode().strip()
+            if not line or line == "data: [DONE]":
+                continue
+            if line.startswith("data: "):  # SSE prefix
+                line = line[len("data: ") :]
 
-#     headers = {"Content-Type": "application/json", "Accept": "text/event-stream"}
+            chunk = json.loads(line)
+            assert (
+                chunk["object"] == "chat.completion.chunk"
+            )  # or something went horribly wrong
+            print(chunk["choices"][0]["delta"]["content"], end="")
+    print()
 
-#     async with session.post(
-#         "/v1/chat/completions", json=payload, headers=headers, timeout=timeout
-#     ) as resp:
-#         async for raw in resp.content:
-#             resp.raise_for_status()
-#             # extract new content and stream it
-#             line = raw.decode().strip()
-#             if not line or line == "data: [DONE]":
-#                 continue
-#             if line.startswith("data: "):  # SSE prefix
-#                 line = line[len("data: ") :]
+if __name__ == "__main__":
+    import asyncio
 
-#             chunk = json.loads(line)
-#             assert (
-#                 chunk["object"] == "chat.completion.chunk"
-#             )  # or something went horribly wrong
-#             print(chunk["choices"][0]["delta"]["content"], end="")
-#     print()
+    # after deployment, we can use the class from anywhere
+    sglang_server = modal.Cls.from_name("example-sglang-low-latency", "SGLang")
+    server = sglang_server()
 
+    async def test(url):
+        messages = [{"role": "user", "content": "Tell me a joke."}]
+        async with aiohttp.ClientSession(base_url=url) as session:
+            await _send_request(session, "llm", messages, timeout=10 * MINUTE)
 
-# # ### Test memory snapshotting
-
-# # Using `modal run` creates an ephemeral Modal App,
-# # rather than a deployed Modal App.
-# # Ephemeral Modal Apps are short-lived,
-# # so they turn off snapshotting.
-
-# # To test the memory snapshot version of the server,
-# # first deploy it with `modal deploy`
-# # and then hit it with a client.
-
-# # You should observe startup improvements
-# # after a handful of cold starts
-# # (usually less than five).
-# # If you want to see the speedup during a test,
-# # we recommend heading to the deployed App in your
-# # [Modal dashboard](https://modal.com/apps)
-# # and manually stopping containers after they have served a request.
-
-# # You can use the client code below to test the endpoint.
-# # It can be run with the command
-
-# # ```
-# # python ministral3_inference.py
-# # ```
-
-# if __name__ == "__main__":
-#     import asyncio
-
-#     # after deployment, we can use the class from anywhere
-#     VllmServer = modal.Cls.from_name("example-ministral3-inference", "VllmServer")
-#     server = VllmServer()
-
-#     async def test(url):
-#         messages = [{"role": "user", "content": "Tell me a joke."}]
-#         async with aiohttp.ClientSession(base_url=url) as session:
-#             await _send_request(session, "llm", messages, timeout=10 * MINUTES)
-
-#     try:
-#         print("calling inference server")
-#         asyncio.run(test(server.serve.get_web_url()))
-#     except modal.exception.NotFoundError:
-#         raise Exception(
-#             f"To take advantage of GPU snapshots, deploy first with modal deploy {__file__}"
-#         )
+    try:
+        print("calling inference server")
+        asyncio.run(test(server.serve.get_web_url()))
+    except modal.exception.NotFoundError:
+        raise Exception(
+            f"To take advantage of GPU snapshots, deploy first with modal deploy {__file__}"
+        )
